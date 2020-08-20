c02673350ad4d2d58d02e055e0942b497e83a9b1
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-08-20 07:17:34 +0300

    Made the recommended changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,69 +18,91 @@ container.addEventListener('click', (event) => {
   const buttonApCol = document.querySelector('.append-column');
   const buttonRemCol = document.querySelector('.remove-column');
 
+  function buttonApRowFunc() {
+    for (let i = 0; i < tableColumn.length; i++) {
+      createTd = document.createElement('td');
+      createTr.append(createTd);
+    }
+    tbody.append(createTr);
+
+    if (tableRow.length === 10) {
+      buttonApRow.disabled = true;
+
+      return buttonApRow.disabled;
+    }
+
+    if (tableRow.length > 2 && tableRow.length < 10) {
+      buttonRemRow.disabled = false;
+
+      return buttonRemRow.disabled;
+    }
+  }
+
+  function buttonRemRowFunc() {
+    tbody.lastElementChild.parentNode.removeChild(tbody.lastElementChild);
+
+    if (tableRow.length === 2) {
+      buttonRemRow.disabled = true;
+
+      return buttonRemRow.disabled;
+    }
+
+    if (tableRow.length < 10) {
+      buttonApRow.disabled = false;
+
+      return buttonApRow.disabled;
+    }
+  }
+
+  function buttonApColFunc() {
+    for (let i = 0; i < tableRow.length; i++) {
+      createTd = document.createElement('td');
+      tableRow[i].append(createTd);
+    }
+
+    if (tableColumn.length > 2 && tableColumn.length < 10) {
+      buttonRemCol.disabled = false;
+
+      return buttonRemCol.disabled;
+    }
+
+    if (tableColumn.length === 10) {
+      buttonApCol.disabled = true;
+
+      return buttonApCol.disabled;
+    }
+  }
+
+  function buttonRemColFunc() {
+    for (let i = 0; i < tableRow.length; i++) {
+      tableRow[i].lastElementChild.parentNode
+        .removeChild(tableRow[i].lastElementChild);
+    }
+
+    if (tableColumn.length === 2) {
+      buttonRemCol.disabled = true;
+
+      return buttonRemCol.disabled;
+    }
+
+    if (tableColumn.length < 10 && tableColumn.length > 2) {
+      buttonApCol.disabled = false;
+
+      return buttonApCol.disabled;
+    }
+  }
+
   switch (item.classList[0]) {
-    case 'append-row':
-      for (let i = 0; i < tableColumn.length; i++) {
-        createTd = document.createElement('td');
-        createTr.append(createTd);
-      }
-      tbody.append(createTr);
-
-      if (tableRow.length === 10) {
-        buttonApRow.disabled = true;
-        break;
-      }
-
-      if (tableRow.length > 2 && tableRow.length < 10) {
-        buttonRemRow.disabled = false;
-        break;
-      }
+    case buttonApRow.classList[0]: buttonApRowFunc();
       break;
-    case 'remove-row':
-      tbody.lastElementChild.parentNode.removeChild(tbody.lastElementChild);
 
-      if (tableRow.length === 2) {
-        buttonRemRow.disabled = true;
-        break;
-      }
-
-      if (tableRow.length < 10) {
-        buttonApRow.disabled = false;
-        break;
-      }
-
+    case buttonRemRow.classList[0]: buttonRemRowFunc();
       break;
-    case 'append-column':
-      for (let i = 0; i < tableRow.length; i++) {
-        createTd = document.createElement('td');
-        tableRow[i].append(createTd);
-      }
 
-      if (tableColumn.length > 2 && tableColumn.length < 10) {
-        buttonRemCol.disabled = false;
-        break;
-      }
-
-      if (tableColumn.length === 10) {
-        buttonApCol.disabled = true;
-        break;
-      }
+    case buttonApCol.classList[0]: buttonApColFunc();
       break;
-    case 'remove-column':
-      for (let i = 0; i < tableRow.length; i++) {
-        tableRow[i].lastElementChild.parentNode
-          .removeChild(tableRow[i].lastElementChild);
-      }
 
-      if (tableColumn.length === 2) {
-        buttonRemCol.disabled = true;
-        break;
-      }
-
-      if (tableColumn.length < 10 && tableColumn.length > 2) {
-        buttonApCol.disabled = false;
-        break;
-      }
+    case buttonRemCol.classList[0]: buttonRemColFunc();
       break;
   }
 });
