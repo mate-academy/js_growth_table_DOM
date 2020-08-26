294bd1857825046a495da6ab9bf5ccdc09dbda4c
nanccyy <lidiapriymak@gmail.com>
2020-08-26 19:54:37 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,76 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+
+const addRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const addColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+removeColumn.addEventListener('click', () => {
+  const row = document.querySelectorAll('tr');
+
+  [...row].map(tr => {
+    tr.lastElementChild.remove();
+
+    switch (tr.childElementCount) {
+      case 2:
+        removeColumn.disabled = true;
+        break;
+
+      case 9:
+        addColumn.disabled = false;
+        break;
+    }
+  });
+});
+
+addColumn.addEventListener('click', () => {
+  const row = document.querySelectorAll('tr');
+
+  [...row].map(tr => {
+    const td = document.createElement('td');
+
+    tr.append(td);
+
+    switch (tr.childElementCount) {
+      case 3:
+        removeColumn.disabled = false;
+        break;
+
+      case 10:
+        addColumn.disabled = true;
+        break;
+    }
+  });
+});
+
+addRow.addEventListener('click', () => {
+  const row = document.querySelectorAll('tr');
+
+  tbody.append(row[0].cloneNode(true));
+
+  switch (tbody.childElementCount) {
+    case 3:
+      removeRow.disabled = false;
+      break;
+
+    case 10:
+      addRow.disabled = true;
+      break;
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  tbody.lastChild.remove();
+
+  switch (tbody.childElementCount) {
+    case 2:
+      removeRow.disabled = true;
+      break;
+
+    case 9:
+      addRow.disabled = false;
+      break;
+  }
+});
