d0bc9b8606a717487c9ee4c65300c6b1f2c6398b
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-08-27 16:17:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,59 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+const rowAppendBtn = document.querySelector('.append-row');
+const rowRemoveBtn = document.querySelector('.remove-row');
+const columnAppendBtn = document.querySelector('.append-column');
+const columnRemoveBtn = document.querySelector('.remove-column');
+
+rowAppendBtn.addEventListener('click', (event) => {
+  table.append(table.firstElementChild.cloneNode(true));
+
+  if (table.children.length === 10) {
+    rowAppendBtn.disabled = true;
+  }
+
+  if (table.children.length > 2) {
+    rowRemoveBtn.disabled = false;
+  }
+});
+
+rowRemoveBtn.addEventListener('click', (event) => {
+  table.lastElementChild.remove();
+
+  if (table.children.length === 2) {
+    rowRemoveBtn.disabled = true;
+  }
+
+  if (table.children.length < 10) {
+    rowAppendBtn.disabled = false;
+  }
+});
+
+columnAppendBtn.addEventListener('click', (event) => {
+  for (const row of table.children) {
+    row.append(row.lastElementChild.cloneNode(true));
+  }
+
+  if (table.firstElementChild.children.length === 10) {
+    columnAppendBtn.disabled = true;
+  }
+
+  if (table.firstElementChild.children.length > 2) {
+    columnRemoveBtn.disabled = false;
+  }
+});
+
+columnRemoveBtn.addEventListener('click', (event) => {
+  for (const row of table.children) {
+    row.lastElementChild.remove();
+  }
+
+  if (table.firstElementChild.children.length === 2) {
+    columnRemoveBtn.disabled = true;
+  }
+
+  if (table.firstElementChild.children.length < 10) {
+    columnAppendBtn.disabled = false;
+  }
+});
