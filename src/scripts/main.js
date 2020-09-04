1032f99a9808ed720e02649472510cb82de8396e
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-09-04 17:41:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,16 @@ const removeColumn = document.querySelector('.remove-column');
 
 addRow.onclick = () => {
   if ([...document.querySelectorAll('tbody tr')].length < 10) {
+    removeRow.disabled = false;
+
     const clone = document.querySelector('tbody tr')
       .cloneNode(document.querySelector('tbody'));
 
     document.querySelector('tbody').append(clone);
+  }
+
+  if ([...document.querySelectorAll('tbody tr')].length === 10) {
+    addRow.disabled = true;
   }
 };
 
@@ -19,11 +25,17 @@ addColumn.onclick = () => {
   const rows = [...document.querySelectorAll('tbody tr')].length;
 
   if (round / rows < 10) {
+    removeColumn.disabled = false;
+
     [...document.querySelectorAll('tbody tr')].map(item => {
       item.innerHTML += item.innerHTML.trim().split(' ')[0];
 
       return item.innerHTML;
     });
+  }
+
+  if (round / rows === 10) {
+    addColumn.disabled = true;
   }
 };
 
@@ -31,14 +43,25 @@ removeColumn.onclick = () => {
   const round = [...document.querySelectorAll('tbody td')].length;
   const rows = [...document.querySelectorAll('tbody tr')].length;
 
-  if (round / rows > 4) {
+  if (round / rows > 2) {
+    addColumn.disabled = false;
+
     ([...document.querySelectorAll('tbody tr')]
       .map(item => [...item.children][0].remove()));
+  }
+
+  if (round / rows === 3) {
+    removeColumn.disabled = true;
   }
 };
 
 removeRow.onclick = () => {
-  if ([...document.querySelectorAll('tbody tr')].length > 4) {
+  if ([...document.querySelectorAll('tbody tr')].length > 2) {
+    addRow.disabled = false;
     document.querySelector('tbody tr').remove();
+  }
+
+  if ([...document.querySelectorAll('tbody tr')].length === 2) {
+    removeRow.disabled = true;
   }
 };
