4050d61a9be6933ed3ccd0ee6d9aca14425e71eb
kshvetsova <katshvetsova@gmail.com>
2020-09-06 04:18:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const table = document.querySelector('tbody');
+const list = table.rows;
+
+container.addEventListener('click', (event) => {
+  if (event.target.className === 'append-column button'
+    && list[0].cells.length !== 10) {
+    [...list].map(elem => {
+      const td = document.createElement('td');
+
+      elem.append(td);
+    });
+  }
+
+  if (event.target.className === 'remove-column button'
+    && list[0].cells.length !== 2) {
+    [...list].map(elem => {
+      elem.lastElementChild.remove();
+    });
+  }
+
+  if (event.target.className === 'append-row button'
+    && list.length !== 10) {
+    const tr = list[0].cloneNode(true);
+
+    table.append(tr);
+  }
+
+  if (event.target.className === 'remove-row button'
+    && list.length !== 2) {
+    table.lastElementChild.remove();
+  }
+});
