a3e42f35f92774e320b7aed90a5ab57ec0b2cc51
Anastasia Chervona <av.chervona@gmail.com>
2020-10-16 16:58:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
-// write code here
+const tableBody = document.querySelector('.field').tBodies[0];
+
+const addRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const addColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+addRowButton.addEventListener('click', (event) => {
+  const row = document.createElement('tr');
+
+  for (let i = 0; i < tableBody.rows[0].cells.length; i++) {
+    row.append(document.createElement('td'));
+  }
+
+  tableBody.append(row);
+  removeRowButton.disabled = false;
+
+  if (tableBody.rows.length >= 10) {
+    addRowButton.disabled = true;
+  }
+});
+
+removeRowButton.addEventListener('click', (event) => {
+  tableBody.removeChild(tableBody.lastElementChild);
+  addRowButton.disabled = false;
+
+  if (tableBody.rows.length <= 2) {
+    removeRowButton.disabled = true;
+  }
+});
+
+addColumnButton.addEventListener('click', (event) => {
+  for (const row of tableBody.rows) {
+    row.append(document.createElement('td'));
+  }
+
+  removeColumnButton.disabled = false;
+
+  if (tableBody.rows[0].cells.length >= 10) {
+    addColumnButton.disabled = true;
+  }
+});
+
+removeColumnButton.addEventListener('click', (event) => {
+  for (const row of tableBody.rows) {
+    row.removeChild(row.lastElementChild);
+  }
+
+  addColumnButton.disabled = false;
+
+  if (tableBody.rows[0].cells.length <= 2) {
+    removeColumnButton.disabled = true;
+  }
+});
