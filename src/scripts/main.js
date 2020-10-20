a4da866eedeb74e14c98bd46fbfa757fccbe28d9
Alina <alina90565@gmail.com>
2020-10-20 19:13:19 +0300

    added maxLength and minLength constants

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,32 +2,33 @@
 
 const field = document.querySelector('.field');
 const table = field.firstElementChild;
+
 const rows = table.rows;
 const cells = rows[0].cells;
+
+const maxLength = 10;
+const minLength = 2;
+
 const removeRow = document.querySelector('.remove-row');
 const appendRow = document.querySelector('.append-row');
 const removeColumn = document.querySelector('.remove-column');
 const appendColumn = document.querySelector('.append-column');
 
 removeRow.addEventListener('click', () => {
-  table.lastElementChild.remove();
-
-  if (rows.length === 9) {
+  if (rows.length === maxLength) {
     appendRow.disabled = false;
   }
 
-  if (rows.length === 2) {
+  table.lastElementChild.remove();
+
+  if (rows.length === minLength) {
     removeRow.disabled = true;
   }
 });
 
 appendRow.addEventListener('click', () => {
-  if (rows.length === 2) {
+  if (rows.length === minLength) {
     removeRow.disabled = false;
-  }
-
-  if (rows.length === 9) {
-    appendRow.disabled = true;
   }
 
   const row = document.createElement('tr');
@@ -35,29 +36,29 @@ appendRow.addEventListener('click', () => {
   row.innerHTML = `<td></td>`.repeat(cells.length);
 
   table.append(row);
+
+  if (rows.length === maxLength) {
+    appendRow.disabled = true;
+  }
 });
 
 removeColumn.addEventListener('click', () => {
-  if (cells.length === 3) {
-    removeColumn.disabled = true;
-  }
-
-  if (cells.length === 10) {
+  if (cells.length === maxLength) {
     appendColumn.disabled = false;
   }
 
   [...rows].forEach(row => {
     row.lastElementChild.remove();
   });
+
+  if (cells.length === minLength) {
+    removeColumn.disabled = true;
+  }
 });
 
 appendColumn.addEventListener('click', () => {
-  if (cells.length === 2) {
+  if (cells.length === minLength) {
     removeColumn.disabled = false;
-  }
-
-  if (cells.length === 9) {
-    appendColumn.disabled = true;
   }
 
   [...rows].forEach(row => {
@@ -65,4 +66,8 @@ appendColumn.addEventListener('click', () => {
 
     row.append(cell);
   });
+
+  if (cells.length === maxLength) {
+    appendColumn.disabled = true;
+  }
 });
