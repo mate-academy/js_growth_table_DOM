b44343389caebbfd0a54c3f40967a08d039b74eb
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-21 10:35:42 +0300

    add names min/max

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,16 @@ const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 const tbody = document.querySelector('.field tbody');
-const min = 2;
-const max = 10;
+
+const minLines = 2;
+const maxLines = 10;
 
 appendRow.onclick = () => {
   removeRow.disabled = false;
 
   tbody.append(tbody.children[0].cloneNode(true));
 
-  appendRow.disabled = tbody.children.length === max;
+  appendRow.disabled = tbody.children.length === maxLines;
 };
 
 removeRow.onclick = () => {
@@ -21,7 +22,7 @@ removeRow.onclick = () => {
 
   tbody.children[0].remove();
 
-  removeRow.disabled = tbody.children.length === min;
+  removeRow.disabled = tbody.children.length === minLines;
 };
 
 appendColumn.onclick = () => {
@@ -33,7 +34,7 @@ appendColumn.onclick = () => {
     td.before(td.cloneNode(true));
   }
 
-  appendColumn.disabled = tbody.children[0].cells.length === max;
+  appendColumn.disabled = tbody.children[0].cells.length === maxLines;
 };
 
 removeColumn.onclick = () => {
@@ -43,5 +44,5 @@ removeColumn.onclick = () => {
     tbody.children[i].cells[0].remove();
   }
 
-  removeColumn.disabled = tbody.children[0].cells.length === min;
+  removeColumn.disabled = tbody.children[0].cells.length === minLines;
 };
