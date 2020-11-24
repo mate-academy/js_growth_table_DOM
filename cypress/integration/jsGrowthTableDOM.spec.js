20c355414af412a2066213912c1d4dfa4e083311
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-11-24 14:37:46 +0200

    fix

--- cypress/integration/jsGrowthTableDOM.spec.js
+++ cypress/integration/jsGrowthTableDOM.spec.js
@@ -1,98 +1,74 @@
 'use strict';
 
+Cypress.Commands.add('multiClick',
+(selector) => {
+  for (let n = 0; n < 7; n++) {
+    cy.get(selector).click( {force: true} );
+  }
+});
+
 describe('Table', () => {
-  beforeEach('Open site', () => {
+  beforeEach(() => {
     cy.visit('/');
     cy.get('tbody > tr').as('rows');
     cy.get('tbody :nth-child(1) :nth-child(n)').as('columns');
+    cy.get('.remove-row').click().as('rmRow');
+    cy.get('.remove-column').click().as('rmColumn');
   });
 
   it('should have 4 rows by default', () => {
-    cy.get('@rows')
-      .then((rows) => {
-        expect(rows, '4 rows').to.have.length(4);
-      });
+    cy.get('@rows').should('have.length', 4)
   });
 
   it('should have 4 columns by default', () => {
-    cy.get('@columns')
-      .then((columns) => {
-        expect(columns, '4 column').to.have.length(4);
-      });
+    cy.get('@columns').should('have.length', 4)
   });
 
-  it('should have max 10 columns', () => {
-    for (let n = 0; n < 6; n++) {
-      cy.get('.append-column').click();
-    }
-
+  it.only('should have max 10 columns', () => {
+    cy.multiClick('.append-column');
     cy.get('tbody > :nth-child(1) > :nth-child(n)')
-      .then(($td) => {
-        expect($td, '10 items').to.have.length(10);
-      });
+      .should('have.length', 10)
   });
 
   it('add column button should be disabled after 10 column', () => {
-    for (let n = 0; n < 6; n++) {
-      cy.get('.append-column').click();
-    }
+    cy.multiClick('.append-column')
     cy.get('.append-column').should('be.disabled');
   });
 
   it('should remove one column', () => {
     cy.get('.remove-column').click();
-
-    cy.get('@columns')
-      .then((columns) => {
-        expect(columns, '3 items').to.have.length(3);
-      });
+    cy.get('@columns').should('have.length', 3)
   });
 
   it('should have max 10 rows', () => {
-    for (let n = 0; n < 6; n++) {
-      cy.get('.append-row').click();
-    }
+    cy.multiClick('.append-row')
     cy.get(':nth-child(7) > :nth-child(1)').should('exist');
   });
 
   it('add row button should be disabled after 10 rows', () => {
-    for (let n = 0; n < 6; n++) {
-      cy.get('.append-row').click();
-    }
+    cy.multiClick('.append-row');
     cy.get('.append-row').should('be.disabled');
   });
 
-  it('should remove one row', () => {
-    cy.get('.remove-row').click();
-
-    cy.get('@rows')
-      .then((rows) => {
-        expect(rows, '3 items').to.have.length(3);
-      });
+  it.only('should remove one row', () => {
+    cy.get('@rmRow');
+    cy.get('@rows').should('have.length', 3)
   });
 
-  it('should have 2 columns and 2 rows minimum', () => {
-    cy.get('.remove-row').click().click();
-    cy.get('.remove-column').click().click();
-
-    cy.get('@rows')
-      .then((rows) => {
-        expect(rows, '2 items').to.have.length(2);
-      });
-
-    cy.get('@columns')
-      .then((columns) => {
-        expect(columns, '2 items').to.have.length(2);
-      });
+  it.only('should have 2 columns and 2 rows minimum', () => {
+    cy.get('@rmRow').click();
+    cy.get('@rmColumn').click();
+    cy.get('@rows').should('have.length', 2)
+    cy.get('@columns').should('have.length', 2)
   });
 
-  it('remove row button should be disabled', () => {
-    cy.get('.remove-row').click().click();
+  it.only('remove row button should be disabled', () => {
+    cy.get('@rmRow').click();
     cy.get('.remove-row').should('be.disabled');
   });
 
   it('remove column button should be disabled', () => {
-    cy.get('.remove-column').click().click();
+    cy.get('@rmColumn').click();
     cy.get('.remove-column').should('be.disabled');
   });
 });
