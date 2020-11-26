fe8be8b3d3624a4db329a6e31bcf065495f7e1df
Taras Shchadylo <tarasdidg@gmail.com>
2020-11-26 15:02:09 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,68 @@
 'use strict';
 
 // write code here
+const table = document.querySelector('table');
+
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+const rowsTable = table.rows;
+const columnTable = rowsTable[0].cells;
+
+const minLength = 2;
+const maxLength = 10;
+
+appendRow.addEventListener('click', () => {
+  if (rowsTable.length < maxLength) {
+    table.append(rowsTable[0].cloneNode(true));
+  }
+
+  if (rowsTable.length === maxLength) {
+    appendRow.disabled = true;
+  } else {
+    removeRow.disabled = false;
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  if (rowsTable.length > minLength) {
+    rowsTable[0].remove();
+  }
+
+  if (rowsTable.length === minLength) {
+    removeRow.disabled = true;
+  } else {
+    appendRow.disabled = false;
+  }
+});
+
+appendColumn.addEventListener('click', () => {
+  for (let i = 0; i < rowsTable.length; i++) {
+    if (columnTable.length <= maxLength) {
+      rowsTable[i].append(rowsTable[0].cells[0].cloneNode(true));
+    }
+
+    if (columnTable.length === maxLength) {
+      appendColumn.disabled = true;
+    } else {
+      removeColumn.disabled = false;
+    }
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  for (let i = 0; i < rowsTable.length; i++) {
+    if (columnTable.length >= minLength) {
+      rowsTable[i].cells[0].remove();
+    }
+
+    if (columnTable.length === minLength) {
+      removeColumn.disabled = true;
+    } else {
+      appendColumn.disabled = false;
+    }
+  }
+});
