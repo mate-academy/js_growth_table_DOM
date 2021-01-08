f16978356a69d4c5d2fa617a2066f84fb963bb96
Alex Lemeshchuk <alexyskr@gmail.com>
2021-01-08 20:56:36 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+const content = document.querySelector('.container');
+
+function rebuildTable(target) {
+  const tableMain = document.querySelector('.field');
+  const parentTR = tableMain.children[0];
+  const allTR = [...document.querySelectorAll('tr')];
+
+  if (target.classList.contains('append-row')
+    && parentTR.children.length < 10) {
+    const cloneTR = parentTR.firstChild.cloneNode(true);
+
+    parentTR.append(cloneTR);
+  }
+
+  if (target.classList.contains('remove-row')
+    && parentTR.children.length > 2) {
+    parentTR.lastChild.remove();
+  }
+
+  if (target.classList.contains('append-column')
+    && allTR[0].children.length < 10) {
+    for (let i = 0; i < allTR.length; i++) {
+      const cloneTD = allTR[0].firstElementChild.cloneNode(true);
+
+      allTR[i].append(cloneTD);
+    }
+  }
+
+  if (target.classList.contains('remove-column')
+    && allTR[0].children.length > 2) {
+    allTR.map(element => element.lastChild.remove());
+  }
+}
+
+content.addEventListener('click', (e) => {
+  const item = e.target;
+
+  if (item.tagName !== 'BUTTON') {
+    return;
+  }
+
+  rebuildTable(item);
+});
