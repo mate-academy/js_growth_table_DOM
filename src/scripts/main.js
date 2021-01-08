2ce730561a416c4f42170f7f811d687a48738a25
PaulT <ptimofejev@gmail.com>
2021-01-09 01:31:26 +0200

    Growth table solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
-// write code here
+const tableBody = document.querySelector('.field').tBodies[0];
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+let rows = tableBody.children.length;
+let columns = tableBody.firstElementChild.children.length;
+
+appendRow.addEventListener('click', () => {
+  tableBody.append(tableBody.firstElementChild.cloneNode(true));
+  rows++;
+  removeRow.disabled = false;
+
+  if (rows === 10) {
+    appendRow.disabled = true;
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  tableBody.lastElementChild.remove();
+  rows--;
+  appendRow.disabled = false;
+
+  if (rows === 2) {
+    removeRow.disabled = true;
+  }
+});
+
+appendColumn.addEventListener('click', ev => {
+  const box = document.createElement('td');
+
+  [...tableBody.children].forEach(row => row.append(box.cloneNode()));
+  columns++;
+  removeColumn.disabled = false;
+
+  if (columns === 10) {
+    appendColumn.disabled = true;
+  }
+});
+
+removeColumn.addEventListener('click', ev => {
+  [...tableBody.children].forEach(row => row.lastElementChild.remove());
+  columns--;
+  appendColumn.disabled = false;
+
+  if (columns === 2) {
+    removeColumn.disabled = true;
+  }
+});
