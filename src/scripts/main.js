f2baaedd499af579ef60aeb7cdf15367330d458b
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-10 15:26:01 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,51 +1,61 @@
 'use strict';
 
 const table = document.querySelector('TBODY');
-const rows = [...document.querySelectorAll('TR')];
+const rows = document.querySelectorAll('TR');
 
 const appendColumnButton = document.querySelector('.append-column');
 const removeColumnButton = document.querySelector('.remove-column');
 const appendrowButton = document.querySelector('.append-row');
 const removerowButton = document.querySelector('.remove-row');
 
-function disabledChanger() {
-  (rows[0].children.length >= 10)
-    ? appendColumnButton.disabled = true
-    : appendColumnButton.disabled = false;
-
-  (rows[0].children.length > 2)
-    ? removeColumnButton.disabled = false
-    : removeColumnButton.disabled = true;
-
-  (table.children.length >= 10)
-    ? appendrowButton.disabled = true
-    : appendrowButton.disabled = false;
-
-  (table.children.length > 2)
-    ? removerowButton.disabled = false
-    : removerowButton.disabled = true;
-}
-
 appendColumnButton.addEventListener('click', () => {
-  rows.forEach(
-    el => el.append(el.lastElementChild.cloneNode())
+  [...table.children].forEach(
+    el => el.append(el.lastElementChild.cloneNode(true))
   );
-  disabledChanger();
+
+  if (rows[0].children.length >= 10) {
+    appendColumnButton.disabled = true;
+  }
+
+  if (rows[0].children.length >= 3) {
+    removeColumnButton.disabled = false;
+  }
 });
 
 removeColumnButton.addEventListener('click', () => {
-  rows.forEach(
+  [...table.children].forEach(
     el => el.lastElementChild.remove()
   );
-  disabledChanger();
+
+  if (rows[0].children.length <= 2) {
+    removeColumnButton.disabled = true;
+  }
+
+  if (rows[0].children.length < 10) {
+    appendColumnButton.disabled = false;
+  }
 });
 
 appendrowButton.addEventListener('click', () => {
   table.append(table.lastElementChild.cloneNode(true));
-  disabledChanger();
+
+  if (table.children.length >= 10) {
+    appendrowButton.disabled = true;
+  }
+
+  if (table.children.length > 2) {
+    removerowButton.disabled = false;
+  }
 });
 
 removerowButton.addEventListener('click', () => {
   table.lastElementChild.remove();
-  disabledChanger();
+
+  if (table.children.length <= 2) {
+    removerowButton.disabled = true;
+  }
+
+  if (table.children.length < 10) {
+    appendrowButton.disabled = false;
+  }
 });
