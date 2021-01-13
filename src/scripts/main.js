7920367ef1d32146dec7cabb489d9f0454bbe840
Yurii Fitsyk <yuriy.fitsyk@gmail.com>
2021-01-13 06:56:59 +0200

    changes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,50 +8,55 @@ const field = document.querySelector('.field').children[0];
 const rows = field.children;
 
 document.addEventListener('click', (e) => {
-  if (e.target === appendRow) {
-    if (field.children.length === 9) {
-      appendRow.disabled = true;
-    }
-
-    if (field.children.length !== 10) {
-      field.append(field.children[0].cloneNode(true));
-    }
-
-    removeRow.disabled = false;
-  }
-
-  if (e.target === removeRow) {
-    if (field.children.length === 3) {
-      removeRow.disabled = true;
-    }
-
-    field.children[0].remove();
-    appendRow.disabled = false;
-  }
-
-  if (e.target === appendColumn) {
-    if (rows[0].children.length === 9) {
-      appendColumn.disabled = true;
-    }
-
-    if (rows[0].children.length !== 10) {
-      for (const row of rows) {
-        row.append(row.children[0].cloneNode(true));
+  switch (e.target) {
+    case appendRow:
+      if (field.children.length === 9) {
+        appendRow.disabled = true;
       }
-    }
 
-    removeColumn.disabled = false;
-  }
+      if (field.children.length !== 10) {
+        field.append(field.children[0].cloneNode(true));
+      }
 
-  if (e.target === removeColumn) {
-    if (rows[0].children.length === 3) {
-      removeColumn.disabled = true;
-    }
+      removeRow.disabled = false;
+      break;
 
-    for (const row of rows) {
-      row.children[0].remove();
-    }
+    case removeRow:
+      if (field.children.length === 3) {
+        removeRow.disabled = true;
+      }
 
-    appendColumn.disabled = false;
+      field.children[0].remove();
+      appendRow.disabled = false;
+      break;
+
+    case appendColumn:
+      if (rows[0].children.length === 9) {
+        appendColumn.disabled = true;
+      }
+
+      if (rows[0].children.length !== 10) {
+        for (const row of rows) {
+          row.append(row.children[0].cloneNode(true));
+        }
+      }
+
+      removeColumn.disabled = false;
+      break;
+
+    case removeColumn:
+      if (rows[0].children.length === 3) {
+        removeColumn.disabled = true;
+      }
+
+      for (const row of rows) {
+        row.children[0].remove();
+      }
+
+      appendColumn.disabled = false;
+      break;
+
+    default:
+      break;
   }
 });
