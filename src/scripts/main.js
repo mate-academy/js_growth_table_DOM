cf87c36fd0e49867fdf082ea0213360f5f960c6a
morozoleh <olegmoroz93@gmail.com>
2021-02-27 21:08:41 +0200

    disabled

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,56 +7,48 @@ const removeColumn = document.querySelector('.remove-column');
 
 const table = document.querySelector('tbody');
 const tableRow = document.querySelector('tr');
-const minElements = 2;
+const minElements = 3;
 const maxElements = 10;
 
 function addRow(e) {
-  if (e.target === appendRow) {
-    table.prepend(tableRow.cloneNode('cloned'));
-    removeRow.disabled = false;
-  };
+  table.append(tableRow.cloneNode('cloned'));
+  removeRow.disabled = false;
 
-  if (table.children.length >= maxElements) {
+  if (table.children.length === maxElements) {
     appendRow.disabled = true;
   }
 }
 
 function deleteRow(e) {
-  if (e.target === removeRow) {
-    table.children[table.children.length - 1].remove();
-    appendRow.disabled = false;
-  };
+  table.children[table.children.length - 1].remove();
+  appendRow.disabled = false;
 
-  if (table.children.length === minElements) {
+  if (table.children.length < minElements) {
     removeRow.disabled = true;
   }
 }
 
 function addColumn(e) {
-  if (e.target === appendColumn) {
-    [...table.rows].forEach(
-      row => {
-        const clonedElement = row.cells[0].cloneNode(true);
+  [...table.rows].forEach(
+    row => {
+      const clonedElement = row.cells[0].cloneNode(true);
 
-        row.insertBefore(clonedElement, row.cells[0]);
-      });
-    removeColumn.disabled = false;
-  };
+      row.insertBefore(clonedElement, row.cells[0]);
+    });
+  removeColumn.disabled = false;
 
-  if (tableRow.children.length >= maxElements) {
+  if (tableRow.children.length === maxElements) {
     appendColumn.disabled = true;
   }
 }
 
 function deleteColumn(e) {
-  if (e.target === removeColumn) {
-    [...table.rows].forEach(
-      row => row.children[row.children.length - 1].remove()
-    );
-    appendColumn.disabled = false;
-  };
+  [...table.rows].forEach(
+    row => row.children[row.children.length - 1].remove()
+  );
+  appendColumn.disabled = false;
 
-  if (tableRow.children.length === minElements) {
+  if (tableRow.children.length < minElements) {
     removeColumn.disabled = true;
   }
 }
