e4fce0f44a07b0209cd2ec3bdafb48da0db66143
Serhii Ivanyk <youdex06@gmail.com>
2021-03-15 14:58:08 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,41 +3,51 @@
 // write code here
 const container = document.querySelector('.container');
 const field = document.querySelector('tbody');
-
 const itemTd = document.createElement('td');
+
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
 
 container.addEventListener('click', (tap) => {
   const element = document.getElementsByTagName('tr');
 
   if (tap.target.matches('.append-row')
   && field.childElementCount !== 10) {
-    document.querySelector('.remove-row').disabled = false;
+    removeRow.disabled = false;
     field.append(field.children[0].cloneNode(true));
-  } else if (tap.target.matches('.remove-row')) {
-    document.querySelector('.append-row').disabled = false;
+
+    if (field.childElementCount === 10) {
+      appendRow.disabled = true;
+    }
+  }
+
+  if (tap.target.matches('.remove-row')) {
+    appendRow.disabled = false;
     field.lastElementChild.remove();
 
     if (field.childElementCount === 2) {
-      document.querySelector('.remove-row').disabled = true;
+      removeRow.disabled = true;
     }
-  } else if (tap.target.matches('.append-column')
+  }
+
+  if (tap.target.matches('.append-column')
   && element[0].childElementCount !== 10) {
-    document.querySelector('.remove-column').disabled = false;
+    removeColumn.disabled = false;
     [...element].forEach(tag => tag.append(itemTd.cloneNode()));
-  } else if (tap.target.matches('.remove-column')) {
-    document.querySelector('.append-column').disabled = false;
+
+    if (element[0].childElementCount === 10) {
+      appendColumn.disabled = true;
+    }
+  }
+
+  if (tap.target.matches('.remove-column')) {
+    appendColumn.disabled = false;
     [...element].forEach(tag => tag.lastElementChild.remove());
 
     if (element[0].childElementCount === 2) {
-      document.querySelector('.remove-column').disabled = true;
+      removeColumn.disabled = true;
     }
-  }
-
-  if (field.childElementCount === 10) {
-    document.querySelector('.append-row').disabled = true;
-  }
-
-  if (element[0].childElementCount === 10) {
-    document.querySelector('.append-column').disabled = true;
   }
 });
