5e1dcd773d2f151ab89396193c0818982cdc6498
Ivan Major <tarstarcas@gmail.com>
2021-05-25 18:57:08 +0300

    Changes V1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,70 +2,140 @@
 
 const container = document.querySelector('.container');
 
-const appendRow = container.querySelector('.append-row');
-
-const removeRow = container.querySelector('.remove-row');
-
-const appendCol = container.querySelector('.append-column');
-
-const removeCol = container.querySelector('.remove-column');
-
 const table = container.querySelector('.field');
 
 const body = table.querySelector('tbody');
 
-appendRow.addEventListener('click', (e) => {
-  const row = table.querySelector('tr');
+container.addEventListener('click', (e) => {
+  switch (e.target) {
+    case container.querySelector('.append-row'):
+      const row = table.querySelector('tr');
 
-  const rowAdd = document.createElement('tr');
+      const rowAdd = document.createElement('tr');
 
-  const cellAdd = document.createElement('td');
+      const cellAdd = document.createElement('td');
 
-  for (let i = 0; i < row.cells.length; i++) {
-    rowAdd.append(cellAdd.cloneNode());
-  };
+      for (let i = 0; i < row.cells.length; i++) {
+        rowAdd.append(cellAdd.cloneNode());
+      };
 
-  body.append(rowAdd);
+      body.append(rowAdd);
 
-  table.rows.length === 10
-    ? appendRow.disabled = true
-    : appendRow.disabled = false;
+      table.rows.length === 10
+        ? container.querySelector('.append-row').disabled = true
+        : container.querySelector('.append-row').disabled = false;
+
+      break;
+
+    case container.querySelector('.remove-row'):
+      const rowDel = table.querySelector('tr');
+
+      rowDel.remove();
+
+      table.rows.length === 2
+        ? container.querySelector('.remove-row').disabled = true
+        : container.querySelector('.remove-row').disabled = false;
+
+      break;
+
+    case container.querySelector('.append-column'):
+      const colAdd = document.createElement('td');
+
+      for (const item of table.rows) {
+        item.append(colAdd.cloneNode());
+      };
+
+      const rowApp = table.querySelector('tr');
+
+      rowApp.cells.length === 10
+        ? container.querySelector('.append-column').disabled = true
+        : container.querySelector('.append-column').disabled = false;
+
+      break;
+
+    case container.querySelector('.remove-column'):
+      for (const item of table.rows) {
+        const td = item.querySelector('td');
+
+        td.remove();
+      };
+
+      const rowRem = table.querySelector('tr');
+
+      rowRem.cells.length === 2
+        ? container.querySelector('.remove-column').disabled = true
+        : container.querySelector('.remove-column').disabled = false;
+
+      break;
+  }
 });
 
-removeRow.addEventListener('click', (e) => {
-  const rowDel = table.querySelector('tr');
+// const container = document.querySelector('.container');
 
-  rowDel.remove();
+// const appendRow = container.querySelector('.append-row');
 
-  table.rows.length === 2
-    ? removeRow.disabled = true
-    : removeRow.disabled = false;
-});
+// const removeRow = container.querySelector('.remove-row');
 
-appendCol.addEventListener('click', (e) => {
-  const colAdd = document.createElement('td');
+// const appendCol = container.querySelector('.append-column');
 
-  for (const item of table.rows) {
-    item.append(colAdd.cloneNode());
-  };
+// const removeCol = container.querySelector('.remove-column');
 
-  const rowApp = table.querySelector('tr');
+// const table = container.querySelector('.field');
 
-  rowApp.cells.length === 10
-    ? appendCol.disabled = true
-    : appendCol.disabled = false;
-});
+// const body = table.querySelector('tbody');
 
-removeCol.addEventListener('click', (e) => {
-  for (const item of table.rows) {
-    const td = item.querySelector('td');
+// appendRow.addEventListener('click', (e) => {
+//   const row = table.querySelector('tr');
 
-    td.remove();
-  };
+//   const rowAdd = document.createElement('tr');
 
-  const rowRem = table.querySelector('tr');
+//   const cellAdd = document.createElement('td');
 
-  rowRem.cells.length === 2
-    ? removeCol.disabled = true
-    : removeCol.disabled = false;
-});
+//   for (let i = 0; i < row.cells.length; i++) {
+//     rowAdd.append(cellAdd.cloneNode());
+//   };
+
+//   body.append(rowAdd);
+
+//   table.rows.length === 10
+//     ? appendRow.disabled = true
+//     : appendRow.disabled = false;
+// });
+
+// removeRow.addEventListener('click', (e) => {
+//   const rowDel = table.querySelector('tr');
+
+//   rowDel.remove();
+
+//   table.rows.length === 2
+//     ? removeRow.disabled = true
+//     : removeRow.disabled = false;
+// });
+
+// appendCol.addEventListener('click', (e) => {
+//   const colAdd = document.createElement('td');
+
+//   for (const item of table.rows) {
+//     item.append(colAdd.cloneNode());
+//   };
+
+//   const rowApp = table.querySelector('tr');
+
+//   rowApp.cells.length === 10
+//     ? appendCol.disabled = true
+//     : appendCol.disabled = false;
+// });
+
+// removeCol.addEventListener('click', (e) => {
+//   for (const item of table.rows) {
+//     const td = item.querySelector('td');
+
+//     td.remove();
+//   };
+
+//   const rowRem = table.querySelector('tr');
+
+//   rowRem.cells.length === 2
+//     ? removeCol.disabled = true
+//     : removeCol.disabled = false;
+// });
