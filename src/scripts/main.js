d419a2a9336874f29c05f36ff05945cd8f4ac634
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-05-26 15:08:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+const buttonAppendRow = document.querySelector('.append-row');
+const buttonRemoveRow = document.querySelector('.remove-row');
+const buttonAppendColumn = document.querySelector('.append-column');
+const buttonRemoveColumn = document.querySelector('.remove-column');
+
+document.addEventListener('click', e => {
+  switch (true) {
+    case (e.target.classList.contains('append-row')):
+      const newRow = table.insertRow();
+
+      [...table.rows[0].cells].forEach(() => newRow.insertCell());
+      break;
+    case (e.target.classList.contains('remove-row')):
+      table.deleteRow(-1);
+      break;
+    case (e.target.classList.contains('append-column')):
+      for (const row of table.rows) {
+        row.insertCell();
+      }
+      break;
+    case (e.target.classList.contains('remove-column')):
+      for (const row of table.rows) {
+        row.deleteCell(-1);
+      }
+      break;
+  }
+
+  if (table.rows.length === 10) {
+    buttonAppendRow.disabled = true;
+  } else {
+    buttonAppendRow.disabled = false;
+  }
+
+  if (table.rows.length === 2) {
+    buttonRemoveRow.disabled = true;
+  } else {
+    buttonRemoveRow.disabled = false;
+  }
+
+  if (table.rows[0].cells.length === 10) {
+    buttonAppendColumn.disabled = true;
+  } else {
+    buttonAppendColumn.disabled = false;
+  }
+
+  if (table.rows[0].cells.length === 2) {
+    buttonRemoveColumn.disabled = true;
+  } else {
+    buttonRemoveColumn.disabled = false;
+  }
+});
