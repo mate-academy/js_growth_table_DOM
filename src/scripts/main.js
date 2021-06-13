0708980fd899e5072c9a9282f489bdec4ced65db
Eugene Khablenko <eugene.khablenko@gmail.com>
2021-06-13 20:10:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const addRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const addColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+addRow.addEventListener('click', () => {
+  if (table.lastElementChild.rows.length >= 10) {
+    return;
+  }
+
+  const elementRow = document.createElement('tr');
+  const currentCells = [...table.lastElementChild.lastElementChild.children];
+
+  table.lastElementChild.appendChild(elementRow);
+
+  for (const cell of currentCells) {
+    elementRow.appendChild(cell.cloneNode());
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  if (table.lastElementChild.rows.length <= 2) {
+    return;
+  }
+
+  const lastRow = table.lastElementChild.lastElementChild;
+
+  lastRow.remove();
+});
+
+addColumn.addEventListener('click', () => {
+  if (table.lastElementChild.lastElementChild.cells.length >= 10) {
+    return;
+  }
+
+  const rows = [...table.lastElementChild.children];
+
+  for (const row of rows) {
+    const item = document.createElement('td');
+
+    row.append(item);
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  if (table.lastElementChild.lastElementChild.children.length <= 2) {
+    return;
+  }
+
+  const rows = [...table.lastElementChild.children];
+
+  for (const row of rows) {
+    const lastColumn = row.lastElementChild;
+
+    lastColumn.remove();
+  }
+});
