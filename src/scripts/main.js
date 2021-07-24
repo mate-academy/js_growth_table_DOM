b8f753e10c7035e6a5633462a1164b4a8c800df8
Andriy Fesych <andriyhimself@gmail.com>
2021-07-24 22:38:10 +0300

    great_solution_IMO

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+document.querySelector('.append-row').addEventListener('click', () => {
+  const trs = document.querySelectorAll('tr');
+
+  (trs.length < 10)
+  && (trs[trs.length - 1].parentElement
+    .append(trs[trs.length - 1].cloneNode(true)));
+});
+
+document.querySelector('.remove-row').addEventListener('click', () => {
+  const trs = document.querySelectorAll('tr');
+
+  (trs.length > 2) && (trs[trs.length - 1].remove());
+});
+
+document.querySelector('.append-column').addEventListener('click', () => {
+  (document.querySelector('tr').children.length < 10)
+  && (document.querySelectorAll('tr').forEach((row) => {
+    row.append(row.lastElementChild.cloneNode(true));
+  }));
+});
+
+document.querySelector('.remove-column').addEventListener('click', () => {
+  (document.querySelector('tr').children.length > 2)
+  && (document.querySelectorAll('tr').forEach((row) => {
+    row.lastElementChild.remove();
+  }));
+});
