982e6bd17ee2fec3cf5711b95a19fd356446b3a6
Andriy Fesych <andriyhimself@gmail.com>
2021-07-24 23:41:04 +0300

    Solution_0.1_as_per_requirements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,27 +3,40 @@
 document.querySelector('.append-row').addEventListener('click', () => {
   const trs = document.querySelectorAll('tr');
 
-  (trs.length < 10)
-  && (trs[trs.length - 1].parentElement
-    .append(trs[trs.length - 1].cloneNode(true)));
+  trs[trs.length - 1].parentElement
+    .append(trs[trs.length - 1].cloneNode(true));
+
+  if (trs.length === 9) {
+    document.querySelector('.append-row').disabled = true;
+  };
 });
 
 document.querySelector('.remove-row').addEventListener('click', () => {
   const trs = document.querySelectorAll('tr');
 
-  (trs.length > 2) && (trs[trs.length - 1].remove());
+  trs[trs.length - 1].remove();
+
+  if (trs.length === 3) {
+    document.querySelector('.remove-row').disabled = true;
+  };
 });
 
 document.querySelector('.append-column').addEventListener('click', () => {
-  (document.querySelector('tr').children.length < 10)
-  && (document.querySelectorAll('tr').forEach((row) => {
+  document.querySelectorAll('tr').forEach((row) => {
     row.append(row.lastElementChild.cloneNode(true));
-  }));
+  });
+
+  if (document.querySelector('tr').children.length === 10) {
+    document.querySelector('.append-column.button').disabled = true;
+  };
 });
 
 document.querySelector('.remove-column').addEventListener('click', () => {
-  (document.querySelector('tr').children.length > 2)
-  && (document.querySelectorAll('tr').forEach((row) => {
+  document.querySelectorAll('tr').forEach((row) => {
     row.lastElementChild.remove();
-  }));
+  });
+
+  if (document.querySelector('tr').children.length === 2) {
+    document.querySelector('.remove-column').disabled = true;
+  }
 });
