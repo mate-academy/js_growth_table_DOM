08205cac8fc8299e9eb70d4eda91f419f630586f
Andriy Fesych <andriyhimself@gmail.com>
2021-07-27 21:09:20 +0300

    Solution_special4Mykola

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,11 @@
 document.querySelector('.append-row').addEventListener('click', () => {
   const trs = document.querySelectorAll('tr');
 
-  trs[trs.length - 1].parentElement
-    .append(trs[trs.length - 1].cloneNode(true));
+  document.querySelector('.remove-row').disabled = false;
+
+  (trs.length < 10)
+  && (trs[trs.length - 1].parentElement
+    .append(trs[trs.length - 1].cloneNode(true)));
 
   if (trs.length === 9) {
     document.querySelector('.append-row').disabled = true;
@@ -14,29 +17,37 @@ document.querySelector('.append-row').addEventListener('click', () => {
 document.querySelector('.remove-row').addEventListener('click', () => {
   const trs = document.querySelectorAll('tr');
 
-  trs[trs.length - 1].remove();
-
   if (trs.length === 3) {
     document.querySelector('.remove-row').disabled = true;
   };
+
+  document.querySelector('.append-row').disabled = false;
+
+  (trs.length > 2) && (trs[trs.length - 1].remove());
 });
 
 document.querySelector('.append-column').addEventListener('click', () => {
-  document.querySelectorAll('tr').forEach((row) => {
+  (document.querySelector('tr').children.length < 10)
+  && (document.querySelectorAll('tr').forEach((row) => {
     row.append(row.lastElementChild.cloneNode(true));
-  });
+  }));
 
   if (document.querySelector('tr').children.length === 10) {
-    document.querySelector('.append-column.button').disabled = true;
+    document.querySelector('.append-column').disabled = true;
   };
+
+  document.querySelector('.remove-column').disabled = false;
 });
 
 document.querySelector('.remove-column').addEventListener('click', () => {
-  document.querySelectorAll('tr').forEach((row) => {
+  (document.querySelector('tr').children.length > 2)
+  && (document.querySelectorAll('tr').forEach((row) => {
     row.lastElementChild.remove();
-  });
+  }));
 
   if (document.querySelector('tr').children.length === 2) {
     document.querySelector('.remove-column').disabled = true;
-  }
+  };
+
+  document.querySelector('.append-column').disabled = false;
 });
