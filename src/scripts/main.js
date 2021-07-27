35c56c9159772ab663240eee4cb0aea13b99cee3
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-27 16:13:44 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,68 +4,93 @@ const addRow = document.querySelector('.append-row');
 const deleteRow = document.querySelector('.remove-row');
 const addColumn = document.querySelector('.append-column');
 const delColumn = document.querySelector('.remove-column');
-const tr = document.querySelectorAll('tr');
 
-addRow.addEventListener('click', () => {
-  const tbody = document.querySelector('tbody');
+document.addEventListener('click', (e) => {
+  const choice = e.target;
+
+  switch (choice) {
+    case addRow:
+      addRows();
+      break;
+
+    case deleteRow:
+      delRow();
+      break;
+
+    case addColumn:
+      addCol();
+      break;
+
+    case delColumn:
+      delCol();
+      break;
+  };
+
   const trLength = document.querySelectorAll('tr').length;
+  const tdLength = document.querySelectorAll('tr')[0]
+    .querySelectorAll('td').length;
+
+  switch (trLength) {
+    case 2:
+      deleteRow.setAttribute('disabled', 'disabled');
+      break;
+
+    case 3:
+      deleteRow.removeAttribute('disabled');
+      break;
+
+    case 9:
+      addRow.removeAttribute('disabled');
+      break;
+
+    case 10:
+      addRow.setAttribute('disabled', 'disabled');
+      break;
+  };
+
+  switch (tdLength) {
+    case 2:
+      delColumn.setAttribute('disabled', 'disabled');
+      break;
+
+    case 3:
+      delColumn.removeAttribute('disabled');
+      break;
+
+    case 9:
+      addColumn.removeAttribute('disabled');
+      break;
+
+    case 10:
+      addColumn.setAttribute('disabled', 'disabled');
+      break;
+  };
+});
+
+function addRows() {
+  const tr = document.querySelectorAll('tr');
+  const tbody = document.querySelector('tbody');
 
   tbody.insertAdjacentHTML('beforeend', ` ${tr[0].innerHTML}`);
+};
 
-  if (trLength === 9) {
-    addRow.setAttribute('disabled', 'disabled');
-  };
-
-  if (trLength === 2) {
-    deleteRow.removeAttribute('disabled');
-  };
-});
-
-deleteRow.addEventListener('click', () => {
-  const trLength = document.querySelectorAll('tr').length;
-
+function delRow() {
   document.querySelector('tbody').lastChild.remove();
   document.querySelector('tbody').lastChild.remove();
+};
 
-  if (trLength === 3) {
-    deleteRow.setAttribute('disabled', 'disabled');
-  };
-
-  if (trLength === 10) {
-    addRow.removeAttribute('disabled');
-  };
-});
-
-addColumn.addEventListener('click', () => {
+function addCol() {
   const getTr = document.querySelectorAll('tr');
-  const trLength = getTr[0].querySelectorAll('td').length;
 
   for (const coll of getTr) {
     coll.insertAdjacentHTML('beforeend', '<td>');
-
-    if (trLength === 9) {
-      addColumn.setAttribute('disabled', 'disabled');
-    };
-
-    if (trLength === 2) {
-      delColumn.removeAttribute('disabled');
-    };
   };
-});
+};
 
-delColumn.addEventListener('click', () => {
+function delCol() {
   const getTr = document.querySelectorAll('tr');
-  const trLength = getTr[0].querySelectorAll('td').length;
 
   for (const coll of getTr) {
     coll.children[0].remove();
   };
-
-  if (trLength <= 3) {
-    delColumn.setAttribute('disabled', 'disabled');
-  };
-
-  if (trLength === 10) {
-    addColumn.removeAttribute('disabled');
-  };
-});
+};
