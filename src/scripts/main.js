98ac274c43934d7f92072cadb0829a2948aab635
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-10 12:21:31 +0200

    solution v02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,60 +10,58 @@ const addCol = document.querySelector('.append-column');
 const removeRow = document.querySelector('.remove-row');
 const addRow = document.querySelector('.append-row');
 
-addRow.addEventListener('click', (e) => {
-  numOfRows++;
+document.querySelector('.container').addEventListener('click', (e) => {
+  switch (e.target) {
+    case addRow:
+      numOfRows++;
 
-  const newRow = document.createElement('tr');
+      const newRow = document.createElement('tr');
 
-  for (let i = 0; i < numOfCols; i++) {
-    const newCol = document.createElement('td');
+      for (let i = 0; i < numOfCols; i++) {
+        const newCol = document.createElement('td');
 
-    newRow.append(newCol);
+        newRow.append(newCol);
+      }
+      table.prepend(newRow);
+      break;
+    case removeRow:
+      numOfRows--;
+      table.removeChild(table.children[0]);
+      break;
+    case addCol:
+      numOfCols++;
+      numOfCols === 10 ? addCol.disabled = true : addCol.disabled = false;
+      numOfCols === 2 ? removeCol.disabled = true : removeCol.disabled = false;
+
+      let allRows = [...document.querySelectorAll('tr')];
+
+      allRows.forEach(row => {
+        const newUnit = document.createElement('td');
+
+        row.append(newUnit);
+      });
+
+      table.prepend(...allRows);
+
+      numOfCols === 10 ? addCol.disabled = true : addCol.disabled = false;
+      numOfCols === 2 ? removeCol.disabled = true : removeCol.disabled = false;
+      break;
+    case removeCol:
+      numOfCols--;
+      removeCol.disabled = false;
+
+      allRows = [...document.querySelectorAll('tr')];
+
+      allRows.forEach(row => {
+        row.removeChild(row.firstElementChild);
+      });
+
+      table.prepend(...allRows);
+      break;
   }
-  table.prepend(newRow);
-  numOfRows === 10 ? addRow.disabled = true : addRow.disabled = false;
-  numOfRows === 2 ? removeRow.disabled = true : removeRow.disabled = false;
-});
-
-removeRow.addEventListener('click', (e) => {
-  numOfRows--;
-  table.removeChild(table.children[0]);
 
   numOfRows === 10 ? addRow.disabled = true : addRow.disabled = false;
   numOfRows === 2 ? removeRow.disabled = true : removeRow.disabled = false;
-});
-
-addCol.addEventListener('click', (e) => {
-  numOfCols++;
-  numOfCols === 10 ? addCol.disabled = true : addCol.disabled = false;
-  numOfCols === 2 ? removeCol.disabled = true : removeCol.disabled = false;
-
-  const allRows = [...document.querySelectorAll('tr')];
-
-  allRows.forEach(row => {
-    const newUnit = document.createElement('td');
-
-    row.append(newUnit);
-  });
-
-  table.prepend(...allRows);
-
-  numOfCols === 10 ? addCol.disabled = true : addCol.disabled = false;
-  numOfCols === 2 ? removeCol.disabled = true : removeCol.disabled = false;
-});
-
-removeCol.addEventListener('click', (e) => {
-  numOfCols--;
-  removeCol.disabled = false;
-
-  const allRows = [...document.querySelectorAll('tr')];
-
-  allRows.forEach(row => {
-    row.removeChild(row.firstElementChild);
-  });
-
-  table.prepend(...allRows);
-
   numOfCols === 10 ? addCol.disabled = true : addCol.disabled = false;
   numOfCols === 2 ? removeCol.disabled = true : removeCol.disabled = false;
 });
