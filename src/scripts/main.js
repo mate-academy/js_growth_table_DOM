b0a0abd74484e33bda8d67c19b0218f679187e2a
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-09-02 09:54:00 +0300

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,77 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const tableBody = table.querySelector('tbody');
+
+const btnAppendRow = document.querySelector('.append-row');
+const btnRemoveRow = document.querySelector('.remove-row');
+const btnAppendColummn = document.querySelector('.append-column');
+const btnRemoveColummn = document.querySelector('.remove-column');
+
+btnAppendRow.addEventListener('click', () => {
+  if (tableBody.rows.length < 10) {
+    const newRow = document.createElement('tr');
+
+    for (let i = 0; i < tableBody.rows[0].cells.length; i++) {
+      const newCell = document.createElement('td');
+
+      newRow.insertAdjacentElement('beforeend', newCell);
+    }
+
+    tableBody.insertAdjacentElement('beforeend', newRow);
+  }
+
+  if (tableBody.rows.length === 10) {
+    btnAppendRow.setAttribute('disabled', 'disabled');
+  }
+
+  if (tableBody.rows.length > 2) {
+    btnRemoveRow.removeAttribute('disabled');
+  }
+});
+
+btnRemoveRow.addEventListener('click', () => {
+  if (tableBody.rows.length > 2) {
+    tableBody.lastElementChild.remove();
+  }
+
+  if (tableBody.rows.length === 2) {
+    btnRemoveRow.setAttribute('disabled', 'disabled');
+  }
+
+  if (tableBody.rows.length < 10) {
+    btnAppendRow.removeAttribute('disabled');
+  }
+});
+
+btnAppendColummn.addEventListener('click', () => {
+  if (tableBody.rows[0].cells.length < 10) {
+    [...tableBody.rows].forEach(row => {
+      row.insertAdjacentHTML('beforeend', `<td></td>`);
+    });
+  }
+
+  if (tableBody.rows[0].cells.length === 10) {
+    btnAppendColummn.setAttribute('disabled', 'disabled');
+  }
+
+  if (tableBody.rows[0].cells.length > 2) {
+    btnRemoveColummn.removeAttribute('disabled');
+  }
+});
+
+btnRemoveColummn.addEventListener('click', () => {
+  if (tableBody.rows[0].cells.length > 2) {
+    [...tableBody.rows].forEach(row => {
+      row.lastElementChild.remove();
+    });
+  }
+
+  if (tableBody.rows[0].cells.length === 2) {
+    btnRemoveColummn.setAttribute('disabled', 'disabled');
+  }
+
+  if (tableBody.rows[0].cells.length < 10) {
+    btnAppendColummn.removeAttribute('disabled');
+  }
+});
