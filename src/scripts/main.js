35852549b846cfa7e40c0012f9864d2d86aebf94
Olga Bordiug <obordiug.gmail.com>
2021-09-02 13:28:43 +0300

    solution after comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,57 +1,57 @@
 'use strict';
 
-const tbody = document.querySelector('tbody');
+const container = document.querySelector('.container');
 const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
+const tbody = document.querySelector('tbody');
+const maxCount = 10;
+const minCount = 2;
 
-appendRow.onclick = () => {
-  if (tbody.querySelectorAll('tr').length < 10) {
-    tbody.append(tbody.lastElementChild.cloneNode(true));
-    removeRow.disabled = false;
+container.addEventListener('click', (e) => {
+  const button = e.target.closest('.button');
+
+  if (!button || !container.contains(button)) {
+    return;
   };
 
-  if (tbody.querySelectorAll('tr').length === 10) {
-    appendRow.disabled = true;
+  switch (button) {
+    case appendRow:
+      tbody.append(tbody.lastElementChild.cloneNode(true));
+      break;
+    case removeRow:
+      tbody.lastElementChild.remove();
+      break;
+    case appendColumn:
+      [ ...tbody.querySelectorAll('tr') ].forEach(tr => {
+        tr.append(tr.lastElementChild.cloneNode(true));
+      });
+      break;
+    case removeColumn:
+      [ ...tbody.querySelectorAll('tr') ].forEach(tr => {
+        tr.lastElementChild.remove();
+      });
+      break;
+    default:
+      break;
   }
-};
 
-removeRow.onclick = () => {
-  if (tbody.querySelectorAll('tr').length > 2) {
-    tbody.lastElementChild.remove();
-    appendRow.disabled = false;
-  }
+  tbody.querySelectorAll('tr').length < maxCount
+    ? appendRow.disabled = false
+    : appendRow.disabled = true;
 
-  if (tbody.querySelectorAll('tr').length === 2) {
-    removeRow.disabled = true;
-  }
-};
+  tbody.querySelectorAll('tr').length > minCount
+    ? removeRow.disabled = false
+    : removeRow.disabled = true;
 
-appendColumn.onclick = () => {
-  [ ...tbody.querySelectorAll('tr') ].forEach(tr => {
-    if (tr.querySelectorAll('td').length < 10) {
-      tr.append(tr.lastElementChild.cloneNode(true));
-    }
-    removeColumn.disabled = false;
-  });
+  [ ...tbody.querySelectorAll('tr') ][0].querySelectorAll('td')
+    .length < maxCount
+    ? appendColumn.disabled = false
+    : appendColumn.disabled = true;
 
-  if ([ ...tbody.querySelectorAll('tr') ][0].querySelectorAll('td')
-    .length === 10) {
-    appendColumn.disabled = true;
-  }
-};
-
-removeColumn.onclick = () => {
-  [ ...tbody.querySelectorAll('tr') ].forEach(tr => {
-    if (tr.querySelectorAll('td').length > 2) {
-      tr.lastElementChild.remove();
-    }
-    appendColumn.disabled = false;
-  });
-
-  if ([ ...tbody.querySelectorAll('tr') ][0].querySelectorAll('td')
-    .length === 2) {
-    removeColumn.disabled = true;
-  }
-};
+  [ ...tbody.querySelectorAll('tr') ][0].querySelectorAll('td')
+    .length > minCount
+    ? removeColumn.disabled = false
+    : removeColumn.disabled = true;
+});
