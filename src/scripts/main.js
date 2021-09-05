5596b449269a5801dee6d1988b70054a19effdd1
Roman Minchenko <minchenko.poma@gmail.com>
2021-09-05 12:35:22 +0300

    fixed-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,51 +12,24 @@ document.body.addEventListener('click', (e) => {
   switch (item) {
     case appendRow:
       field.append([...field.children][0].cloneNode(true));
-
-      if ([...field.children].length > 9) {
-        appendRow.setAttribute('disabled', '');
-      };
-
-      if ([...field.children].length >= 3) {
-        removeRow.removeAttribute('disabled');
-      };
       break;
     case removeRow:
       [...field.children][0].remove();
-
-      if ([...field.children].length <= 9) {
-        appendRow.removeAttribute('disabled');
-      };
-
-      if ([...field.children].length < 3) {
-        removeRow.setAttribute('disabled', '');
-      };
       break;
     case appendColumn:
       [...field.children].forEach(items => {
         items.append([...field.children][0].children[0].cloneNode(true));
-
-        if ([...items.children].length > 9) {
-          appendColumn.setAttribute('disabled', '');
-        };
-
-        if ([...items.children].length >= 3) {
-          removeColumn.removeAttribute('disabled');
-        };
       });
       break;
     case removeColumn:
       [...field.children].forEach(items => {
         [...items.children][0].remove();
-
-        if ([...items.children].length <= 9) {
-          appendColumn.removeAttribute('disabled');
-        };
-
-        if ([...items.children].length < 3) {
-          removeColumn.setAttribute('disabled', '');
-        };
       });
       break;
   };
+
+  appendRow.disabled = [...field.children].length > 9;
+  removeRow.disabled = [...field.children].length < 3;
+  appendColumn.disabled = [...field.children[0].children].length < 3;
+  removeColumn.disabled = [...field.children[0].children].length > 9;
 });
