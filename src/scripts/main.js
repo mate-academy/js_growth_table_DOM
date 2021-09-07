77be96e1f651f2bc1e8a2d62af2828bca8c0a22e
Alex <alex.krynytskyi@gmail.com>
2021-09-07 18:15:50 +0300

    added btn status

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,25 +6,26 @@ const removeColumnBtn = document.querySelector('.remove-column');
 const removeRowsBtn = document.querySelector('.remove-row');
 const addColumnsBtn = document.querySelector('.append-column');
 
-addRowsBtn.addEventListener('click', addRow);
+addRowsBtn.addEventListener('click', () => {
+  const newRow = table.rows[0].cloneNode(true);
 
-function addRow() {
-  if (table.rows.length < 10) {
-    const newRow = document.createElement('tr');
+  removeRowsBtn.removeAttribute('disabled');
 
-    for (let i = 0; i < table.rows[0].cells.length; i++) {
-      const td = document.createElement('td');
-
-      newRow.append(td);
-    };
-
+  if (table.rows.length < 9) {
     table.append(newRow);
+  } else {
+    table.append(newRow);
+    addRowsBtn.setAttribute('disabled', true);
   }
-}
+});
 
 removeRowsBtn.addEventListener('click', () => {
-  if (table.rows.length > 2) {
+  if (table.rows.length > 3) {
     table.querySelector('tr:last-child').remove();
+    addRowsBtn.removeAttribute('disabled');
+  } else {
+    table.querySelector('tr:last-child').remove();
+    removeRowsBtn.setAttribute('disabled', true);
   }
 });
 
@@ -34,7 +35,11 @@ addColumnsBtn.addEventListener('click', () => {
   for (let i = 0; i < rows.length; i++) {
     const column = document.createElement('td');
 
-    if (rows[i].cells.length < 10) {
+    if (rows[i].cells.length === 9) {
+      rows[i].append(column);
+      addColumnsBtn.setAttribute('disabled', true);
+    } else {
+      removeColumnBtn.removeAttribute('disabled');
       rows[i].append(column);
     }
   }
@@ -44,8 +49,12 @@ removeColumnBtn.addEventListener('click', () => {
   const rows = document.querySelectorAll('tr');
 
   for (let j = 0; j < table.rows.length; j++) {
-    if (rows[j].cells.length > 2) {
+    if (rows[j].cells.length > 3) {
       rows[j].querySelector('td:last-child').remove();
+      addColumnsBtn.removeAttribute('disabled');
+    } else {
+      rows[j].querySelector('td:last-child').remove();
+      removeColumnBtn.setAttribute('disabled', true);
     }
   }
 });
