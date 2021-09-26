da82d387b5d1250bf73ac9edc1ab3523cf12c3a8
Viktormaz <vitecc91@gmail.com>
2021-09-26 20:16:07 +0300

    one more new best solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,68 +11,58 @@ let removeColumnCount = 0;
 const min = 2;
 const max = 10;
 
-appendRow.addEventListener('click', (el) => {
-  if (table.rows.length < max) {
-    const newRow = table.rows[0].cloneNode(true);
-
-    table.append(newRow);
-  }
-});
-
-removeRow.addEventListener('click', (el) => {
-  if (table.rows.length > 2) {
-    table.deleteRow(0);
-  }
-});
-
-appendColumn.addEventListener('click', (el) => {
-  for (const row of table.rows) {
-    appendColumnCount = row.cells.length;
-  }
-
-  if (appendColumnCount < max) {
-    for (const row of table.rows) {
-      row.insertBefore(
-        row.children[0].cloneNode(true),
-        row.children[table.rows[0].cells.length - 1]);
-    }
-  }
-
-  appendColumnCount++;
-});
-
-removeColumn.addEventListener('click', (el) => {
-  for (const row of table.rows) {
-    removeColumnCount = row.cells.length;
-  }
-
-  if (removeColumnCount > min) {
-    for (const row of table.rows) {
-      row.deleteCell(0);
-    }
-  }
-
-  removeColumnCount--;
-});
-
 document.addEventListener('click', (ev) => {
   switch (ev.target) {
     case appendRow:
+      if (table.rows.length < max) {
+        const newRow = table.rows[0].cloneNode(true);
+
+        table.append(newRow);
+      }
+
       appendRow.disabled = table.rows.length === max;
       removeRow.disabled = false;
       break;
 
     case removeRow:
+      if (table.rows.length > 2) {
+        table.deleteRow(0);
+      }
+
       removeRow.disabled = table.rows.length === min;
       appendRow.disabled = false;
       break;
 
     case appendColumn:
+      for (const row of table.rows) {
+        appendColumnCount = row.cells.length;
+      }
+
+      if (appendColumnCount < max) {
+        for (const row of table.rows) {
+          row.insertBefore(
+            row.children[0].cloneNode(true),
+            row.children[table.rows[0].cells.length - 1]);
+        }
+      }
+
+      appendColumnCount++;
       appendColumn.disabled = appendColumnCount === max;
       removeColumn.disabled = false;
       break;
 
     case removeColumn:
+      for (const row of table.rows) {
+        removeColumnCount = row.cells.length;
+      }
+
+      if (removeColumnCount > min) {
+        for (const row of table.rows) {
+          row.deleteCell(0);
+        }
+      }
+
+      removeColumnCount--;
       removeColumn.disabled = removeColumnCount === min;
       appendColumn.disabled = false;
       break;
