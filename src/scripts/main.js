77a1ad0713e0ae7b1c59ca48d5af7ab098487870
Aleksandr Maltsev <0po4ta@gmail.com>
2021-10-26 20:59:44 +0300

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,59 +2,36 @@
 
 const addRow = document.querySelector('.append-row');
 const delRow = document.querySelector('.remove-row');
-const addCol = document.querySelector('.append-column');
-const delCol = document.querySelector('.remove-column');
+const addColumn = document.querySelector('.append-column');
+const delColumn = document.querySelector('.remove-column');
+const container = document.querySelector('.container');
+const table = document.querySelector('tbody');
 
-const table = document.querySelector('.field tbody');
+container.addEventListener('click', (e) => {
+  switch (e.target.classList[0]) {
+    case 'append-column':
+      [...table.rows].forEach(row => {
+        row.append(table.rows[0].children[0].cloneNode(true));
+      });
+      break;
 
-addRow.addEventListener('click', () => {
-  table.append(table.firstElementChild.cloneNode(true));
+    case 'append-row':
+      table.append(table.firstElementChild.cloneNode(true));
+      break;
 
-  if (table.children.length === 10) {
-    addRow.disabled = true;
+    case 'remove-row':
+      table.lastElementChild.remove();
+      break;
+
+    case 'remove-column':
+      [...table.rows].forEach(row => row.lastElementChild.remove());
+      break;
+    default:
+      break;
   }
 
-  if (table.children.length > 2) {
-    delRow.disabled = false;
-  }
-});
-
-delRow.addEventListener('click', () => {
-  table.lastElementChild.remove();
-
-  if (table.children.length === 2) {
-    delRow.disabled = true;
-  }
-
-  if (table.children.length < 10) {
-    addRow.disabled = false;
-  }
-
-  addCol.addEventListener('click', () => {
-    for (const row of table.children) {
-      row.append(row.lastElementChild.cloneNode(true));
-    }
-
-    if (table.firstElementChild.children.length === 10) {
-      addCol.disabled = true;
-    }
-
-    if (table.firstElementChild.children.length > 2) {
-      delCol.disabled = false;
-    }
-  });
-
-  delCol.addEventListener('click', () => {
-    for (const row of table.children) {
-      row.lastElementChild.remove();
-    };
-
-    if (table.firstElementChild.children.length === 2) {
-      delCol.disabled = true;
-    }
-
-    if (table.firstElementChild.children.length < 10) {
-      addCol.disabled = false;
-    }
-  });
+  addColumn.disabled = table.rows[0].children.length >= 10;
+  addRow.disabled = table.rows.length >= 10;
+  delRow.disabled = table.rows.length <= 2;
+  delColumn.disabled = table.rows[0].children.length <= 2;
 });
