a7e58b14c1409bb7486898ffbb11e678ddcc1886
Mariia Kovach <maria_kovach@ukr.net>
2021-11-03 23:33:29 +0200

    changes applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,52 +1,40 @@
 'use strict';
 
 const table = document.querySelector('tbody');
+const container = document.querySelector('.container');
 
 const appendRowBtn = document.querySelector('.append-row');
 const removeRowBtn = document.querySelector('.remove-row');
 const appendColBtn = document.querySelector('.append-column');
 const removeColBtn = document.querySelector('.remove-column');
 
-appendRowBtn.addEventListener('click', () => {
-  removeRowBtn.disabled = false;
+container.addEventListener('click', e => {
+  const button = e.target;
 
-  table.append(table.rows[0].cloneNode(true));
+  switch (button) {
+    case appendRowBtn:
+      table.append(table.rows[0].cloneNode(true));
+      break;
 
-  if (table.rows.length >= 10) {
-    appendRowBtn.disabled = true;
+    case removeRowBtn:
+      table.rows[table.rows.length - 1].remove();
+      break;
+
+    case appendColBtn:
+      [...table.rows].forEach(row => {
+        row.append(row.cells[0].cloneNode(true));
+      });
+      break;
+
+    case removeColBtn:
+      [...table.rows].forEach(row => {
+        row.lastChild.remove();
+      });
+      break;
   }
-});
 
-removeRowBtn.addEventListener('click', () => {
-  appendRowBtn.disabled = false;
-
-  table.rows[table.rows.length - 1].remove();
-
-  if (table.rows.length <= 2) {
-    removeRowBtn.disabled = true;
-  }
-});
-
-appendColBtn.addEventListener('click', () => {
-  removeColBtn.disabled = false;
-
-  [...table.rows].forEach(row => {
-    row.append(row.cells[0].cloneNode(true));
-  });
-
-  if (table.rows[0].children.length >= 10) {
-    appendColBtn.disabled = true;
-  }
-});
-
-removeColBtn.addEventListener('click', () => {
-  appendColBtn.disabled = false;
-
-  [...table.rows].forEach(row => {
-    row.lastChild.remove();
-  });
-
-  if (table.rows[0].children.length <= 2) {
-    removeColBtn.disabled = true;
-  }
+  appendRowBtn.disabled = table.rows.length >= 10;
+  removeRowBtn.disabled = table.rows.length <= 2;
+  appendColBtn.disabled = table.rows[0].children.length >= 10;
+  removeColBtn.disabled = table.rows[0].children.length <= 2;
 });
