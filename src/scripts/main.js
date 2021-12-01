76d73e28e1e563149ce6b93e33087ccaae41228f
Andriy Shevtsiv <andriy.shevtsiv@gmail.com>
2021-12-01 14:37:40 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,71 @@
 'use strict';
 
 // write code here
+
+const container = document.querySelector('div');
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+container.addEventListener('click', (e) => {
+  const clickPoint = e.target.closest('button');
+
+  if (!clickPoint) {
+    return;
+  }
+
+  const table = document.querySelector('table');
+
+  if (e.target === appendRowButton
+  && appendRowButton.disabled === false) {
+    const rowCopy = table.rows[0];
+
+    table.insertAdjacentHTML('beforeend', `${rowCopy.outerHTML}`);
+
+    if (table.rows.length === 10) {
+      appendRowButton.disabled = true;
+    }
+  }
+
+  if (e.target === removeRowButton
+  && removeRowButton.disabled === false) {
+    const lastRow = table.rows[table.rows.length - 1];
+
+    lastRow.remove();
+
+    if (table.rows.length === 2) {
+      removeRowButton.disabled = true;
+    }
+  }
+
+  if (e.target === appendColumnButton
+  && appendColumnButton.disabled === false) {
+    for (let i = 0; i < table.rows.length; i++) {
+      const currentRow = table.rows[i];
+      const cellOfNewColumn = currentRow.cells[0];
+
+      currentRow.insertAdjacentHTML('beforeend', `${
+        cellOfNewColumn.outerHTML
+      }`);
+    }
+
+    if (table.rows[0].cells.length === 10) {
+      appendColumnButton.disabled = true;
+    }
+  }
+
+  if (e.target === removeColumnButton
+  && removeColumnButton.disabled === false) {
+    for (let i = 0; i < table.rows.length; i++) {
+      const currentRow = table.rows[i];
+      const cellOfLastColumn = currentRow.cells[currentRow.cells.length - 1];
+
+      cellOfLastColumn.remove();
+    }
+
+    if (table.rows[0].cells.length === 2) {
+      removeColumnButton.disabled = true;
+    }
+  }
+});
