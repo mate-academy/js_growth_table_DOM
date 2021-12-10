df05c9fdd70a8d0f2af1b914ea5acb9603b5019e
FocusHuK <kirill919191kirill@gmail.com>
2021-12-10 14:57:00 +0200

    Edited2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,93 +1,97 @@
 'use strict';
 
 // write code here
+const div = document.querySelector('.container');
 const table = document.querySelector('.field');
-const appendRow = document.querySelector('.append-row');
-const removeRow = document.querySelector('.remove-row');
-const appendColumn = document.querySelector('.append-column');
-const removeColumn = document.querySelector('.remove-column');
 const buttonAR = document.querySelector('.append-row');
 const buttonRR = document.querySelector('.remove-row');
 const buttonAC = document.querySelector('.append-column');
 const buttonRC = document.querySelector('.remove-column');
 
-const column = (events) => {
-  const newColumn = table.rows;
-  const limitColumn = table.rows[0].cells.length + 1;
+div.addEventListener('click', (events) => {
+  const buttonClick = events.target;
 
-  if (limitColumn <= 10) {
-    for (let i = 0, l = newColumn.length; i < l; i++) {
-      newColumn[i].insertCell(-1);
+  if (buttonClick.className === 'append-row button') {
+    const colsLength = table.rows[0].cells.length;
+    const limitRow = table.rows.length;
+
+    if (limitRow <= 10) {
+      const newRow = table.insertRow(-1);
+
+      for (let i = 0; i < colsLength; i++) {
+        newRow.insertCell(-1);
+      }
+
+      if (limitRow === 10) {
+        buttonClick.classList.add('disabled');
+        buttonClick.style.background = 'grey';
+      }
+
+      if (limitRow > 0 && limitRow !== 10) {
+        buttonRR.style.background = '#ffac00';
+        buttonRR.classList.remove('disabled');
+      }
     }
+  };
 
-    if (limitColumn === 10) {
-      events.target.style.background = 'grey';
+  if (buttonClick.className === 'remove-row button') {
+    const oldRow = table.rows.length;
+    const limitRow = table.rows.length - 1;
+
+    if (limitRow >= 2) {
+      table.deleteRow(oldRow - 1);
+
+      if (limitRow === 2) {
+        buttonClick.classList.add('disabled');
+        buttonClick.style.background = 'grey';
+      }
+
+      if (limitRow < 12) {
+        buttonAR.style.background = '#ffac00';
+        buttonAR.classList.remove('disabled');
+      }
     }
+  };
 
-    if (limitColumn > 2 && limitColumn !== 10) {
-      buttonRC.style.background = '#ffac00';
-    }
-  }
-};
+  if (buttonClick.className === 'append-column button') {
+    const newColumn = table.rows;
+    const limitColumn = table.rows[0].cells.length + 1;
 
-const delColumn = (events) => {
-  const oldColumn = table.rows;
-  const limitColumn = table.rows[0].cells.length - 1;
+    if (limitColumn <= 10) {
+      for (let i = 0, l = newColumn.length; i < l; i++) {
+        newColumn[i].insertCell(-1);
+      }
 
-  if (limitColumn >= 2) {
-    for (let i = 0; i < oldColumn.length; i++) {
-      oldColumn[i].deleteCell(-1);
-    }
+      if (limitColumn === 10) {
+        buttonClick.classList.add('disabled');
+        buttonClick.style.background = 'grey';
+      }
 
-    if (limitColumn === 2) {
-      events.target.style.background = 'grey';
-    }
-
-    if (limitColumn < 10) {
-      buttonAC.style.background = '#ffac00';
-    }
-  }
-};
-
-const row = (events) => {
-  const colsLength = table.rows[0].cells.length;
-  const limitRow = table.rows.length;
-
-  if (limitRow <= 10) {
-    const newRow = table.insertRow(-1);
-
-    for (let i = 0; i < colsLength; i++) {
-      newRow.insertCell(-1);
-    }
-
-    if (limitRow === 10) {
-      events.target.style.background = 'grey';
-    }
-
-    if (limitRow > 0 && limitRow !== 10) {
-      buttonRR.style.background = '#ffac00';
-    }
-  }
-};
-
-const delRow = (events) => {
-  const oldRow = table.rows.length;
-  const limitRow = table.rows.length - 1;
-
-  if (limitRow >= 2) {
-    table.deleteRow(oldRow - 1);
-
-    if (limitRow === 2) {
-      events.target.style.background = 'grey';
-    }
-
-    if (limitRow < 12) {
-      buttonAR.style.background = '#ffac00';
+      if (limitColumn > 2 && limitColumn !== 10) {
+        buttonRC.style.background = '#ffac00';
+        buttonRC.classList.remove('disabled');
+      }
     }
   }
-};
 
-appendColumn.addEventListener('click', column);
-removeColumn.addEventListener('click', delColumn);
-appendRow.addEventListener('click', row);
-removeRow.addEventListener('click', delRow);
+  if (buttonClick.className === 'remove-column button') {
+    const oldColumn = table.rows;
+    const limitColumn = table.rows[0].cells.length - 1;
+
+    if (limitColumn >= 2) {
+      for (let i = 0; i < oldColumn.length; i++) {
+        oldColumn[i].deleteCell(-1);
+      }
+
+      if (limitColumn === 2) {
+        buttonClick.classList.add('disabled');
+        buttonClick.style.background = 'grey';
+      }
+
+      if (limitColumn < 10) {
+        buttonAC.style.background = '#ffac00';
+        buttonAC.classList.remove('disabled');
+      }
+    }
+  }
+});
