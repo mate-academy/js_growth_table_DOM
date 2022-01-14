a439a8a777c2734dd05f57a0bc4c970bb0d209b8
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2022-01-14 17:42:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,78 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+const addRow = document.querySelector('.append-row');
+const remRow = document.querySelector('.remove-row');
+const addCol = document.querySelector('.append-column');
+const remCol = document.querySelector('.remove-column');
+
+const appendRow = () => {
+  const rowCount = table.children.length;
+
+  if (rowCount < 10) {
+    const cloneRow = table.firstElementChild.cloneNode(true);
+
+    table.append(cloneRow);
+  }
+
+  if (rowCount >= 10) {
+    addRow.disabled = true;
+  } else {
+    remRow.disabled = null;
+  }
+};
+
+const deleteRow = () => {
+  const rowCount = table.children.length;
+
+  if (rowCount > 2) {
+    table.lastElementChild.remove();
+  }
+
+  if (rowCount <= 2) {
+    remRow.disabled = true;
+  } else {
+    addRow.disabled = null;
+  }
+};
+
+const appendCol = () => {
+  const colCount = table.firstElementChild.children.length;
+  const rowCount = table.children.length;
+
+  if (colCount < 10) {
+    for (let i = 0; i < rowCount; i++) {
+      const cell = document.createElement('td');
+
+      table.children[i].append(cell);
+    }
+  }
+
+  if (colCount >= 10) {
+    addCol.disabled = true;
+  } else {
+    remCol.disabled = null;
+  }
+};
+
+const deleteCol = () => {
+  const colCount = table.firstElementChild.children.length;
+  const rowCount = table.children.length;
+
+  if (colCount > 2) {
+    for (let i = 0; i < rowCount; i++) {
+      table.children[i].lastElementChild.remove();
+    }
+  }
+
+  if (colCount <= 2) {
+    remCol.disabled = true;
+  } else {
+    addCol.disabled = null;
+  }
+};
+
+addRow.addEventListener('click', appendRow);
+remRow.addEventListener('click', deleteRow);
+addCol.addEventListener('click', appendCol);
+remCol.addEventListener('click', deleteCol);
