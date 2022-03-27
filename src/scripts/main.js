2551d43f77962684ec77dd2fdb31cc88ba6330b8
Mykhailo Ianush <yanushmisha@gmail.com>
2022-03-27 12:39:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,89 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field > tbody');
+const addRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const addColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+let currentrows = table.children.length;
+let currentcolumns = table.children[0].children.length;
+
+addRow.addEventListener('click', e => {
+  currentrows++;
+  removeRow.disabled = false;
+
+  if (currentrows === 10) {
+    e.target.disabled = true;
+  }
+
+  table.innerHTML = '';
+
+  table.insertAdjacentHTML(
+    'beforeend',
+    buildTable(currentrows, currentcolumns),
+  );
+});
+
+removeRow.addEventListener('click', e => {
+  currentrows--;
+  addRow.disabled = false;
+
+  if (currentrows === 2) {
+    e.target.disabled = true;
+  }
+
+  table.innerHTML = '';
+
+  table.insertAdjacentHTML(
+    'beforeend',
+    buildTable(currentrows, currentcolumns),
+  );
+});
+
+addColumn.addEventListener('click', e => {
+  currentcolumns++;
+  removeColumn.disabled = false;
+
+  if (currentcolumns === 10) {
+    e.target.disabled = true;
+  }
+
+  table.innerHTML = '';
+
+  table.insertAdjacentHTML(
+    'beforeend',
+    buildTable(currentrows, currentcolumns),
+  );
+});
+
+removeColumn.addEventListener('click', e => {
+  currentcolumns--;
+  addColumn.disabled = false;
+
+  if (currentcolumns === 2) {
+    e.target.disabled = true;
+  }
+
+  table.innerHTML = '';
+
+  table.insertAdjacentHTML(
+    'beforeend',
+    buildTable(currentrows, currentcolumns),
+  );
+});
+
+function buildTable(rows, columns) {
+  let innerHtml = '';
+
+  for (let i = 0; i < rows; i++) {
+    innerHtml += '<tr>';
+
+    for (let j = 0; j < columns; j++) {
+      innerHtml += '<td></td>';
+    }
+
+    innerHtml += '</tr>';
+  }
+
+  return innerHtml;
+}
