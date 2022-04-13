3cfad434de2c1d794c7364e82887a2031e1e8001
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2022-04-13 21:01:21 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const tbody = document.querySelector('tbody');
+
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+container.addEventListener('click', e => {
+  const button = e.target.closest('button');
+
+  if (!button) {
+    return;
+  }
+
+  const rows = document.querySelectorAll('tr');
+  const singleRow = rows[0].cloneNode(true);
+  const [ firstClass ] = button.classList;
+  const maxLength = 10;
+  const minLength = 2;
+
+  let rowsLength = rows.length;
+
+  switch (firstClass) {
+    case 'append-row':
+      tbody.append(singleRow);
+      rowsLength++;
+      break;
+
+    case 'remove-row':
+      tbody.lastElementChild.remove();
+      rowsLength--;
+      break;
+
+    case 'append-column':
+      rows.forEach(
+        row => row.append(singleRow.lastElementChild.cloneNode(true)));
+      break;
+
+    case 'remove-column':
+      rows.forEach(
+        row => row.lastElementChild.remove());
+      break;
+
+    default:
+      break;
+  }
+
+  removeColumn.disabled = rows[0].childElementCount === minLength;
+  appendColumn.disabled = rows[0].childElementCount === maxLength;
+  removeRow.disabled = rowsLength === minLength;
+  appendRow.disabled = rowsLength === maxLength;
+});
