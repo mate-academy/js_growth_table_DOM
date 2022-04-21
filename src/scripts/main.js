0532fca2ddf2cb118ce27edde7c570e24b3ea371
Max Shylenko <maxshylenko@gmail.com>
2022-04-21 17:11:30 +0200

    growth table DOM is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,66 @@
 'use strict';
 
-// write code here
+const page = document.querySelector('.container');
+const table = document.querySelector('.field');
+const tBody = table.querySelector('tbody');
+const addRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const addColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+const startCount = 4;
+let countOfRows = startCount;
+let countOfColumns = startCount;
+
+page.addEventListener('click', e => {
+  const activeButton = e.target.matches('.button');
+
+  if (!activeButton) {
+    return;
+  }
+
+  const row = tBody.lastElementChild;
+  const rows = [...table.rows];
+
+  switch (e.target.classList[0]) {
+    case 'append-row':
+      tBody.append(row.cloneNode(true));
+      countOfRows++;
+      break;
+    case 'remove-row':
+      row.remove();
+      countOfRows--;
+      break;
+    case 'append-column':
+      rows.forEach(r => r.append(r.lastElementChild.cloneNode(true)));
+      countOfColumns++;
+      break;
+    case 'remove-column':
+      rows.forEach(r => r.lastElementChild.remove());
+      countOfColumns--;
+      break;
+  }
+
+  switch (!!countOfRows) {
+    case countOfRows === 10:
+      addRowButton.disabled = true;
+      break;
+    case countOfRows === 2:
+      removeRowButton.disabled = true;
+      break;
+    default:
+      addRowButton.disabled = null;
+      removeRowButton.disabled = null;
+  }
+
+  switch (!!countOfColumns) {
+    case countOfColumns === 10:
+      addColumnButton.disabled = true;
+      break;
+    case countOfColumns === 2:
+      removeColumnButton.disabled = true;
+      break;
+    default:
+      addColumnButton.disabled = null;
+      removeColumnButton.disabled = null;
+  }
+});
