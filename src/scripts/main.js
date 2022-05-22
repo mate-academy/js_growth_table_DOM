795320ac95ec66d1537264173d24d37d807b1ac1
Roman Nikitenko <Lomkaaa@ukr.net>
2022-05-22 21:18:12 +0300

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const appendRowPlus = document.querySelector('.append-row');
+const appendColumPlus = document.querySelector('.append-column');
+const removeColum = document.querySelector('.remove-column');
+const removeRow = document.querySelector('.remove-row');
+const tbody = document.querySelector('tbody');
+
+appendColumPlus.addEventListener('click', e => {
+  const trRow = document.querySelectorAll('tr');
+
+  trRow.forEach(el => {
+    const td = document.createElement('td');
+
+    el.append(td);
+  });
+});
+
+appendRowPlus.addEventListener('click', e => {
+  const trRow = document.querySelectorAll('tr');
+
+  tbody.append(trRow[trRow.length - 1].cloneNode(true));
+});
+
+removeColum.addEventListener('click', e => {
+  const trRow = document.querySelectorAll('tr');
+
+  trRow.forEach(el => {
+    const td = el.querySelector('td');
+
+    td.remove();
+  });
+});
+
+removeRow.addEventListener('click', e => {
+  const trRow = document.querySelector('tr');
+
+  trRow.remove();
+});
