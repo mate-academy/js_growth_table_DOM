5d11f1c5beda6eb203b7cfec0e4a93376c2c2d95
ShapovalDenys <denys.igorevich@gmail.com>
2022-06-10 11:49:36 +0300

    solution v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,39 +6,38 @@ const tbody = document.querySelector('tbody');
 container.addEventListener('click', (e) => action(e));
 
 function action(e) {
-  const typeActionButton = e.target.closest('button');
   const list = tbody.querySelectorAll('tr');
 
-  if (!typeActionButton) {
+  if (!e.target.closest('button')) {
     return;
   }
 
-  if (typeActionButton.matches('.append-row')) {
-    const tr = document.createElement('tr');
-    const trLength = tbody.lastElementChild.querySelectorAll('td').length;
+  switch (e.target) {
+    case container.querySelector('.append-row'):
+      const tr = document.createElement('tr');
+      const trLength = tbody.lastElementChild.querySelectorAll('td').length;
 
-    for (let i = 0; i < trLength; i++) {
+      for (let i = 0; i < trLength; i++) {
+        tr.insertAdjacentElement('beforeend', document.createElement('td'));
+      }
+
+      tbody.insertAdjacentElement('beforeend', tr);
+      break;
+
+    case container.querySelector('.remove-row'):
+      tbody.lastElementChild.remove();
+      break;
+
+    case container.querySelector('.append-column'):
       const td = document.createElement('td');
 
-      tr.insertAdjacentElement('beforeend', td);
-    }
+      list.forEach(el => el.append(td.cloneNode()));
+      break;
 
-    tbody.insertAdjacentElement('beforeend', tr);
-  };
-
-  if (typeActionButton.matches('.remove-row')) {
-    tbody.lastElementChild.remove();
-  };
-
-  if (typeActionButton.matches('.append-column')) {
-    const td = document.createElement('td');
-
-    list.forEach(el => el.append(td.cloneNode()));
-  };
-
-  if (typeActionButton.matches('.remove-column')) {
-    list.forEach(el => el.lastElementChild.remove());
-  };
+    case container.querySelector('.remove-column'):
+      list.forEach(el => el.lastElementChild.remove());
+      break;
+  }
 
   buttonDisabler();
 }
@@ -57,19 +56,8 @@ function buttonDisabler() {
   addColumnButton.disabled = '';
   removeColumnButton.disabled = '';
 
-  if (columnLength >= 10) {
-    addRowButton.disabled = 'true';
-  }
-
-  if (columnLength <= 2) {
-    removeRowButton.disabled = 'true';
-  }
-
-  if (rowLength >= 10) {
-    addColumnButton.disabled = 'true';
-  }
-
-  if (rowLength <= 2) {
-    removeColumnButton.disabled = 'true';
-  }
+  addRowButton.disabled = columnLength >= 10;
+  removeRowButton.disabled = columnLength <= 2;
+  addColumnButton.disabled = rowLength >= 10;
+  removeColumnButton.disabled = rowLength <= 2;
 }
