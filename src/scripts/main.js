535b8b9518be6ae4963b826ed75a6e40bf2c10c7
silchyn <ivan.silchin@gmail.com>
2022-07-07 19:35:52 +0300

    simplified checks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,28 +6,24 @@ const removeRow = document.querySelector('.remove-row');
 const removeColumn = document.querySelector('.remove-column');
 const tbody = document.querySelector('tbody');
 
+function checkDisability() {
+  appendRow.disabled = tbody.children.length >= 10;
+  removeRow.disabled = tbody.children.length <= 2;
+
+  appendColumn.disabled = tbody.children[0].children.length >= 10;
+  removeColumn.disabled = tbody.children[0].children.length <= 2;
+}
+
 appendRow.addEventListener('click', e => {
   tbody.append(tbody.children[0].cloneNode(true));
 
-  if (tbody.children.length === 10) {
-    appendRow.disabled = true;
-  }
-
-  if (tbody.children.length > 2) {
-    removeRow.disabled = false;
-  }
+  checkDisability();
 });
 
 removeRow.addEventListener('click', e => {
   tbody.lastElementChild.remove();
 
-  if (tbody.children.length === 2) {
-    removeRow.disabled = true;
-  }
-
-  if (tbody.children.length < 10) {
-    appendRow.disabled = false;
-  }
+  checkDisability();
 });
 
 appendColumn.addEventListener('click', e => {
@@ -35,13 +31,7 @@ appendColumn.addEventListener('click', e => {
     row.append(row.children[0].cloneNode(true));
   }
 
-  if (tbody.children[0].children.length === 10) {
-    appendColumn.disabled = true;
-  }
-
-  if (tbody.children[0].children.length > 2) {
-    removeColumn.disabled = false;
-  }
+  checkDisability();
 });
 
 removeColumn.addEventListener('click', e => {
@@ -49,11 +39,5 @@ removeColumn.addEventListener('click', e => {
     row.lastElementChild.remove();
   }
 
-  if (tbody.children[0].children.length === 2) {
-    removeColumn.disabled = true;
-  }
-
-  if (tbody.children[0].children.length < 10) {
-    appendColumn.disabled = false;
-  }
+  checkDisability();
 });
