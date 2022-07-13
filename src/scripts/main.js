2ee6c40341b1c37ac80ba772194e562e9b841c92
fledith <barateon023@gmail.com>
2022-07-13 13:38:40 +0300

    Soluiton

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,62 @@
 'use strict';
 
-// write code here
+const buttons = document.querySelectorAll('.button');
+const rows = document.querySelector('.field').firstElementChild;
+
+function isEnough() {
+  for (const button of buttons) {
+    if (button.classList.contains('append-row')) {
+      button.disabled = rows.children.length >= 10;
+    }
+
+    if (button.classList.contains('remove-row')) {
+      button.disabled = rows.children.length <= 2;
+    }
+
+    if (button.classList.contains('append-column')) {
+      button.disabled = rows.children[0].children.length >= 10;
+    }
+
+    if (button.classList.contains('remove-column')) {
+      button.disabled = rows.children[0].children.length <= 2;
+    }
+  }
+}
+
+for (const button of buttons) {
+  if (button.classList.contains('append-row')) {
+    button.addEventListener('click', (e) => {
+      rows.append(rows.children[0].cloneNode(true));
+
+      isEnough();
+    });
+  }
+
+  if (button.classList.contains('remove-row')) {
+    button.addEventListener('click', (e) => {
+      rows.removeChild(rows.children[0]);
+
+      isEnough();
+    });
+  }
+
+  if (button.classList.contains('append-column')) {
+    button.addEventListener('click', (e) => {
+      for (const row of rows.children) {
+        row.append(row.children[0].cloneNode(true));
+      }
+
+      isEnough();
+    });
+  }
+
+  if (button.classList.contains('remove-column')) {
+    button.addEventListener('click', (e) => {
+      for (const row of rows.children) {
+        row.removeChild(row.children[0]);
+      }
+
+      isEnough();
+    });
+  }
+}
