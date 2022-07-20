bacc18c5fac605e6e80dfdb452fd3fd88333801d
Sergii Maniurov <maniurov_sv@ukr.net>
2022-07-20 14:13:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const maxRows = 10;
+const maxColumns = 10;
+const minRows = 2;
+const minColumns = 2;
+
+const table = document.querySelector('.field');
+const events = [
+  document.querySelector('.append-row'),
+  document.querySelector('.remove-row'),
+  document.querySelector('.append-column'),
+  document.querySelector('.remove-column')];
+const actions = [
+  () => table.append(table.rows[0].cloneNode(true)),
+  () => table.rows[0].remove(),
+  () => [...table.rows].forEach(e => e.append(e.children[0].cloneNode(true))),
+  () => [...table.rows].forEach(e => e.children[0].remove())];
+const disable = [
+  () => !(table.rows.length < maxRows),
+  () => !(table.rows.length > minRows),
+  () => !(table.rows[0].children.length < maxColumns),
+  () => !(table.rows[0].children.length > minColumns)];
+
+document.querySelector('.container').addEventListener('click', eventClick => {
+  actions[events.indexOf(eventClick.target)]();
+  events.forEach((e, index) => (e.disabled = disable[index]()));
+});
