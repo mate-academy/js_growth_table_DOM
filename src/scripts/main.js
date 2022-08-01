450aacaf54aacbdbebdc93b795d47a31787979f5
Bohdana Usenko <usenkobogdana19@gmail.com>
2022-08-01 16:46:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,61 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const tbody = document.querySelector('tbody');
+const rows = tbody.rows;
+
+const appendRow = document.querySelector(`.append-row`);
+const removeRow = document.querySelector(`.remove-row`);
+const appendColumn = document.querySelector(`.append-column`);
+const removeColumn = document.querySelector(`.remove-column`);
+
+const max = 9;
+const min = 2;
+
+container.addEventListener('click', (eventObj) => {
+  const target = eventObj.target;
+
+  let numberOfRows = rows.length;
+  let numberOfColums = rows[0].children.length;
+
+  switch (target) {
+    case appendRow:
+      const newEl = document.createElement('tr');
+
+      tbody.append(newEl);
+
+      for (let i = 0; i < rows[0].children.length; i++) {
+        newEl.insertAdjacentHTML('beforeend', '<td></td>');
+      }
+
+      numberOfRows++;
+      break;
+
+    case appendColumn:
+      for (const cell of rows) {
+        cell.insertAdjacentHTML('beforeend', '<td></td>');
+      }
+
+      numberOfColums++;
+      break;
+
+    case removeRow:
+      tbody.deleteRow(tbody.lastElementChild);
+
+      numberOfRows--;
+      break;
+
+    case removeColumn:
+      for (let i = 0; i < rows.length; i++) {
+        rows[i].deleteCell(rows[i].children[0]);
+      }
+
+      numberOfColums--;
+      break;
+  }
+
+  appendRow.disabled = numberOfRows > max;
+  appendColumn.disabled = numberOfColums > max;
+  removeRow.disabled = numberOfRows <= min;
+  removeColumn.disabled = numberOfColums <= min;
+});
