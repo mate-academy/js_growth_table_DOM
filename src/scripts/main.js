a34d352fe1d9c3a76ff263dfb5d60f6e9b6e231f
Aleksei Evtushenko <hlauhalash@gmail.com>
2022-08-20 12:58:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendCol = document.querySelector('.append-column');
+const removeCol = document.querySelector('.remove-column');
+
+appendRow.addEventListener('click', (e) => {
+  const listTr = document.querySelectorAll('tr');
+
+  if (listTr.length < 10) {
+    const tr = listTr[0].outerHTML;
+
+    listTr[0].insertAdjacentHTML('beforebegin', tr);
+    removeRow.disabled = false;
+    e.target.disabled = listTr.length === 9;
+  }
+});
+
+removeRow.addEventListener('click', (e) => {
+  const listTr = document.querySelectorAll('tr');
+
+  if (listTr.length > 2) {
+    listTr[0].remove();
+    appendRow.disabled = false;
+    e.target.disabled = listTr.length === 3;
+  }
+});
+
+appendCol.addEventListener('click', (e) => {
+  const listTr = document.querySelectorAll('tr');
+
+  if (listTr[0].children.length < 10) {
+    for (const item of listTr) {
+      const tr = item.children[0].outerHTML;
+
+      item.insertAdjacentHTML('beforeend', tr);
+    }
+
+    removeCol.disabled = false;
+    e.target.disabled = listTr[0].children.length === 10;
+  }
+});
+
+removeCol.addEventListener('click', (e) => {
+  const listTr = document.querySelectorAll('tr');
+
+  if (listTr[0].children.length > 2) {
+    for (const item of listTr) {
+      item.children[0].outerHTML = '';
+    }
+    appendCol.disabled = false;
+    e.target.disabled = listTr[0].children.length === 2;
+  }
+});
