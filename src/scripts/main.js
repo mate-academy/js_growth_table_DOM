80986360e7dce8834b0be536172b9c0a5c5a08a0
Inna <dragotaiv@gmail.com>
2022-09-20 22:47:26 +0300

    second commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,25 +6,27 @@ const appendRowButton = document.querySelector('.append-row');
 const removeRowButton = document.querySelector('.remove-row');
 const appendColumnButton = document.querySelector('.append-column');
 const removeColumnButton = document.querySelector('.remove-column');
+const max = 10;
+const min = 2;
 
 appendRowButton.addEventListener('click', function() {
-  if (tbody.children.length < 10) {
+  if (tbody.children.length < max) {
     removeRowButton.disabled = false;
     tbody.append(tbody.lastElementChild.cloneNode(true));
   }
 
-  if (tbody.children.length === 10) {
+  if (tbody.children.length === max) {
     appendRowButton.disabled = true;
   }
 });
 
 removeRowButton.addEventListener('click', function() {
-  if (tbody.children.length > 2) {
+  if (tbody.children.length > min) {
     appendRowButton.disabled = false;
     tbody.lastElementChild.remove();
   }
 
-  if (tbody.children.length === 2) {
+  if (tbody.children.length === min) {
     removeRowButton.disabled = true;
   }
 });
@@ -32,7 +34,7 @@ removeRowButton.addEventListener('click', function() {
 appendColumnButton.addEventListener('click', function() {
   const rows = document.querySelectorAll('tr');
 
-  if (rows[0].children.length < 10) {
+  if (rows[0].children.length < max) {
     removeColumnButton.disabled = false;
 
     [...rows].forEach(row => {
@@ -40,7 +42,7 @@ appendColumnButton.addEventListener('click', function() {
     });
   }
 
-  if (rows[0].children.length === 10) {
+  if (rows[0].children.length === max) {
     appendColumnButton.disabled = true;
   }
 });
@@ -48,7 +50,7 @@ appendColumnButton.addEventListener('click', function() {
 removeColumnButton.addEventListener('click', function() {
   const rows = document.querySelectorAll('tr');
 
-  if (rows[0].children.length > 2) {
+  if (rows[0].children.length > min) {
     appendColumnButton.disabled = false;
 
     [...rows].forEach(row => {
@@ -56,7 +58,7 @@ removeColumnButton.addEventListener('click', function() {
     });
   }
 
-  if (rows[0].children.length === 2) {
+  if (rows[0].children.length === min) {
     removeColumnButton.disabled = true;
   }
 });
