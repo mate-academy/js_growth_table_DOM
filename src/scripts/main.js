3ba1be94376ce3d4c0a634d34e7dc2f2661bbae0
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-21 10:14:58 +0300

    growth_table_DOM_JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,71 +1,81 @@
 'use strict';
 
-const buttons = document.getElementsByClassName('container')[0].children;
+const container = document.querySelector('.container');
 
-for (const button of buttons) {
-  if (button.classList.contains('button')) {
-    button.addEventListener('click', () => {
-      switch (true) {
-        case button.classList.contains('append-row'):
-          if (document.querySelector('tbody').children.length < 9) {
-            document.querySelector('tbody')
-              .append(document.querySelector('tbody')
-                .children[0].cloneNode(true));
-            document.getElementsByClassName('remove-row')[0].disabled = false;
-          }
+container.addEventListener('click', (evt) => {
+  const appendRowButton = evt.target.classList.contains('append-row');
+  const removeRowButton = evt.target.classList.contains('remove-row');
+  const appendColumnButton = evt.target.classList.contains('append-column');
+  const removeColumnButton = evt.target.classList.contains('remove-column');
+  const tableBody = document.querySelector('tbody');
 
-          if (document.querySelector('tbody').children.length === 9) {
-            document.querySelector('tbody')
-              .append(document.querySelector('tbody')
-                .children[0].cloneNode(true));
-            button.disabled = true;
-          }
-          break;
-        case button.classList.contains('remove-row'):
-          if (document.querySelector('tbody').children.length >= 3) {
-            document.querySelector('tbody').children[0]
-              .remove();
-            document.getElementsByClassName('append-row')[0].disabled = false;
-          }
-
-          if (document.querySelector('tbody').children.length === 2) {
-            button.disabled = true;
-          }
-          break;
-        case button.classList.contains('append-column'):
-          if (document.querySelector('tbody')
-            .children[0].children.length < 9) {
-            [...document.querySelector('tbody').children]
-              .forEach(row => row.append(document.createElement('td')));
-
-            document.getElementsByClassName('remove-column')[0]
-              .disabled = false;
-          }
-
-          if (document.querySelector('tbody')
-            .children[0].children.length === 9) {
-            [...document.querySelector('tbody').children]
-              .forEach(row => row.append(document.createElement('td')));
-            button.disabled = true;
-          }
-          break;
-        case button.classList.contains('remove-column'):
-          if (document.querySelector('tbody').children[0]
-            .children.length >= 3) {
-            [...document.querySelector('tbody').children]
-              .forEach(row => row.children[0].remove());
-
-            document.getElementsByClassName('append-column')[0]
-              .disabled = false;
-          }
-
-          if (document.querySelector('tbody')
-            .children[0].children.length === 2) {
-            button.disabled = true;
-          }
-          break;
-        default:
-      }
-    });
+  switch (true) {
+    case appendRowButton:
+      appendRow(tableBody, evt);
+      break;
+    case removeRowButton:
+      removeRow(tableBody, evt);
+      break;
+    case appendColumnButton:
+      appendColumn(tableBody, evt);
+      break;
+    case removeColumnButton:
+      removeColumn(tableBody, evt);
+      break;
+    default:
   }
-}
+});
+
+function appendRow(tableBody, evt) {
+  if (tableBody.children.length < 9) {
+    tableBody.append(tableBody.children[0].cloneNode(true));
+    document.getElementsByClassName('remove-row')[0].disabled = false;
+  }
+
+  if (tableBody.children.length === 9) {
+    tableBody.append(tableBody.children[0].cloneNode(true));
+    evt.target.disabled = true;
+  }
+};
+
+function removeRow(tableBody, evt) {
+  if (tableBody.children.length >= 3) {
+    tableBody.children[0].remove();
+    document.getElementsByClassName('append-row')[0].disabled = false;
+  }
+
+  if (document.querySelector('tbody').children.length === 2) {
+    evt.target.disabled = true;
+  }
+};
+
+function appendColumn(tableBody, evt) {
+  if (tableBody.children[0].children.length < 9) {
+    [...tableBody.children]
+      .forEach(row => row.append(document.createElement('td')));
+
+    document.getElementsByClassName('remove-column')[0]
+      .disabled = false;
+  }
+
+  if (tableBody.children[0].children.length === 9) {
+    [...document.querySelector('tbody').children]
+      .forEach(row => row.append(document.createElement('td')));
+    evt.target.disabled = true;
+  }
+};
+
+function removeColumn(tableBody, evt) {
+  if (tableBody.children[0].children.length >= 3) {
+    [...tableBody.children]
+      .forEach(row => row.children[0].remove());
+
+    document.getElementsByClassName('append-column')[0]
+      .disabled = false;
+  }
+
+  if (document.querySelector('tbody')
+    .children[0].children.length === 2) {
+    evt.target.disabled = true;
+  }
+};
