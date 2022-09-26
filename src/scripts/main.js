0ad0ba6f92eb9038c813215c79f86cd0be6e6038
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-09-26 17:48:09 +0300

    Solution after 1st review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,38 +6,42 @@ const appendRowButton = document.querySelector('.append-row');
 const appendColumnButton = document.querySelector('.append-column');
 const removeRowButton = document.querySelector('.remove-row');
 const removeColumnButton = document.querySelector('.remove-column');
+const maxLength = 10;
+const minLength = 2;
 
 appendRowButton.addEventListener('click', () => {
   const rowClone = rows[0].cloneNode(true);
 
   table.children[0].append(rowClone);
-  removeRowButton.disabled = false;
 
-  if (rows.length >= 10) {
+  if (rows.length >= maxLength) {
     appendRowButton.disabled = true;
+  } else {
+    removeRowButton.disabled = false;
   }
 });
 
 removeRowButton.addEventListener('click', () => {
   rows[rows.length - 1].remove();
-  appendRowButton.disabled = false;
 
-  if (rows.length <= 2) {
+  if (rows.length <= minLength) {
     removeRowButton.disabled = true;
+  } else {
+    appendRowButton.disabled = false;
   }
 });
 
 appendColumnButton.addEventListener('click', () => {
-  for (let i = 0; i < table.children[0].children.length; i++) {
-    const cloneCell = rows[i].firstElementChild.cloneNode(true);
+  const cellClone = rows[0].firstElementChild.cloneNode();
 
-    rows[i].append(cloneCell);
+  for (let i = 0; i < table.children[0].children.length; i++) {
+    rows[i].append(cellClone.cloneNode());
   }
 
-  removeColumnButton.disabled = false;
-
-  if (rows[0].children.length >= 10) {
+  if (rows[0].children.length >= maxLength) {
     appendColumnButton.disabled = true;
+  } else {
+    removeColumnButton.disabled = false;
   }
 });
 
@@ -46,9 +50,9 @@ removeColumnButton.addEventListener('click', () => {
     rows[i].lastElementChild.remove();
   }
 
-  appendColumnButton.disabled = false;
-
-  if (rows[0].children.length <= 2) {
+  if (rows[0].children.length <= minLength) {
     removeColumnButton.disabled = true;
+  } else {
+    appendColumnButton.disabled = false;
   }
 });
