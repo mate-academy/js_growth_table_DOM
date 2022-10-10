e018bb6a284b97d21798fe72caae00dcc9bbdaae
Danil <dasha8854567@gmail.com>
2022-10-10 13:14:56 +0300

    commit4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,10 @@ const removeColumn = document.querySelector('.remove-column');
 const removeRow = document.querySelector('.remove-row');
 const appendRow = document.querySelector('.append-row');
 
-function examination(maxLength, button, maxOrMin) {
+const max = 9;
+const min = 3;
+
+function toggleDisabled(maxLength, button, maxOrMin) {
   if (maxLength === maxOrMin) {
     button.disabled = true;
   } else {
@@ -15,51 +18,51 @@ function examination(maxLength, button, maxOrMin) {
 }
 
 appendColumn.addEventListener('click', () => {
-  const coloms = [...document.querySelectorAll('tr')];
-  const amountTr = coloms[0].children.length;
+  const colomns = [...document.querySelectorAll('tr')];
+  const amountTr = colomns[0].children.length;
 
   removeColumn.disabled = false;
 
-  coloms.forEach(colom => {
-    colom.insertAdjacentHTML('afterbegin', `<td></td>`);
+  colomns.forEach(colomn => {
+    colomn.insertAdjacentHTML('afterbegin', `<td></td>`);
   });
 
-  examination(amountTr, appendColumn, 9);
+  toggleDisabled(amountTr, appendColumn, max);
 });
 
 removeColumn.addEventListener('click', () => {
-  const coloms = [...document.querySelectorAll('tr')];
-  const amountTr = coloms[0].children.length;
+  const colomns = [...document.querySelectorAll('tr')];
+  const amountTr = colomns[0].children.length;
 
   appendColumn.disabled = false;
 
-  coloms.forEach(colom => {
+  colomns.forEach(colom => {
     colom.children[colom.children.length - 1].remove();
   });
 
-  examination(amountTr, removeColumn, 3);
+  toggleDisabled(amountTr, removeColumn, min);
 });
 
 appendRow.addEventListener('click', () => {
-  const coloms = [...document.querySelectorAll('tr')];
-  const amountColoms = coloms.length;
+  const colomns = [...document.querySelectorAll('tr')];
+  const amountColomns = colomns.length;
 
-  const colom = document.querySelector('tr');
+  const colomn = document.querySelector('tr');
 
   removeRow.disabled = false;
 
-  tbody.insertAdjacentHTML('beforeend', colom.innerHTML);
+  tbody.insertAdjacentHTML('beforeend', colomn.innerHTML);
 
-  examination(amountColoms, appendRow, 9);
+  toggleDisabled(amountColomns, appendRow, max);
 });
 
 removeRow.addEventListener('click', () => {
-  const coloms = [...document.querySelectorAll('tr')];
-  const amountColoms = coloms.length;
+  const colomns = [...document.querySelectorAll('tr')];
+  const amountColomns = colomns.length;
 
   appendRow.disabled = false;
 
-  coloms[amountColoms - 1].remove();
+  colomns[amountColomns - 1].remove();
 
-  examination(amountColoms, removeRow, 3);
+  toggleDisabled(amountColomns, removeRow, min);
 });
