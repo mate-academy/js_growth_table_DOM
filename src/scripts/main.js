c60f2b898be5f5196632aaf20b9c32eea5a4f09c
Hlazman <glazman.b@gmail.com>
2022-10-16 13:04:09 +0300

    fixed previous comments about separate functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,66 +7,48 @@ const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 
-const minCount = 2;
-const maxCount = 10;
+const disableButtons = (node, append, remove) => {
+  const minCount = 2;
+  const maxCount = 10;
+
+  if (node.children.length === maxCount) {
+    append.disabled = true;
+  }
+
+  if (node.children.length > minCount) {
+    remove.disabled = false;
+  }
+
+  if (node.children.length === minCount) {
+    remove.disabled = true;
+  }
+
+  if (node.children.length < maxCount) {
+    append.disabled = false;
+  }
+};
 
 const addRow = () => {
-  const newRow = tableBody.lastElementChild.cloneNode(true);
-
-  tableBody.append(newRow);
-
-  if (tableBody.children.length === maxCount) {
-    appendRow.disabled = true;
-  }
-
-  if (tableBody.children.length > minCount) {
-    removeRow.disabled = false;
-  }
+  tableBody.append(tableBody.lastElementChild.cloneNode(true));
+  disableButtons(tableBody, appendRow, removeRow);
 };
 
 const deleteRow = () => {
   tableBody.lastElementChild.remove();
-
-  if (tableBody.children.length === minCount) {
-    removeRow.disabled = true;
-  }
-
-  if (tableBody.children.length < maxCount) {
-    appendRow.disabled = false;
-  }
+  disableButtons(tableBody, appendRow, removeRow);
 };
 
 const addCell = () => {
-  const rows = [...tableBody.children];
-
-  rows.forEach(row => {
-    const newColumn = row.lastElementChild.cloneNode(true);
-
-    row.append(newColumn);
-
-    if (row.children.length === maxCount) {
-      appendColumn.disabled = true;
-    }
-
-    if (row.children.length > minCount) {
-      removeColumn.disabled = false;
-    }
+  [...tableBody.children].forEach(row => {
+    row.append(row.lastElementChild.cloneNode(true));
+    disableButtons(row, appendColumn, removeColumn);
   });
 };
 
 const deleteCell = () => {
-  const rows = [...tableBody.children];
-
-  rows.forEach(row => {
+  [...tableBody.children].forEach(row => {
     row.lastElementChild.remove();
-
-    if (row.children.length < maxCount) {
-      appendColumn.disabled = false;
-    }
-
-    if (row.children.length === minCount) {
-      removeColumn.disabled = true;
-    }
+    disableButtons(row, appendColumn, removeColumn);
   });
 };
 
