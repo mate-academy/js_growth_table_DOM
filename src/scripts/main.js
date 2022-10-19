3484e8927ac3912098685fd033359ed557c6c18f
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-10-19 19:29:37 +0300

    Solution with fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const table = document.querySelector('tbody');
-const tableRow = document.querySelector('tr');
 const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 
 appendRow.addEventListener('click', () => {
+  const table = document.querySelector('tbody');
+
   if (table.children.length < 10) {
     const cloneItem = table.firstChild.cloneNode(true);
 
@@ -16,12 +16,17 @@ appendRow.addEventListener('click', () => {
 });
 
 removeRow.addEventListener('click', () => {
+  const table = document.querySelector('tbody');
+
   if (table.children.length > 2) {
     table.lastChild.remove();
   }
 });
 
 appendColumn.addEventListener('click', () => {
+  const table = document.querySelector('tbody');
+  const tableRow = document.querySelector('tr');
+
   if (tableRow.children.length < 10) {
     [...table.children].forEach(el => {
       const newTableItem = document.createElement('td');
@@ -32,6 +37,9 @@ appendColumn.addEventListener('click', () => {
 });
 
 removeColumn.addEventListener('click', () => {
+  const table = document.querySelector('tbody');
+  const tableRow = document.querySelector('tr');
+
   if (tableRow.children.length > 2) {
     [...table.children].forEach(el => el.lastChild.remove());
   }
