bb1e1ab96f8bedbdb2674cedde4f7f7a0899393c
Kolya Movchan <kmovchan261@gmail.com>
2022-11-04 00:44:02 +0200

    js_growth_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
-// write code here
+const findE = (element) => document.querySelector(element);
+const modeOn = (element) => element.removeAttribute('disabled');
+const modeOff = (element) => element.setAttribute('disabled', '');
+
+const addRow = findE('.append-row');
+const removeRow = findE('.remove-row');
+const addColumn = findE('.append-column');
+const removeColumn = findE('.remove-column');
+
+const tbody = findE('tbody');
+const cell = findE('td');
+
+const allRows = document.getElementsByTagName('tr');
+
+addRow.addEventListener('click', () => {
+  tbody.append(allRows[0].cloneNode(true));
+  modeOn(removeRow);
+
+  if (allRows.length === 10) {
+    modeOff(addRow);
+  };
+});
+
+removeRow.addEventListener('click', () => {
+  tbody.firstElementChild.remove();
+  modeOn(addRow);
+
+  if (allRows.length === 2) {
+    modeOff(removeRow);
+  };
+});
+
+addColumn.addEventListener('click', () => {
+  modeOn(removeColumn);
+
+  [...allRows].map(line => {
+    line.append(cell.cloneNode(true));
+
+    if (allRows[0].childElementCount === 10) {
+      modeOff(addColumn);
+    }
+  });
+});
+
+removeColumn.addEventListener('click', () => {
+  modeOn(addColumn);
+
+  [...allRows].map(line => {
+    line.firstElementChild.remove();
+
+    if (allRows[0].childElementCount === 2) {
+      modeOff(removeColumn);
+    }
+  });
+});
