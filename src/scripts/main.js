4112bfbb05b38d593bf33e5a3c33ca7f2e52f91a
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-11-17 11:45:07 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,77 @@
 'use strict';
 
 // write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const tableRef = document.querySelector('.field');
+
+appendRow.addEventListener('click', function() {
+  notDisable(removeRow);
+
+  const lengthR = tableRef.querySelectorAll('tr').length;
+
+  if (lengthR > 8) {
+    appendRow.setAttribute('disabled', 'disabled');
+  }
+
+  const row = tableRef.insertRow(tableRef.rows.length);
+
+  for (let i = 0; i < tableRef.rows[0].cells.length; i++) {
+    createCell(row.insertCell(i), 'row');
+  }
+});
+
+removeRow.addEventListener('click', function() {
+  notDisable(appendRow);
+
+  const row = tableRef.querySelectorAll('tr');
+
+  if (row.length > 2) {
+    tableRef.deleteRow(-1);
+  } else {
+    removeRow.setAttribute('disabled', 'disabled');
+  }
+});
+
+removeColumn.addEventListener('click', function() {
+  notDisable(appendColumn);
+
+  const row = tableRef.querySelectorAll('tr');
+
+  for (let i = 0; i < row.length; i++) {
+    row[i].deleteCell(0);
+  }
+
+  if (tableRef.rows[0].cells.length < 3) {
+    removeColumn.setAttribute('disabled', 'disabled');
+  }
+});
+
+appendColumn.addEventListener('click', function() {
+  notDisable(removeColumn);
+
+  for (let i = 0; i < tableRef.rows.length; i++) {
+    createCell(tableRef.rows[i].insertCell(tableRef.rows[i].cells.length),
+      i, 'col');
+  }
+
+  if (tableRef.rows[0].cells.length > 9) {
+    appendColumn.setAttribute('disabled', 'disabled');
+  }
+});
+
+function createCell(cell, style) {
+  const div = document.createElement('div');
+
+  div.setAttribute('class', style);
+  div.setAttribute('className', style);
+  cell.appendChild(div);
+}
+
+function notDisable(button) {
+  if (event) {
+    button.removeAttribute('disabled', 'disabled');
+  }
+}
