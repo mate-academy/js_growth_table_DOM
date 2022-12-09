486f87ef29acab752d802d9aba03359b48f9a642
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-09 21:46:04 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const trAll = document.querySelectorAll('tr');
+
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+
+appendColumn.addEventListener('click', e => {
+  if (trAll[0].children.length > 9) {
+    return;
+  }
+
+  for (let i = 0; i < trAll.length; i++) {
+    trAll[i].insertAdjacentHTML('beforeend', '<td></td>');
+  }
+});
+
+removeColumn.addEventListener('click', e => {
+  if (trAll[0].children.length < 3) {
+    return;
+  }
+
+  for (let i = 0; i < trAll.length; i++) {
+    trAll[i].lastElementChild.remove();
+  }
+});
+
+appendRow.addEventListener('click', e => {
+  if (document.querySelectorAll('tr').length > 9) {
+    return;
+  }
+
+  tbody.append(tbody.lastElementChild.cloneNode(true));
+});
+
+removeRow.addEventListener('click', e => {
+  if (document.querySelectorAll('tr').length < 3) {
+    return;
+  }
+
+  tbody.lastElementChild.remove();
+});
