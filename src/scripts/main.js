6ca565349b6ab8049d18f5cf8fc6d7190778a014
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-10 11:36:21 +0200

    Buttons can now turn on and off correctly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const tbody = document.querySelector('tbody');
-const trAll = document.querySelectorAll('tr');
+let allRows;
 
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
@@ -9,38 +9,80 @@ const removeColumn = document.querySelector('.remove-column');
 const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 
+// add column
+
 appendColumn.addEventListener('click', e => {
-  if (trAll[0].children.length > 9) {
+  allRows = document.querySelectorAll('tr');
+
+  removeColumn.disabled = false;
+
+  if (allRows[0].children.length > 9) {
     return;
   }
 
-  for (let i = 0; i < trAll.length; i++) {
-    trAll[i].insertAdjacentHTML('beforeend', '<td></td>');
+  for (let i = 0; i < allRows.length; i++) {
+    allRows[i].insertAdjacentHTML('beforeend', '<td></td>');
+  }
+
+  allRows = document.querySelectorAll('tr');
+
+  if (allRows[0].children.length === 10) {
+    appendColumn.disabled = true;
   }
 });
+
+// remove column
 
 removeColumn.addEventListener('click', e => {
-  if (trAll[0].children.length < 3) {
+  allRows = document.querySelectorAll('tr');
+
+  appendColumn.disabled = false;
+
+  if (allRows[0].children.length < 3) {
     return;
   }
 
-  for (let i = 0; i < trAll.length; i++) {
-    trAll[i].lastElementChild.remove();
+  for (let i = 0; i < allRows.length; i++) {
+    allRows[i].lastElementChild.remove();
+  }
+
+  if (allRows[0].children.length === 2) {
+    removeColumn.disabled = true;
   }
 });
 
+// add row
+
 appendRow.addEventListener('click', e => {
-  if (document.querySelectorAll('tr').length > 9) {
+  allRows = document.querySelectorAll('tr');
+
+  if (allRows.length > 9) {
     return;
   }
 
   tbody.append(tbody.lastElementChild.cloneNode(true));
+
+  allRows = document.querySelectorAll('tr');
+
+  if (allRows.length === 10) {
+    appendRow.disabled = true;
+  }
 });
 
+// remove row
+
 removeRow.addEventListener('click', e => {
-  if (document.querySelectorAll('tr').length < 3) {
+  allRows = document.querySelectorAll('tr');
+
+  if (allRows.length < 3) {
     return;
   }
 
   tbody.lastElementChild.remove();
+
+  allRows = document.querySelectorAll('tr');
+
+  if (allRows.length === 2) {
+    removeRow.disabled = true;
+  }
 });
