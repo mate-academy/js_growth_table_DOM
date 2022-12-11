689810fb889a1d090a2c9ced92702b62cca2ead8
vadiimvooo <ASD4S.Profile@gmail.com>
2022-12-11 16:05:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const tableBody = document.querySelector('tbody');
+const getAllRows = document.getElementsByTagName('tr');
+
+appendRow.addEventListener('click', e => {
+  tableBody.append(tableBody.firstChild.cloneNode(true));
+
+  if (getAllRows.length >= 10) {
+    appendRow.disabled = true;
+  }
+
+  removeRow.disabled = false;
+});
+
+removeRow.addEventListener('click', e => {
+  tableBody.lastElementChild.remove();
+
+  if (getAllRows.length <= 2) {
+    removeRow.disabled = true;
+  }
+
+  appendRow.disabled = false;
+});
+
+appendColumn.addEventListener('click', e => {
+  for (const element of getAllRows) {
+    element.append(getAllRows[0].lastElementChild.cloneNode(true));
+  };
+
+  if (getAllRows[0].children.length >= 10) {
+    appendColumn.disabled = true;
+  }
+
+  removeColumn.disabled = false;
+});
+
+removeColumn.addEventListener('click', e => {
+  for (const element of getAllRows) {
+    element.lastElementChild.remove();
+  };
+
+  if (getAllRows[0].children.length <= 2) {
+    removeColumn.disabled = true;
+  }
+
+  appendColumn.disabled = false;
+});
