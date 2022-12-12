24ee591a4aec70588e0a7abf4eb5c949b56710c9
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-12-12 16:09:31 +0200

    Update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,39 +5,39 @@ const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 
-const allTr = document.getElementsByTagName('tr');
-const tbody = document.querySelector('tbody');
+const rowsColection = document.getElementsByTagName('tr');
+const tableConteiner = document.querySelector('tableConteiner');
 
 function addColumn() {
-  for (const tr of allTr) {
-    tr.append(allTr[0].firstElementChild.cloneNode(true));
+  for (const tr of rowsColection) {
+    tr.append(rowsColection[0].firstElementChild.cloneNode(true));
   }
 
   removeColumn.disabled = false;
-  appendColumn.disabled = allTr[0].children.length > 9;
+  appendColumn.disabled = rowsColection[0].children.length > 9;
 }
 
 function deleteColumn() {
-  for (const tr of allTr) {
+  for (const tr of rowsColection) {
     tr.lastElementChild.remove();
   }
 
   appendColumn.disabled = false;
-  removeColumn.disabled = allTr[0].children.length < 3;
+  removeColumn.disabled = rowsColection[0].children.length < 3;
 }
 
 function addRow() {
-  removeRow.disabled = false;
-  appendRow.disabled = tbody.children.length > 9;
+  tableConteiner.append(tableConteiner.children[0].cloneNode(true));
 
-  tbody.append(tbody.children[0].cloneNode(true));
+  removeRow.disabled = false;
+  appendRow.disabled = tableConteiner.children.length > 9;
 }
 
 function deleteRow() {
-  tbody.lastElementChild.remove();
+  tableConteiner.lastElementChild.remove();
 
   appendRow.disabled = false;
-  removeRow.disabled = tbody.children.length < 3;
+  removeRow.disabled = tableConteiner.children.length < 3;
 }
 
 appendColumn.addEventListener('click', addColumn);
