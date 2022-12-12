ce1c4009d2ca02e17a559df4c3b689d171293ad3
Andrii Medintsev <medintsev.andrew@gmail.com>
2022-12-12 22:24:28 +0200

    Code duplicating was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@
 
 const table = document.querySelector('.field');
 const tbody = table.querySelector('tbody');
+const rowsCollection = tbody.rows;
 
 const appendRowButton = document.querySelector('.append-row');
 const removeRowButton = document.querySelector('.remove-row');
@@ -9,65 +10,68 @@ const appendColumnButton = document.querySelector('.append-column');
 const removeColumnButton = document.querySelector('.remove-column');
 
 appendRowButton.addEventListener('click', () => {
-  if (tbody.rows.length >= 10) {
+  if (rowsCollection.length >= 10) {
     return;
   }
 
   removeRowButton.disabled = false;
 
   tbody.insertAdjacentHTML('beforeend', `
-    ${tbody.rows[tbody.rows.length - 1].outerHTML}
+    ${rowsCollection[rowsCollection.length - 1].outerHTML}
   `);
 
-  if (tbody.rows.length >= 10) {
+  if (rowsCollection.length >= 10) {
     appendRowButton.disabled = true;
   }
 });
 
 removeRowButton.addEventListener('click', () => {
-  if (tbody.rows.length <= 2) {
+  if (rowsCollection.length <= 2) {
     return;
   }
 
   appendRowButton.disabled = false;
 
-  tbody.deleteRow(tbody.rows.length - 1);
+  tbody.deleteRow(rowsCollection.length - 1);
 
-  if (tbody.rows.length <= 2) {
+  if (rowsCollection.length <= 2) {
     removeRowButton.disabled = true;
   }
 });
 
 appendColumnButton.addEventListener('click', () => {
-  if (tbody.rows[0].cells.length >= 10) {
+  if (rowsCollection[0].cells.length >= 10) {
     return;
   }
 
   removeColumnButton.disabled = false;
 
-  for (let i = 0; i < tbody.rows.length; i++) {
-    const cube = tbody.rows[0].cells[0];
+  for (let i = 0; i < rowsCollection.length; i++) {
+    const cube = rowsCollection[0].cells[0];
 
-    tbody.rows[i].insertAdjacentHTML('beforeend', cube.outerHTML);
+    rowsCollection[i].insertAdjacentHTML('beforeend', cube.outerHTML);
   }
 
-  if (tbody.rows[0].cells.length >= 10) {
+  if (rowsCollection[0].cells.length >= 10) {
     appendColumnButton.disabled = true;
   }
 });
 
 removeColumnButton.addEventListener('click', () => {
-  if (tbody.rows[0].cells.length <= 2) {
+  if (rowsCollection[0].cells.length <= 2) {
     return;
   }
 
   appendColumnButton.disabled = false;
 
-  for (let i = 0; i < tbody.rows.length; i++) {
-    tbody.rows[i].cells[tbody.rows[i].cells.length - 1].outerHTML = null;
+  for (let i = 0; i < rowsCollection.length; i++) {
+    const columnCollection = rowsCollection[i].cells;
+
+    columnCollection[columnCollection.length - 1].outerHTML
+      = null;
   }
 
-  if (tbody.rows[0].cells.length === 2) {
+  if (rowsCollection[0].cells.length === 2) {
     removeColumnButton.disabled = true;
   }
 });
