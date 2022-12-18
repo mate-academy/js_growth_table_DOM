0bb7dffb12312b8e86056c9b5ef3130c9a3e4606
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-12-18 12:42:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write code here
+const tableChild = document.querySelector('table').firstElementChild;
+const container = document.querySelector('.container');
+
+const buttonCreateRow = document.querySelector('.append-row');
+const buttonRemoveRow = document.querySelector('.remove-row');
+const buttonCreateColumn = document.querySelector('.append-column');
+const buttonRemoveColumn = document.querySelector('.remove-column');
+
+container.addEventListener('click', (action) => {
+  const rowsCollection = [...tableChild.children];
+
+  switch (action.target) {
+    case buttonCreateRow:
+      buttonRemoveRow.disabled = false;
+
+      tableChild.children.length < 10
+        ? tableChild.append(tableChild.firstElementChild.cloneNode(true))
+        : buttonCreateRow.disabled = true;
+      break;
+
+    case buttonRemoveRow:
+      buttonCreateRow.disabled = false;
+
+      tableChild.children.length > 2
+        ? tableChild.lastElementChild.remove()
+        : buttonRemoveRow.disabled = true;
+      break;
+
+    case buttonCreateColumn:
+      buttonRemoveColumn.disabled = false;
+
+      rowsCollection.forEach(function(row) {
+        row.children.length < 10
+          ? row.append(row.firstElementChild.cloneNode(true))
+          : buttonCreateColumn.disabled = true;
+      });
+      break;
+
+    case buttonRemoveColumn:
+      buttonCreateColumn.disabled = false;
+
+      rowsCollection.forEach(function(row) {
+        row.children.length > 2
+          ? row.lastElementChild.remove()
+          : buttonRemoveColumn.disabled = true;
+      });
+  }
+});
