6d9827ddadff8336517f839155564ead02e837b4
Aleks <offsset@gmail.com>
2022-12-30 17:07:58 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const tr = document.getElementsByTagName('tr');
+
+container.addEventListener('click', (e) => {
+  switch (e.target.className) {
+    case 'append-row button':
+      if (tr.length < 10) {
+        tr[tr.length - 1].after(tr[tr.length - 1].cloneNode(true));
+      }
+
+      if (tr.length === 10) {
+        e.target.setAttribute('disabled', true);
+      }
+      break;
+    case 'remove-row button':
+      if (tr.length > 2) {
+        tr[tr.length - 1].remove();
+      }
+
+      if (tr.length === 2) {
+        e.target.setAttribute('disabled', true);
+      }
+
+      break;
+    case 'append-column button':
+      [...tr].forEach(element => {
+        const amauntTD = element.getElementsByTagName('td').length;
+
+        if (amauntTD < 10) {
+          element.lastElementChild.after(
+            element.lastElementChild.cloneNode(true));
+        }
+
+        if (amauntTD === 9) {
+          e.target.setAttribute('disabled', true);
+        }
+      });
+
+      break;
+    case 'remove-column button':
+      [...tr].forEach(element => {
+        const amauntTD = element.getElementsByTagName('td').length;
+
+        if (amauntTD > 2) {
+          element.lastElementChild.remove();
+        }
+
+        if (amauntTD === 3) {
+          e.target.setAttribute('disabled', true);
+        }
+      });
+      break;
+  }
+});
