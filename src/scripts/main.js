3c1f7eab8cfd8960338d2f3c1e9fd4b7cf97942a
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-01-13 00:58:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
 'use strict';
 
-// write code here
+const MAX = 10;
+const MIN = 2;
+
+const field = document.querySelector('.field');
+const [appendRow, removeRow, appendColumn, removeColumn]
+  = document.querySelectorAll('.button');
+
+appendColumn.addEventListener('click', () => {
+  if (field.rows[0].cells.length >= MAX + 1) {
+    appendColumn.disabled = true;
+  }
+
+  for (const row of field.rows) {
+    row.append(row.cells[0].cloneNode(true));
+  }
+
+  if (field.rows[0].cells.length >= MIN + 1) {
+    removeColumn.disabled = false;
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  if (field.rows[0].cells.length <= MIN + 1) {
+    removeColumn.disabled = true;
+  }
+
+  for (const row of field.rows) {
+    row.cells[0].remove();
+  }
+
+  if (field.rows[0].cells.length <= MAX + 1) {
+    appendColumn.disabled = false;
+  }
+});
+
+appendRow.addEventListener('click', () => {
+  if (field.rows.length >= MAX + 1) {
+    appendRow.disabled = true;
+  }
+
+  field.append(field.rows[0].cloneNode(true));
+
+  if (field.rows.length >= MIN + 1) {
+    removeRow.disabled = false;
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  if (field.rows.length <= MIN + 1) {
+    removeRow.disabled = true;
+  }
+
+  field.rows[0].remove();
+
+  if (field.rows.length <= MAX + 1) {
+    appendRow.disabled = false;
+  }
+});
