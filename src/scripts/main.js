7db4eda3c3811cca4c6f2d880bcdb090f0852b1e
EhorSaprykin <astralik1993@gmail.com>
2023-01-19 12:42:22 +0200

    Done add/remove buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field tbody');
+let rows = table.querySelectorAll('tr');
+
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+appendRow.addEventListener('click', () => {
+  const newRow = rows[rows.length - 1].cloneNode(true);
+
+  table.append(newRow);
+
+  rows = table.querySelectorAll('tr');
+
+  rows.length >= 10
+    ? (appendRow.disabled = true)
+    : (removeRow.disabled = '');
+});
+
+removeRow.addEventListener('click', () => {
+  table.lastElementChild.remove();
+
+  rows = table.querySelectorAll('tr');
+
+  rows.length <= 2
+    ? (removeRow.disabled = true)
+    : (appendRow.disabled = '');
+});
+
+appendColumn.addEventListener('click', () => {
+  rows.forEach(row => {
+    const newColumn = document.createElement('td');
+
+    row.appendChild(newColumn);
+
+    row.childElementCount >= 10
+      ? appendColumn.disabled = true
+      : removeColumn.disabled = '';
+  });
+});
+
+removeColumn.addEventListener('click', () => {
+  rows.forEach(row => {
+    row.lastElementChild.remove();
+
+    row.childElementCount <= 2
+      ? removeColumn.disabled = true
+      : appendColumn.disabled = '';
+  });
+});
