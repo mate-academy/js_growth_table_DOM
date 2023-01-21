48401cdadbd10742e80178b3d2377664ea74828b
Mykola Chernysh <nanoteck7@gmail.com>
2023-01-21 18:44:25 +0200

    min,max to variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,15 @@ const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 const tableBody = document.querySelector('tbody');
 
+const maxLength = 10;
+const minLength = 2;
+
 appendColumn.addEventListener('click', (e) => {
   [...tableBody.children].forEach(elem => {
     elem.append(elem.lastElementChild.cloneNode(true));
   });
 
-  if (tableBody.firstChild.children.length >= 10) {
+  if (tableBody.firstChild.children.length >= maxLength) {
     appendColumn.disabled = true;
   }
 
@@ -23,7 +26,7 @@ removeColumn.addEventListener('click', (e) => {
     elem.lastElementChild.remove();
   });
 
-  if (tableBody.firstChild.children.length <= 2) {
+  if (tableBody.firstChild.children.length <= minLength) {
     removeColumn.disabled = true;
   }
 
@@ -33,7 +36,7 @@ removeColumn.addEventListener('click', (e) => {
 appendRow.addEventListener('click', (e) => {
   tableBody.append(tableBody.lastElementChild.cloneNode(true));
 
-  if (tableBody.children.length >= 10) {
+  if (tableBody.children.length >= maxLength) {
     appendRow.disabled = true;
   }
 
@@ -43,7 +46,7 @@ appendRow.addEventListener('click', (e) => {
 removeRow.addEventListener('click', (e) => {
   tableBody.lastElementChild.remove();
 
-  if (tableBody.children.length <= 2) {
+  if (tableBody.children.length <= minLength) {
     removeRow.disabled = true;
   }
 
