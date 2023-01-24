d5d562450340d02e45c76fe83b93ef013f165eda
Vitalii Skomskiy <skomskiy@ukr.net>
2023-01-24 20:31:07 +0200

    commit-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,13 +9,16 @@ const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 
 container.addEventListener('click', (e) => {
+  const rowsLenght = rows.children.length;
+  const columnLenght = tbody.children.length;
+
   if (e.target.tagName !== 'BUTTON') {
     return;
   };
 
   switch (e.target) {
     case appendColumn:
-      if (rows.children.length < 10) {
+      if (rowsLenght < 10) {
         const allRows = document.querySelectorAll('tr');
 
         for (let i = 0; i < allRows.length; i++) {
@@ -24,23 +27,51 @@ container.addEventListener('click', (e) => {
       }
       break;
     case removeColumn:
-      if (rows.children.length > 2) {
-        const allRow = document.querySelectorAll('tr');
+      if (rowsLenght > 2) {
+        const allRows = document.querySelectorAll('tr');
 
-        for (let i = 0; i < allRow.length; i++) {
-          allRow[i].lastElementChild.remove();
+        for (let i = 0; i < allRows.length; i++) {
+          allRows[i].lastElementChild.remove();
         }
       }
       break;
     case appendRow:
-      if (tbody.children.length < 10) {
+      if (columnLenght < 10) {
         tbody.append(tbody.lastElementChild.cloneNode(true));
       }
       break;
     case removeRow:
-      if (tbody.children.length > 2) {
+      if (columnLenght > 2) {
         tbody.lastElementChild.remove();
       }
       break;
   };
+
+  const newRowsLenght = rows.children.length;
+
+  appendColumn.style.display = '';
+  removeColumn.style.display = '';
+
+  switch (newRowsLenght) {
+    case 10:
+      appendColumn.style.display = 'none';
+      break;
+    case 2:
+      removeColumn.style.display = 'none';
+      break;
+  }
+
+  const newColumnLenght = tbody.children.length;
+
+  appendRow.style.display = '';
+  removeRow.style.display = '';
+
+  switch (newColumnLenght) {
+    case 10:
+      appendRow.style.display = 'none';
+      break;
+    case 2:
+      removeRow.style.display = 'none';
+      break;
+  }
 });
