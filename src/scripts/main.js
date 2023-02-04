152e878f0d17b13007fec47bcac7507b14142a2d
Roman Shevchenko <r.shevchenko87@gmail.com>
2023-02-04 18:03:22 +0200

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,42 +9,62 @@ const rows = document.getElementsByTagName('tr');
 const field = document.querySelector('tbody');
 const newRow = field.getElementsByTagName('tr')[0];
 
+const max = 10;
+const min = 2;
+
 appendRow.onclick = () => {
-  if (rows.length < 10) {
+  removeRow.disabled = false;
+
+  if (rows.length < max) {
     field.append(newRow.cloneNode(true));
+  }
+
+  if (rows.length === max) {
+    appendRow.disabled = true;
   }
 };
 
 removeRow.onclick = () => {
-  if (rows.length > 2) {
+  appendRow.disabled = false;
+
+  if (rows.length > min) {
     field.children[rows.length - 1].remove();
+  }
+
+  if (rows.length === min) {
+    removeRow.disabled = true;
   }
 };
 
 let collAmount = 4;
 
 appendCol.onclick = () => {
-  if (collAmount < 10) {
+  removeCol.disabled = false;
+
+  if (collAmount < max) {
     for (const row of rows) {
       row.children[0].before(row.children[0].cloneNode(true));
     }
   }
   collAmount++;
 
-  if (collAmount > 10) {
-    collAmount = 10;
+  if (collAmount === max) {
+    appendCol.disabled = true;
   }
 };
 
 removeCol.onclick = () => {
-  if (collAmount > 2) {
+  appendCol.disabled = false;
+
+  if (collAmount > min) {
     for (const row of rows) {
       row.children[collAmount - 1].remove();
     }
   }
+
   collAmount--;
 
-  if (collAmount < 2) {
-    collAmount = 2;
+  if (collAmount === min) {
+    removeCol.disabled = true;
   }
 };
