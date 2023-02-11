1a35eb1473d50e83474888e20f7d3c64f377ea9e
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-02-11 15:28:46 +0200

    Js growth table task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const table = document.querySelector('tbody');
+const maxItems = 10;
+const minItems = 2;
+
+appendRow.addEventListener('click', () => {
+  const clone = table.rows[0].cloneNode(true);
+
+  table.append(clone);
+
+  if (table.rows.length === maxItems) {
+    appendRow.setAttribute('disabled', 'true');
+  }
+
+  removeRow.removeAttribute('disabled');
+});
+
+removeRow.addEventListener('click', () => {
+  table.lastElementChild.remove();
+
+  if (table.rows.length === minItems) {
+    removeRow.setAttribute('disabled', 'true');
+  }
+
+  appendRow.removeAttribute('disabled');
+});
+
+appendColumn.addEventListener('click', () => {
+  [...table.children].map((row) => {
+    const newSquare = document.createElement('td');
+
+    row.prepend(newSquare);
+  });
+
+  if (table.rows[0].cells.length === maxItems) {
+    appendColumn.setAttribute('disabled', 'true');
+  }
+
+  removeColumn.removeAttribute('disabled');
+});
+
+removeColumn.addEventListener('click', () => {
+  [...table.children].map((row) => {
+    row.firstElementChild.remove();
+  });
+
+  if (table.rows[0].cells.length === minItems) {
+    removeColumn.setAttribute('disabled', 'true');
+  }
+
+  appendColumn.removeAttribute('disabled');
+});
