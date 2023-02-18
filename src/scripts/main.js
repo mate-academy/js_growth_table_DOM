2493906dd4f3109883e53549988a937ee70da362
Oleksandr <98667890az@gmail.com>
2023-02-18 17:02:21 +0200

    added disable buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,30 +9,60 @@ const addRow = document.querySelector('.append-row');
 const removeColumn = document.querySelector('.remove-column');
 const addColumn = document.querySelector('.append-column');
 
-removeColumn.addEventListener('click', e => {
-  for (const row of tableBody.children) {
-    if (row.childElementCount > 2) {
-      row.lastElementChild.remove();
+const maxSize = 10;
+const minSize = 2;
+
+  removeRow.addEventListener('click', e => {
+    if (tableBody.childElementCount > minSize) {
+      tableBody.lastElementChild.remove();
+
+      if (tableBody.childElementCount === minSize) {
+        removeRow.setAttribute("disabled", "on");
+      } 
+      else if (tableBody.childElementCount < maxSize)
+        addRow.removeAttribute("disabled");
     }
-  }
-});
+  });
+  
+  addRow.addEventListener('click', e => {
+    if (tableBody.childElementCount < maxSize) {
+      tableBody.append(tableBody.lastElementChild.cloneNode(true))
 
-removeRow.addEventListener('click', e => {
-  if (tableBody.childElementCount > 2) {
-    tableBody.lastElementChild.remove();
-  }
-});
+      if (tableBody.childElementCount === maxSize) {
+        addRow.setAttribute("disabled", "on")
+      }
+      else if (tableBody.childElementCount > minSize) {
+        removeRow.removeAttribute("disabled")
+      }
+    }
+  });
+  
+  removeColumn.addEventListener('click', e => {
+    for (const row of tableBody.children) {
+      if (row.childElementCount > minSize) {
+        row.lastElementChild.remove();
 
-addRow.addEventListener('click', e => {
-  if (tableBody.childElementCount < 10) {
-    tableBody.append(tableBody.lastElementChild.cloneNode(true));
-  }
-});
-
+        if (row.childElementCount === minSize) {
+          removeColumn.setAttribute("disabled", "on");
+        }
+        else if (row.childElementCount < maxSize) {
+          addColumn.removeAttribute("disabled");
+        } 
+      }
+    }
+  });
+  
 addColumn.addEventListener('click', e => {
   for (const row of tableBody.children) {
-    if (row.childElementCount < 10) {
+    if (row.childElementCount < maxSize) {
       row.append(row.lastElementChild.cloneNode(true));
-    }
+
+      if (row.childElementCount === maxSize) {
+        addColumn.setAttribute("disabled", "on");
+      }
+      else if (row.childElementCount > minSize) {
+        removeColumn.removeAttribute("disabled");
+      }
+    } 
   }
 });
