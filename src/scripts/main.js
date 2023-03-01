69459a34a0b62cb49c197170fb78dd31944f4cdb
Nikitin Evgeny <ngenya81@gmail.com>
2023-03-01 11:08:16 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,69 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const appendColumn = document.querySelector('.append-column');
+const removeRow = document.querySelector('.remove-row');
+const removeColumn = document.querySelector('.remove-column');
+const tabl = document.getElementsByTagName('tbody')[0];
+let amountColum = 4;
+let amountRow = 4;
+
+const AddRow = () => {
+  const tr = document.createElement('tr');
+
+  if (amountRow >= 10) {
+    return;
+  }
+
+  for (let i = 0; i < amountColum; i++) {
+    const td = document.createElement('td');
+
+    tr.append(td);
+  }
+  tabl.prepend(tr);
+  amountRow++;
+};
+
+const AddColumn = () => {
+  if (amountColum >= 10) {
+    return;
+  }
+
+  for (const child of tabl.children) {
+    const td = document.createElement('td');
+
+    child.append(td);
+  }
+  amountColum++;
+};
+
+const RemoveRow = () => {
+  if (amountRow <= 2) {
+    return;
+  }
+
+  const RemoveChild = tabl.firstElementChild;
+
+  RemoveChild.remove();
+
+  amountRow--;
+};
+
+const RemoveColumn = () => {
+  if (amountColum <= 2) {
+    return;
+  }
+
+  for (const child of tabl.children) {
+    const RemoveFirstElement = child.firstElementChild;
+
+    RemoveFirstElement.remove();
+  }
+
+  amountColum--;
+};
+
+appendRow.addEventListener('click', AddRow);
+appendColumn.addEventListener('click', AddColumn);
+removeRow.addEventListener('click', RemoveRow);
+removeColumn.addEventListener('click', RemoveColumn);
