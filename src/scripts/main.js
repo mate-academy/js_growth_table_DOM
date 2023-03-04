af0d531a49ae1274b229c66c8dc4bc90b28d34fb
Vlad Ptashnichenko <vp.vlad.vp@gmail.com>
2023-03-04 15:24:06 +0200

    Changed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,39 +7,49 @@ const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
+const maxColumnsOrRows = 10;
+const minColumnsOrRows = 2;
 
 appendRow.addEventListener('click', () => {
-  if ([...tbody.children].length === 10) {
-    return;
-  }
-
   tbody.append(tr.cloneNode(true));
+
+  removeRow.disabled = false;
+
+  if ([...tbody.children].length === maxColumnsOrRows) {
+    appendRow.disabled = true;
+  }
 });
 
 removeRow.addEventListener('click', () => {
-  if ([...tbody.children].length === 2) {
-    return;
-  }
-
   tbody.lastElementChild.remove();
+
+  appendRow.disabled = false;
+
+  if ([...tbody.children].length === minColumnsOrRows) {
+    removeRow.disabled = true;
+  }
 });
 
 appendColumn.addEventListener('click', () => {
-  if ([...tr.children].length === 10) {
-    return;
-  }
-
   for (const itemTr of tbody.children) {
     itemTr.append(td.cloneNode(true));
+  }
+
+  removeColumn.disabled = false;
+
+  if ([...tr.children].length === maxColumnsOrRows) {
+    appendColumn.disabled = true;
   }
 });
 
 removeColumn.addEventListener('click', () => {
-  if ([...tr.children].length === 2) {
-    return;
-  }
-
   for (const itemTr of tbody.children) {
     itemTr.lastElementChild.remove();
+  }
+
+  appendColumn.disabled = false;
+
+  if ([...tr.children].length === minColumnsOrRows) {
+    removeColumn.disabled = true;
   }
 });
