ef7ec7a83d2d3e0fd8b97bdc2614c29689fb840f
Oleksandr <Khabailiuk@gmail.com>
2023-03-09 20:00:02 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const buttons = document
+  .querySelectorAll('.remove-row, .append-row, .append-column, .remove-column');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const lastRowIndex = table.rows.length - 1;
+    const lastColumnIndex = table.rows[0].cells.length - 1;
+
+    if (button.classList.contains('remove-row') && lastRowIndex >= 2) {
+      table.deleteRow(lastRowIndex);
+    } else if (button.classList.contains('append-row') && lastRowIndex <= 8) {
+      const newRow = table.insertRow(-1);
+
+      newRow.innerHTML = table.rows[lastRowIndex].innerHTML;
+    } else if (button.classList.contains('append-column')) {
+      [...table.rows].forEach(row => row.insertCell(-1));
+    } else if (button.classList.contains('remove-column')
+    && lastColumnIndex >= 1) {
+      [...table.rows].forEach(row => row.deleteCell(lastColumnIndex));
+    }
+  });
+});
