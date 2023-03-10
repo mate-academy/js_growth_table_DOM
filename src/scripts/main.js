c7924ac3688f6106e6cc6aa7a3d0f54457aa102e
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-03-10 15:47:57 +0200

    Growth table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const tableBody = document.querySelector('tbody');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+container.addEventListener('click', (action) => {
+  const rows = tableBody.children;
+
+  if (action.target === appendRow) {
+    tableBody.append(tableBody.firstElementChild.cloneNode(true));
+  } else if (action.target === removeRow) {
+    tableBody.lastElementChild.remove();
+  } else if (action.target === appendColumn) {
+    for (const row of rows) {
+      row.append(row.firstElementChild.cloneNode(true));
+    }
+  } else if (action.target === removeColumn) {
+    for (const row of rows) {
+      row.lastElementChild.remove();
+    }
+  }
+
+  appendRow.disabled = tableBody.children.length >= 10;
+  removeRow.disabled = tableBody.children.length <= 2;
+  appendColumn.disabled = tableBody.firstElementChild.children.length >= 10;
+  removeColumn.disabled = tableBody.firstElementChild.children.length <= 2;
+});
