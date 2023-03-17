1b687836c2ae689b7f9fa7098f6149a89b07845d
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-17 11:43:15 +0200

    Add, remove cells on cube by click on buttons with javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,86 @@
 'use strict';
 
-// write code here
+const grid = document.querySelector('.field');
+const gridRows = grid.rows;
+const gridBody = document.querySelector('tr').parentNode;
+
+const addCellButton = document.querySelector('.append-column');
+const removeCellButton = document.querySelector('.remove-column');
+const addRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+
+addCellButton.addEventListener('click', () => {
+  [...gridRows].forEach(row => {
+    const newCell = document.createElement('td');
+
+    row.append(newCell);
+  });
+
+  if (gridBody.rows[0].cells.length > 9) {
+    addCellButton.setAttribute('disabled', 'true');
+  }
+
+  if (
+    gridBody.rows[0].cells.length > 2
+    && removeCellButton.hasAttribute('disabled')
+  ) {
+    removeCellButton.removeAttribute('disabled');
+  }
+});
+
+removeCellButton.addEventListener('click', () => {
+  const currentCells = allCells('td');
+  const step = gridBody.rows[0].cells.length;
+
+  for (let i = 0; i < currentCells.length; i += step) {
+    currentCells[i].remove();
+  }
+
+  if (gridBody.rows[0].cells.length < 3) {
+    removeCellButton.setAttribute('disabled', 'true');
+  }
+
+  if (
+    gridBody.rows[0].cells.length < 10
+    && addCellButton.hasAttribute('disabled')
+  ) {
+    addCellButton.removeAttribute('disabled');
+  }
+});
+
+addRowButton.addEventListener('click', () => {
+  const newRow = document.createElement('tr');
+  const currentCellsAmountInRow = gridBody.rows[0].cells.length;
+
+  for (let i = 0; i < currentCellsAmountInRow; i++) {
+    const newCell = document.createElement('td');
+
+    newRow.appendChild(newCell);
+  }
+
+  gridBody.append(newRow);
+
+  if (gridBody.rows.length > 9) {
+    addRowButton.setAttribute('disabled', 'true');
+  }
+
+  if (gridBody.rows.length > 2 && removeRowButton.hasAttribute('disabled')) {
+    removeRowButton.removeAttribute('disabled');
+  }
+});
+
+removeRowButton.addEventListener('click', () => {
+  gridBody.lastElementChild.remove();
+
+  if (gridBody.rows.length < 3) {
+    removeRowButton.setAttribute('disabled', 'true');
+  }
+
+  if (gridBody.rows.length < 10 && addRowButton.hasAttribute('disabled')) {
+    addRowButton.removeAttribute('disabled');
+  }
+});
+
+function allCells(nameOfTag) {
+  return document.querySelectorAll(`${nameOfTag}`);
+}
