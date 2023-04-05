950d7d677b3a98d7a7d8ad6c94025dec36f1a711
metja <mikolapenzanik@gmail.com>
2023-04-05 08:30:52 +0300

    Completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,69 @@
 'use strict';
 
-// write code here
+const tableBody = document.querySelector('tbody');
+const tableItems = tableBody.querySelectorAll('tr');
+
+const maxItems = 10;
+const minItems = 2;
+
+const addRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const addColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+addRow.addEventListener('click', e => {
+  e.preventDefault();
+
+  const currentItems = tableBody.children.length + 1;
+
+  if (currentItems >= maxItems) {
+    addRow.disabled = true;
+  }
+
+  tableBody.append(tableBody.firstChild.cloneNode(true));
+  removeRow.disabled = false;
+});
+
+removeRow.addEventListener('click', e => {
+  e.preventDefault();
+
+  const currentItems = tableBody.children.length - 1;
+
+  if (currentItems <= minItems) {
+    removeRow.disabled = true;
+  }
+
+  tableBody.lastElementChild.remove();
+  addRow.disabled = false;
+});
+
+addColumn.addEventListener('click', e => {
+  e.preventDefault();
+
+  for (const element of tableItems) {
+    const newColumn = document.createElement('td');
+    const currentItems = element.children.length + 1;
+
+    if (currentItems >= maxItems) {
+      addColumn.disabled = true;
+    }
+
+    element.append(newColumn);
+    removeColumn.disabled = false;
+  }
+});
+
+removeColumn.addEventListener('click', e => {
+  e.preventDefault();
+
+  for (const element of tableItems) {
+    const currentItems = element.children.length - 1;
+
+    if (currentItems <= minItems) {
+      removeColumn.disabled = true;
+    }
+
+    element.lastElementChild.remove();
+    addColumn.disabled = false;
+  }
+});
