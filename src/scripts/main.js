f85079fd39f164800d4d53a620c07e3cfda2833c
Anna Peretiatko <tobossanna@gmail.com>
2023-04-08 16:17:29 +0200

    Growth table solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
 // write code here
+const table = document.querySelector('table');
+
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+appendRowButton.addEventListener('click', () => {
+  const rows = table.querySelectorAll('tr');
+  const lastRow = rows[rows.length - 1];
+  const newRow = lastRow.cloneNode(true);
+
+  if (table.rows.length < 10) {
+    table.appendChild(newRow);
+  }
+});
+
+removeRowButton.addEventListener('click', () => {
+  const rows = table.querySelectorAll('tr');
+  const lastRow = rows[rows.length - 1];
+
+  if (table.rows.length > 2) {
+    lastRow.remove();
+  }
+});
+
+appendColumnButton.addEventListener('click', () => {
+  const rows = table.querySelectorAll('tr');
+
+  for (let i = 0; i < rows.length; i++) {
+    const newCell = document.createElement('td');
+
+    if (rows[i].cells.length < 10) {
+      rows[i].appendChild(newCell);
+    }
+  }
+});
+
+removeColumnButton.addEventListener('click', () => {
+  const rows = table.querySelectorAll('tr');
+
+  for (let i = 0; i < rows.length; i++) {
+    const lastCell = table.rows[i].lastElementChild;
+
+    if (rows[i].cells.length > 2) {
+      lastCell.remove();
+    }
+  }
+});
