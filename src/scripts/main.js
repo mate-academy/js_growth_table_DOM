3e361d3beb31f09850f718a0deeafc44ad607015
unknown <vorobetst777@gmail.com>
2023-04-15 20:21:51 +0300

    fixed coments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,44 +2,63 @@
 
 const table = document.querySelector('.field');
 const addRow = document.querySelector('.append-row');
-const remoweRow = document.querySelector('.remove-row');
+const removeRow = document.querySelector('.remove-row');
 const addColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 
-addRow.addEventListener('click', () => {
-  if(table.rows.length === 10) {
-    return;
+function control() {
+  if (table.rows.length === 10) {
+    addRow.disabled = true;
+  } else {
+    addRow.disabled = false;
   }
+  
+  if (table.rows.length === 2) {
+    removeRow.disabled = true;
+  } else {
+    removeRow.disabled = false;
+  }
+  
+  if (table.rows[0].cells.length === 10) {
+    addColumn.disabled = true;
+  } else {
+    addColumn.disabled = false;
+  }
+  
+  if (table.rows[0].cells.length === 2) {
+    removeColumn.disabled = true;
+  } else {
+    removeColumn.disabled = false;
+  }
+}
+
+
+addRow.addEventListener('click', () => {
   const rowToClone = table.rows[0];
   const newRow = rowToClone.cloneNode(true);
+
   table.appendChild(newRow);
+  control();
 });
 
-remoweRow.addEventListener('click', () => {
-  if(table.rows.length === 2) {
-    return;
-  }
+removeRow.addEventListener('click', () => {
   table.deleteRow(0);
-});
+  control();
+})
 
 addColumn.addEventListener('click', () => {
-  if (table.rows[0].cells.length === 10) {
-    return;
+  for (let i = 0; i < table.rows.length; i++) {
+    const cellToClone = table.rows[i].cells[0];
+    const newCell = cellToClone.cloneNode(true);
+
+    table.rows[i].appendChild(newCell);
+    control();
   }
-    for (let i = 0; i < table.rows.length; i++) {
-      const cellToClone = table.rows[i].cells[0];
-      const newCell = cellToClone.cloneNode(true);
-      table.rows[i].appendChild(newCell);
-    }
 });
 
 removeColumn.addEventListener('click', () => {
-  if (table.rows[0].cells.length === 2) {
-    return;
-  };
-
   for (let i = 0; i < table.rows.length; i++) {
-    const lastCellIndex = table.rows[i].cells.length - 1;
     table.rows[i].deleteCell(0);
+    control();
   }
 });
