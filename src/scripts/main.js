9b2dfe27e0692be27ebb7647b5f0339429efb666
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-05-13 21:12:07 +0300

    SOlution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,134 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+
+const apppendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+const tableSize = {
+  width: 4,
+  height: 4,
+};
+
+const buttonsState = {
+  apppendRowButton: 'enabled',
+  removeRowButton: 'enabled',
+  appendColumnButton: 'enabled',
+  removeColumnButton: 'enabled',
+};
+
+apppendRow.addEventListener('click', () => {
+  if (buttonsState.apppendRowButton === 'disabled') {
+    return;
+  }
+
+  tableSize.height++;
+
+  const trClone = table.lastElementChild.cloneNode(true);
+
+  table.append(trClone);
+
+  if (buttonsState.removeRowButton === 'disabled') {
+    buttonsState.removeRowButton = 'enabled';
+    removeRow.disabled = false;
+  }
+
+  if (tableSize.height >= 10) {
+    apppendRow.disabled = true;
+    buttonsState.apppendRowButton = 'disabled';
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  if (buttonsState.removeRowButton === 'disabled') {
+    return;
+  }
+
+  tableSize.height--;
+
+  const trLast = table.lastElementChild;
+
+  if (trLast) {
+    table.removeChild(trLast);
+  }
+
+  if (buttonsState.apppendRowButton === 'disabled') {
+    buttonsState.apppendRowButton = 'enabled';
+    apppendRow.disabled = false;
+  }
+
+  if (tableSize.height <= 2) {
+    removeRow.disabled = true;
+    buttonsState.removeRowButton = 'disabled';
+  }
+});
+
+appendColumn.addEventListener('click', () => {
+  if (buttonsState.appendColumnButton === 'disabled') {
+    return;
+  }
+
+  tableSize.width++;
+
+  [...table.children].forEach((child) => {
+    const td = document.createElement('td');
+
+    child.append(td);
+  });
+
+  if (buttonsState.removeColumnButton === 'disabled') {
+    buttonsState.removeColumnButton = 'enabled';
+    removeColumn.disabled = false;
+  }
+
+  if (tableSize.width >= 10) {
+    appendColumn.disabled = true;
+    buttonsState.appendColumnButton = 'disabled';
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  if (buttonsState.removeColumnButton === 'disabled') {
+    return;
+  }
+
+  tableSize.width--;
+
+  [...table.children].forEach((child) => {
+    const tdLast = child.lastElementChild;
+
+    if (tdLast) {
+      child.removeChild(tdLast);
+    }
+  });
+
+  if (buttonsState.appendColumnButton === 'disabled') {
+    buttonsState.appendColumnButton = 'enabled';
+    appendColumn.disabled = false;
+  }
+
+  if (tableSize.width <= 2) {
+    removeColumn.disabled = true;
+    buttonsState.removeColumnButton = 'disabled';
+  }
+});
+
+// in case we need to rerender the table
+// eslint-disable-next-line
+function render() {
+  table.innerHTML = '';
+
+  for (let i = 0; i < 20; i++) {
+    const tr = document.createElement('tr');
+
+    for (let j = 0; j < 20; j++) {
+      const td = document.createElement('td');
+
+      tr.append(td);
+    }
+
+    table.append(tr);
+  }
+}
