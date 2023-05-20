b6c8e2d56c2fb8df0322338f184c28efc6bf7405
Yelyzaveta <molochiza@gmail.com>
2023-05-20 16:43:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,73 @@
 'use strict';
 
-// write code here
+const addRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const addColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+const table = document.querySelector('table');
+let rows = document.querySelectorAll('tr');
+const max = 10;
+const min = 2;
+let rowsNumber = rows.length;
+let columnsNumber = rows[0].children.length;
+
+addRow.addEventListener('click', () => {
+  const row = rows[0].cloneNode(true);
+
+  table.append(row);
+  rows = document.querySelectorAll('tr');
+  rowsNumber++;
+
+  if (rowsNumber === max) {
+    addRow.disabled = true;
+  }
+
+  if (rowsNumber > min) {
+    removeRow.disabled = false;
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  table.lastChild.remove();
+  rows = document.querySelectorAll('tr');
+  rowsNumber--;
+
+  if (rowsNumber === min) {
+    removeRow.disabled = true;
+  }
+
+  if (rowsNumber < max) {
+    addRow.disabled = false;
+  }
+});
+
+addColumn.addEventListener('click', () => {
+  rows.forEach(el => {
+    const cell = document.createElement('td');
+
+    el.append(cell);
+  });
+  columnsNumber++;
+
+  if (columnsNumber >= max) {
+    addColumn.disabled = true;
+  }
+
+  if (columnsNumber < min) {
+    removeColumn.disabled = false;
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  rows.forEach(el => el.lastElementChild.remove());
+  columnsNumber--;
+
+  if (columnsNumber <= min) {
+    removeColumn.disabled = true;
+  }
+
+  if (columnsNumber < max) {
+    addColumn.disabled = false;
+  }
+});
