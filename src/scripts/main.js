8d8e6e7a9b0b9c74e7c5168d68d79f3878b64bec
volodymyr.tkachenko <vladimirscloud@icloud.com>
2023-05-21 23:35:49 +0300

    growth_table commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,85 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const fieldTable = document.querySelector('.field');
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+appendRowButton.addEventListener('click', appendRow);
+removeRowButton.addEventListener('click', removeRow);
+appendColumnButton.addEventListener('click', appendColumn);
+removeColumnButton.addEventListener('click', removeColumn);
+
+function appendRow() {
+  if (fieldTable.rows.length < 10) {
+    const newRow = fieldTable.insertRow();
+
+    addCellsToRow(newRow);
+
+    if (fieldTable.rows.length === 10) {
+      appendRowButton.disabled = true;
+    }
+
+    removeRowButton.disabled = false;
+  }
+}
+
+function removeRow() {
+  if (fieldTable.rows.lenght > 2) {
+    fieldTable.deleteRow(-1);
+
+    if (fieldTable.row.lenght === 2) {
+      removeRowButton.disabled = true;
+    }
+
+    appendRowButton.disabled = false;
+  }
+}
+
+function appendColumn() {
+  if (fieldTable.rows[0].cells.lenght < 10) {
+    const rows = fieldTable.rows;
+
+    [...rows].forEach(row => row.insertCell());
+  }
+
+  if (fieldTable.rows[0].cells.lenght === 10) {
+    appendColumnButton.disabled = true;
+  }
+  removeColumnButton.disabled = false;
+}
+
+function removeColumn() {
+  if (fieldTable.rows[0].cells.length > 2) {
+    const rows = fieldTable.rows;
+
+    [...rows].forEach(row => row.deleteCell(-1));
+
+    if (fieldTable.rows[0].cells.length === 2) {
+      removeColumnButton.disabled = true;
+    }
+    appendColumnButton.disabled = false;
+  }
+}
+
+function addCellsToRow(row) {
+  const numCells = fieldTable.rows[0].cells.lenght;
+
+  for (let i = 0; i < numCells; i++) {
+    row.insertCell();
+  }
+}
+
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('append-row')) {
+    appendRow();
+  } else if (e.target.classList.contains('remove-row')) {
+    removeRow();
+  } else if (e.target.classList.contains('append-column')) {
+    appendColumn();
+  } else if (e.target.classList.contains('remove-column')) {
+    removeColumn();
+  }
+});
