23248bd2d6c3d47f3acc015f6d04ec6ee9bc5433
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-05-24 22:25:42 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,52 +5,47 @@ const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 
-let tableRow = document.querySelectorAll('tr');
+const table = document.querySelectorAll('.field')[0].children;
+const tableRow = table[0].children;
 
-const maxCount = 10;
-const minCount = 2;
+const MAX_COUNT = 10;
+const MIN_COUNT = 2;
 
 let countRow = tableRow.length;
 let countColumn = tableRow[0].children.length;
 
 appendRow.addEventListener('click', e => {
-  if (countRow < maxCount) {
+  if (countRow < MAX_COUNT) {
     countRow++;
     removeRow.disabled = false;
-
-    tableRow = document.querySelectorAll('tr');
 
     const clone = tableRow[0].cloneNode(true);
 
     tableRow[tableRow.length - 1].after(clone);
 
-    if (countRow === maxCount) {
+    if (countRow === MAX_COUNT) {
       appendRow.disabled = true;
     }
   }
 });
 
 removeRow.addEventListener('click', e => {
-  if (countRow > minCount) {
+  if (countRow > MIN_COUNT) {
     countRow--;
     appendRow.disabled = false;
 
-    tableRow = document.querySelectorAll('tr');
-
     tableRow[tableRow.length - 1].remove();
 
-    if (countRow === minCount) {
+    if (countRow === MIN_COUNT) {
       removeRow.disabled = true;
     }
   }
 });
 
 appendColumn.addEventListener('click', e => {
-  if (countColumn < maxCount) {
+  if (countColumn < MAX_COUNT) {
     countColumn++;
     removeColumn.disabled = false;
-
-    tableRow = document.querySelectorAll('tr');
 
     for (const row of tableRow) {
       const clone = tableRow[0].firstElementChild.cloneNode(true);
@@ -58,24 +53,22 @@ appendColumn.addEventListener('click', e => {
       row.append(clone);
     }
 
-    if (countColumn === maxCount) {
+    if (countColumn === MAX_COUNT) {
       appendColumn.disabled = true;
     }
   }
 });
 
 removeColumn.addEventListener('click', e => {
-  if (countColumn > minCount) {
+  if (countColumn > MIN_COUNT) {
     countColumn--;
     appendColumn.disabled = false;
-
-    tableRow = document.querySelectorAll('tr');
 
     for (const row of tableRow) {
       row.lastElementChild.remove();
     }
 
-    if (countColumn === minCount) {
+    if (countColumn === MIN_COUNT) {
       removeColumn.disabled = true;
     }
   }
