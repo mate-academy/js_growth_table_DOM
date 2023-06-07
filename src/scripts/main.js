4691d38a20283cc86a60fdb344e45244106bae94
Anton <crossnaders4949@gmail.com>
2023-06-07 20:43:58 +0200

    Fixed mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,54 +14,45 @@ container.addEventListener('click', (e) => {
   const item = e.target;
 
   if (item.classList.contains('append-row')) {
-    if (mainRow.length > 9) {
-      item.setAttribute('disabled', 'disabled');
-
-      return;
-    } else {
-      if (removeRow.hasAttribute('disabled')) {
-        removeRow.removeAttribute('disabled');
-      }
-
+    if (mainRow.length <= 10) {
       const tr = document.createElement('tr');
 
       table.children[0].append(tr);
-
-      if (mainRow.length >= 9) {
-        item.setAttribute('disabled', 'disabled');
-      }
 
       for (let i = 0; i < tds.length; i++) {
         const td = document.createElement('td');
 
         tr.append(td);
       }
+
+      if (mainRow.length >= 9) {
+        item.setAttribute('disabled', 'disabled');
+      }
+
+      if (removeRow.hasAttribute('disabled')) {
+        removeRow.removeAttribute('disabled');
+      }
     }
   }
 
   if (item.classList.contains('remove-row')) {
-    if (mainRow.length <= 2) {
-      item.setAttribute('disabled', 'disabled');
+    if (mainRow.length > 2) {
+      table.deleteRow(mainRow.length - 1);
 
-      return;
-    } else {
       if (appendRow.hasAttribute('disabled')) {
         appendRow.removeAttribute('disabled');
       }
-      table.children[0].children[0].remove();
+
+      const countRows = Array.from(table.rows);
+
+      if (countRows.length <= 2) {
+        item.setAttribute('disabled', 'disabled');
+      }
     }
-  }
+  };
 
   if (item.classList.contains('append-column')) {
-    if (tds.length > 9) {
-      item.setAttribute('disabled', 'disabled');
-
-      return;
-    } else {
-      if (removeColumn.hasAttribute('disabled')) {
-        removeColumn.removeAttribute('disabled');
-      }
-
+    if (tds.length <= 10) {
       const rows = document.querySelectorAll('tr');
 
       for (const row of rows) {
@@ -69,21 +60,31 @@ container.addEventListener('click', (e) => {
 
         row.append(td);
       }
+
+      if (tds.length > 9) {
+        item.setAttribute('disabled', 'disabled');
+      }
+
+      if (removeColumn.hasAttribute('disabled')) {
+        removeColumn.removeAttribute('disabled');
+      }
     }
   }
 
   if (item.classList.contains('remove-column')) {
-    if (tds.length <= 2) {
-      item.setAttribute('disabled', 'disabled');
-    } else {
-      if (appendColumn.hasAttribute('disabled')) {
-        appendColumn.removeAttribute('disabled');
-      }
-
+    if (tds.length > 2) {
       const rows = document.querySelectorAll('tr');
 
       for (const row of rows) {
-        row.children[tds.length - 1].remove();
+        row.deleteCell(tds.length - 1);
+      }
+
+      if (tds.length <= 2) {
+        item.setAttribute('disabled', 'disabled');
+      }
+
+      if (appendColumn.hasAttribute('disabled')) {
+        appendColumn.removeAttribute('disabled');
       }
     }
   }
