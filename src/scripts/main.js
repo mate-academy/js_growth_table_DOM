743ad3b0c7458030ad1b4d0ee13defee2da2e26f
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-08-10 18:24:18 +0300

    requested changes were made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,65 +1,92 @@
 'use strict';
 
-const container = document.querySelector('.container');
-const tBody = document.querySelector('.field').firstElementChild;
-const rows = tBody.children;
+const tableBody = document.querySelector('.field').firstElementChild;
+const buttons = document.querySelectorAll('button');
+const rowAdder = buttons[0];
+const rowRemover = buttons[1];
+const columnAdder = buttons[2];
+const columnRemover = buttons[3];
+const rows = tableBody.children;
 
-const appendRowBtn = document.querySelector('append-row, button');
-const removeRowBtn = document.querySelector('remove-row, button');
-const appendColumnBtn = document.querySelector('append-column, button');
-const removeColumnBtn = document.querySelector('remove-column, button');
+function removeColumn(row) {
+  row.lastElementChild.remove();
+}
 
-container.addEventListener('click', e => {
-  let rowLength = tBody.lastElementChild.rowIndex + 1;
-  let columnLength = tBody.firstElementChild.lastElementChild.cellIndex + 1;
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    let rowLength = tableBody.lastElementChild.rowIndex + 1;
+    let columnLength = tableBody
+      .firstElementChild.lastElementChild.cellIndex + 1;
 
-  if (e.target.className === 'append-row button') {
-    rowLength++;
+    if (e.target.className === 'append-row button') {
+      const rowToClone = tableBody.firstElementChild;
+      const newRow = rowToClone.cloneNode(true);
 
-    const rowToClone = tBody.firstElementChild;
-    const newRow = rowToClone.cloneNode(true);
+      if (rowLength < 10) {
+        rowLength++;
+        tableBody.appendChild(newRow);
+      }
 
-    if (rowLength === 10) {
-      appendRowBtn.disabled = true;
+      if (rowLength > 2) {
+        rowRemover.disabled = false;
+      }
+
+      if (rowLength === 10) {
+        e.target.disabled = true;
+      }
+    } else if (e.target.className === 'remove-row button') {
+      if (rowLength > 2) {
+        rowLength--;
+        tableBody.lastElementChild.remove();
+      }
+
+      if (rowLength < 10) {
+        rowAdder.disabled = false;
+      }
+
+      if (rowLength === 2) {
+        e.target.disabled = true;
+      }
+    } else if (e.target.className === 'append-column button') {
+      if (columnLength < 10) {
+        columnLength++;
+      }
+
+      for (const row of rows) {
+        const newCell = row.firstElementChild.cloneNode(true);
+
+        if (columnLength < 10) {
+          row.appendChild(newCell);
+        }
+
+        if (columnLength > 2) {
+          columnRemover.disabled = false;
+        }
+
+        if (columnLength === 10) {
+          row.appendChild(newCell);
+          e.target.disabled = true;
+        }
+      }
+    } else {
+      if (columnLength > 2) {
+        columnLength--;
+      }
+
+      for (const row of rows) {
+        if (columnLength > 2) {
+          removeColumn(row);
+        }
+
+        if (columnLength < 10) {
+          columnAdder.disabled = false;
+        }
+
+        if (columnLength === 2) {
+          removeColumn(row);
+          e.target.disabled = true;
+        }
+      }
     }
-
-    tBody.appendChild(newRow);
-  } else if (e.target.className === 'remove-row button') {
-    rowLength--;
-
-    if (rowLength === 2) {
-      removeRowBtn.disabled = true;
-    }
-    tBody.lastElementChild.remove();
-
-    if (rowLength < 10) {
-      appendRowBtn.disabled = false;
-    }
-  } else if (e.target.className === 'append-column button') {
-    columnLength++;
-
-    if (columnLength === 10) {
-      appendColumnBtn.disabled = true;
-    }
-
-    for (const row of rows) {
-      const newCell = row.firstElementChild.cloneNode();
-
-      row.appendChild(newCell);
-    }
-  } else {
-    columnLength--;
-
-    if (columnLength === 2) {
-      removeColumnBtn.disabled = true;
-    }
-
-    for (const row of rows) {
-      row.lastElementChild.remove();
-    }
-
-    if (columnLength < 10) {
-      appendColumnBtn.disabled = false;
-    }
-  }
+  });
 });
