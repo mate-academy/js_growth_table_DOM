672a98406f53bd30f5f55550c22a3c70e3bbf811
Oleksii Lavka <lavka.alexey37gmail.com>
2023-08-14 14:25:24 +0300

    fixed mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,9 @@ const buttons = [...document.querySelectorAll('.button')];
 const table = document.querySelector('.field');
 const [tableBody] = table.tBodies;
 
+let rowsCount = table.rows.length;
+let columnsCount = table.rows[0].childElementCount;
+
 const MAX = 10;
 const MIN = 2;
 
@@ -14,18 +17,36 @@ const APPEND_COLUMN = 'append-column';
 const REMOVE_COLUMN = 'remove-column';
 
 const appendRow = () => {
+  if ((rowsCount) > MAX) {
+    return;
+  }
+
+  rowsCount++;
+
   const newRow = tableBody.lastElementChild.cloneNode(true);
 
   tableBody.append(newRow);
 };
 
 const removeRow = () => {
+  if ((rowsCount - 1) < MIN) {
+    return;
+  }
+
+  rowsCount--;
+
   const rowToDelete = tableBody.lastElementChild;
 
   rowToDelete.remove();
 };
 
 const appendColumn = () => {
+  if ((columnsCount + 1) > MAX) {
+    return;
+  }
+
+  columnsCount++;
+
   [...table.rows].forEach(row => {
     const newCell = row.lastElementChild.cloneNode(true);
 
@@ -34,33 +55,34 @@ const appendColumn = () => {
 };
 
 const removeColumn = () => {
+  if ((columnsCount - 1) < MIN) {
+    return;
+  }
+
+  columnsCount--;
+
   [...table.rows].forEach(row => {
     row.lastElementChild.remove();
   });
 };
 
-const isTargerButton = (item, className) => {
-  return item.classList.contains(className);
-};
+const isTargetButton = (item, className) => item.classList.contains(className);
 
 const checkButtons = () => {
-  const rowsCount = table.rows.length;
-  const columnsCount = table.rows[0].childElementCount;
-
   buttons.forEach((button) => {
-    if (isTargerButton(button, APPEND_ROW)) {
+    if (isTargetButton(button, APPEND_ROW)) {
       button.disabled = rowsCount >= MAX;
     }
 
-    if (isTargerButton(button, REMOVE_ROW)) {
+    if (isTargetButton(button, REMOVE_ROW)) {
       button.disabled = rowsCount <= MIN;
     }
 
-    if (isTargerButton(button, APPEND_COLUMN)) {
+    if (isTargetButton(button, APPEND_COLUMN)) {
       button.disabled = columnsCount >= MAX;
     }
 
-    if (isTargerButton(button, REMOVE_COLUMN)) {
+    if (isTargetButton(button, REMOVE_COLUMN)) {
       button.disabled = columnsCount <= MIN;
     }
   });
@@ -71,24 +93,26 @@ const growthTable = (event) => {
   const targetItem = event.target;
   const button = targetItem.closest('.button');
 
+  const checkButton = (className) => isTargetButton(button, className);
+
   if (!button || button.disabled) {
     return;
   }
 
   switch (true) {
-    case isTargerButton(button, APPEND_ROW):
+    case checkButton(APPEND_ROW):
       appendRow();
       break;
 
-    case isTargerButton(button, REMOVE_ROW):
+    case checkButton(REMOVE_ROW):
       removeRow();
       break;
 
-    case isTargerButton(button, APPEND_COLUMN):
+    case checkButton(APPEND_COLUMN):
       appendColumn();
       break;
 
-    case isTargerButton(button, REMOVE_COLUMN):
+    case checkButton(REMOVE_COLUMN):
       removeColumn();
       break;
 
@@ -98,7 +122,5 @@ const growthTable = (event) => {
 
   checkButtons();
 };
-
-checkButtons();
 
 container.addEventListener('click', growthTable);
