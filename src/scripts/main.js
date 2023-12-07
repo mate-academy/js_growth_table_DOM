2c65eb5181bb7203ebff3cd1dc136382025f60dd
Юрий Мисюра <apriory13@meta.ua>
2023-12-07 13:20:40 +0200

    fixed buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,43 +3,58 @@
 const TABLE_MAX_COUNT_LENGTH = 10;
 const TABLE_MIN_COUNT_LENGTH = 2;
 
-const buttons = document.querySelectorAll('.button');
+const buttons = {
+  appendRowButton: document.querySelector('.append-row'),
+  removeRowButton: document.querySelector('.remove-row'),
+  appendColumnButton: document.querySelector('.append-column'),
+  removeColumnButton: document.querySelector('.remove-column'),
+};
+
 const table = document.querySelector('.field');
 const rows = table.getElementsByTagName('tr');
 const columns = rows[0].getElementsByTagName('td');
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    modifyTable(button.classList[0]);
+Object.keys(buttons).forEach(buttonKey => {
+  const targetButton = buttons[buttonKey];
+
+  targetButton.addEventListener('click', () => {
+    modifyTable(targetButton);
+    isButtonDisabled();
   });
 });
 
-function modifyTable(action) {
-  const targetButton
-    = [...buttons].find(button => button.classList.contains(action));
+function modifyTable(targetButton) {
+  const action = targetButton.classList[0];
 
   switch (action) {
     case 'append-row':
       table.append(rows[0].cloneNode(true));
-      targetButton.disabled = rows.length >= TABLE_MAX_COUNT_LENGTH;
       break;
 
     case 'remove-row':
       rows[rows.length - 1].remove();
-      targetButton.disabled = rows.length <= TABLE_MIN_COUNT_LENGTH;
       break;
 
     case 'append-column':
       [...rows].forEach(row => row.append(columns[0].cloneNode(true)));
-      targetButton.disabled = columns.length >= TABLE_MAX_COUNT_LENGTH;
       break;
 
     case 'remove-column':
       [...rows].forEach(row => row.children[columns.length - 1].remove());
-      targetButton.disabled = columns.length <= TABLE_MIN_COUNT_LENGTH;
       break;
 
     default:
       break;
   }
 }
+
+function isButtonDisabled() {
+  buttons.appendRowButton.disabled = rows.length >= TABLE_MAX_COUNT_LENGTH;
+  buttons.removeRowButton.disabled = rows.length <= TABLE_MIN_COUNT_LENGTH;
+
+  buttons.appendColumnButton.disabled
+    = columns.length >= TABLE_MAX_COUNT_LENGTH;
+
+  buttons.removeColumnButton.disabled
+    = columns.length <= TABLE_MIN_COUNT_LENGTH;
+};
