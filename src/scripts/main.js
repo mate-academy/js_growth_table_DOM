e51a5b26ad5d274edb56db918568820d97836bd7
andrij <34485522+deezhizyu@users.noreply.github.com>
2024-06-15 19:56:26 +0200

    Use constants instead of hardcoded values

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,11 +8,14 @@ const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 
+const MAX_TABLE_SIZE = 10;
+const MIN_TABLE_SIZE = 2;
+
 container.addEventListener('click', () => {
-  appendRow.disabled = tbody.children.length >= 10;
-  removeRow.disabled = tbody.children.length <= 2;
-  appendColumn.disabled = tbody.firstElementChild.children.length >= 10;
-  removeColumn.disabled = tbody.firstElementChild.children.length <= 2;
+  appendRow.disabled = tbody.children.length >= MAX_TABLE_SIZE;
+  removeRow.disabled = tbody.children.length <= MIN_TABLE_SIZE;
+  appendColumn.disabled = tbody.firstElementChild.children.length >= MAX_TABLE_SIZE;
+  removeColumn.disabled = tbody.firstElementChild.children.length <= MIN_TABLE_SIZE;
 });
 
 appendRow.addEventListener('click', () => {
