ebfdff379fdc1ef6089e8c6e67d6f4bcf2abc1c0
deezhizyu <kalenskuja@gmail.com>
2024-06-15 14:35:06 +0200

    Implement task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const tbody = document.querySelector('table tbody');
+
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+container.addEventListener('click', () => {
+  appendRow.disabled = tbody.children.length >= 10;
+  removeRow.disabled = tbody.children.length <= 2;
+  appendColumn.disabled = tbody.firstElementChild.children.length >= 10;
+  removeColumn.disabled = tbody.firstElementChild.children.length <= 2;
+});
+
+appendRow.addEventListener('click', () => {
+  tbody.append(tbody.firstElementChild.cloneNode(true));
+});
+
+removeRow.addEventListener('click', () => {
+  tbody.firstElementChild.remove();
+});
+
+appendColumn.addEventListener('click', (e) => {
+  for (const child of tbody.children) {
+    child.append(child.firstElementChild.cloneNode(true));
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  for (const child of tbody.children) {
+    child.firstElementChild.remove();
+  }
+});
