3e96a58b8d6e01d9b8aa4c75093feeb0e05106d6
Hanna Svirina <26anna07@ukr.net>
2024-06-19 00:15:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
-// write code here
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+const table = document.querySelector('.field');
+
+function isDisabledButton() {
+  if (table.getElementsByTagName('tr').length >= 10) {
+    appendRowButton.disabled = true;
+  }
+
+  if (table.getElementsByTagName('tr').length <= 2) {
+    removeRowButton.disabled = true;
+  }
+
+  if (table.rows[0].cells.length >= 10) {
+    appendColumnButton.disabled = true;
+  }
+
+  if (table.rows[0].cells.length <= 2) {
+    removeColumnButton.disabled = true;
+  }
+}
+
+appendRowButton.addEventListener('click', () => {
+  const newTr = table.insertRow();
+
+  for (let i = 0; i < table.rows[0].cells.length; i++) {
+    newTr.insertCell(i);
+  }
+
+  isDisabledButton();
+});
+
+appendColumnButton.addEventListener('click', () => {
+  for (const row of table.rows) {
+    row.insertCell(row.cells.length);
+  }
+
+  isDisabledButton();
+});
+
+removeRowButton.addEventListener('click', () => {
+  table.deleteRow(table.rows.length - 1);
+
+  isDisabledButton();
+});
+
+removeColumnButton.addEventListener('click', () => {
+  for (const row of table.rows) {
+    row.deleteCell(row.cells.length - 1);
+  }
+
+  isDisabledButton();
+});
