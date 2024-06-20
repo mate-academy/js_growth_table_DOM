9fdb29d69b8482bff35a38f48a23a24fec78c5fd
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-20 16:11:40 +0300

    [Feature] added growth table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,89 @@
 'use strict';
 
-// write code here
+const MIN_LENGTH = 2;
+const MAX_LENGTH = 10;
+
+const table = document.querySelector('.field');
+const tBody = table.querySelector('tbody');
+
+const addRowBtn = document.querySelector('.append-row');
+const removeRowBtn = document.querySelector('.remove-row');
+const addColumnBtn = document.querySelector('.append-column');
+const removeColumnBtn = document.querySelector('.remove-column');
+
+function onAddRow(rows) {
+  const [currentRow] = rows;
+  const currentRowClone = currentRow.cloneNode(true);
+
+  tBody.append(currentRowClone);
+
+  if (rows.length >= MAX_LENGTH) {
+    addRowBtn.setAttribute('disabled', true);
+  }
+
+  if (rows.length > MIN_LENGTH) {
+    removeRowBtn.removeAttribute('disabled');
+  }
+}
+
+function onRemoveRow(rows) {
+  const [currentRow] = rows;
+
+  currentRow.remove();
+
+  if (rows.length <= MIN_LENGTH) {
+    removeRowBtn.setAttribute('disabled', true);
+  }
+
+  if (rows.length < MAX_LENGTH) {
+    addRowBtn.removeAttribute('disabled');
+  }
+}
+
+function onAddColumn(rows) {
+  [...rows].forEach((row) => {
+    row.insertCell();
+  });
+
+  const cellLength = rows[0].cells.length;
+
+  if (cellLength >= MAX_LENGTH) {
+    addColumnBtn.setAttribute('disabled', true);
+  }
+
+  if (cellLength > MIN_LENGTH) {
+    removeColumnBtn.removeAttribute('disabled');
+  }
+}
+
+function onRemoveColumn(rows) {
+  [...rows].forEach((row) => {
+    row.deleteCell(0);
+  });
+
+  const cellLength = rows[0].cells.length;
+
+  if (cellLength <= MIN_LENGTH) {
+    removeColumnBtn.setAttribute('disabled', true);
+  }
+
+  if (cellLength < MAX_LENGTH) {
+    addColumnBtn.removeAttribute('disabled');
+  }
+}
+
+addRowBtn.addEventListener('click', () => {
+  onAddRow(tBody.rows);
+});
+
+removeRowBtn.addEventListener('click', () => {
+  onRemoveRow(tBody.rows);
+});
+
+addColumnBtn.addEventListener('click', () => {
+  onAddColumn(tBody.rows);
+});
+
+removeColumnBtn.addEventListener('click', () => {
+  onRemoveColumn(tBody.rows);
+});
