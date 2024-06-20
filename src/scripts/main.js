5987ce7a2cf91534437b1ba01436e23656739ce5
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-21 00:44:38 +0200

    add function update button states

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,54 +4,42 @@ const container = document.querySelector('.container');
 const field = container.querySelector('.field');
 const rows = field.rows;
 
-const addRowButton = document.querySelector('.append-row');
-const removeRowButton = document.querySelector('.remove-row');
-const addColumn = document.querySelector('.append-column');
-const removeColumnButton = document.querySelector('.remove-column');
+const buttons = {
+  addRowButton: document.querySelector('.append-row'),
+  removeRowButton: document.querySelector('.remove-row'),
+  addColumn: document.querySelector('.append-column'),
+  removeColumnButton: document.querySelector('.remove-column'),
+};
+
+const updateButtonsStates = () => {
+  const rowCount = rows.length;
+  const colCount = rows[0].cells.length;
+
+  buttons.addRowButton.disabled = rowCount >= 10;
+  buttons.removeRowButton.disabled = rowCount <= 2;
+  buttons.addColumn.disabled = colCount >= 10;
+  buttons.removeColumnButton.disabled = colCount <= 2;
+};
 
 container.addEventListener('click', (ev) => {
   const tBody = field.tBodies[0];
 
-  switch (ev.target.classList[0]) {
-    case 'append-row':
-      const newRow = rows[0].cloneNode(true);
-
-      tBody.append(newRow);
+  switch (ev.target) {
+    case buttons.addRowButton:
+      tBody.append(rows[0].cloneNode(true));
       break;
 
-    case 'remove-row':
+    case buttons.removeRowButton:
       tBody.lastElementChild.remove();
       break;
 
-    case 'append-column':
+    case buttons.addColumn:
       [...rows].forEach((row) => row.append(document.createElement('td')));
       break;
 
-    case 'remove-column':
+    case buttons.removeColumnButton:
       [...rows].forEach((row) => row.lastElementChild.remove());
   }
 
-  if (rows.length >= 10) {
-    addRowButton.disabled = true;
-  } else {
-    addRowButton.disabled = false;
-  }
-
-  if (rows.length <= 2) {
-    removeRowButton.disabled = true;
-  } else {
-    removeRowButton.disabled = false;
-  }
-
-  if (rows[0].cells.length >= 10) {
-    addColumn.disabled = true;
-  } else {
-    addColumn.disabled = false;
-  }
-
-  if (rows[0].cells.length <= 2) {
-    removeColumnButton.disabled = true;
-  } else {
-    removeColumnButton.disabled = false;
-  }
+  updateButtonsStates();
 });
