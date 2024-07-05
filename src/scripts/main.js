7ae6636436c7ab05af3e09f5fd58d6879bcc4941
Marian <getmoodyup@gmail.com>
2024-07-06 01:29:54 +0300

    growth table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,62 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const newRowBtn = document.querySelector('.append-row');
+const deleteRowBtn = document.querySelector('.remove-row');
+const newColBtn = document.querySelector('.append-column');
+const deleteColBtn = document.querySelector('.remove-column');
+
+const max = 10;
+const min = 2;
+
+function updateTable() {
+  const rowAmount = table.rows.length;
+  const colAmount = table.rows[0].cells.length;
+
+  newRowBtn.disabled = rowAmount >= max;
+  deleteRowBtn.disabled = rowAmount <= min;
+  newColBtn.disabled = colAmount >= max;
+  deleteColBtn.disabled = colAmount <= min;
+}
+
+function addRow() {
+  const newRow = table.insertRow();
+  const colAmount = table.rows[0].cells.length;
+
+  for (let i = 0; i < colAmount; i++) {
+    newRow.insertCell();
+  }
+
+  updateTable();
+}
+
+function removeRow() {
+  if (table.rows.length > min) {
+    table.deleteRow(-1);
+  }
+
+  updateTable();
+}
+
+function addCol() {
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].insertCell();
+  }
+
+  updateTable();
+}
+
+function removeCol() {
+  if (table.rows[0].cells.length > min) {
+    for (let i = 0; i < table.rows.length; i++) {
+      table.rows[i].deleteCell(-1);
+    }
+  }
+
+  updateTable();
+}
+
+newRowBtn.addEventListener('click', addRow);
+deleteRowBtn.addEventListener('click', removeRow);
+newColBtn.addEventListener('click', addCol);
+deleteColBtn.addEventListener('click', removeCol);
