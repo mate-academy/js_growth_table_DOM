d349f2fd707e6da8a81abb2ec132442c1f0f36f4
Seva Podolskiy <s_podolskiy@live.com>
2024-07-10 09:22:23 -0400

    removed redundant Ifs and replace switch with if to implement PR comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,63 +3,40 @@
 const container = document.querySelector('.container');
 
 container?.addEventListener('click', (ev) => {
-  const table = document.querySelector('.field');
+  const table = document.querySelector('tbody');
   const btAppendRow = container.querySelector('.append-row');
   const btDelRow = container.querySelector('.remove-row');
   const btAppendCol = container.querySelector('.append-column');
   const btDelCol = container.querySelector('.remove-column');
 
-  switch (ev.target.classList[0]) {
-    case 'append-row':
-      if (table?.rows?.length < 10) {
-        table.append(table.rows[0].cloneNode(true));
-      }
-      break;
-
-    case 'remove-row':
-      if (table.rows.length > 2) {
-        table.rows[0].remove();
-      }
-      break;
-
-    case 'append-column':
-      if (table?.rows[0]?.cells.length < 10) {
-        for (let col = 0; col < table.rows.length; col++) {
-          table.rows[col].append(table.rows[col].cells[0].cloneNode(true));
-        }
-      }
-      break;
-
-    case 'remove-column':
-      if (table?.rows[0]?.cells.length > 2) {
-        for (let col = 0; col < table.rows.length; col++) {
-          table.rows[col].cells[0].remove();
-        }
-      }
-      break;
+  if (ev.target.classList.contains('append-row') && table?.rows?.length < 10) {
+    table.append(table.rows[0].cloneNode(true));
   }
 
-  if (table?.rows?.length < 10) {
-    btAppendRow.disabled = false;
-  } else {
-    btAppendRow.disabled = true;
+  if (ev.target.classList.contains('remove-row') && table.rows.length > 2) {
+    table.rows[0].remove();
   }
 
-  if (table.rows.length > 2) {
-    btDelRow.disabled = false;
-  } else {
-    btDelRow.disabled = true;
+  if (
+    ev.target.classList.contains('append-column') &&
+    table?.rows[0]?.cells.length < 10
+  ) {
+    for (let col = 0; col < table.rows.length; col++) {
+      table.rows[col].append(table.rows[col].cells[0].cloneNode(true));
+    }
   }
 
-  if (table?.rows[0]?.cells.length < 10) {
-    btAppendCol.disabled = false;
-  } else {
-    btAppendCol.disabled = true;
+  if (
+    ev.target.classList.contains('remove-column') &&
+    table?.rows[0]?.cells.length > 2
+  ) {
+    for (let col = 0; col < table.rows.length; col++) {
+      table.rows[col].cells[0].remove();
+    }
   }
 
-  if (table?.rows[0]?.cells.length > 2) {
-    btDelCol.disabled = false;
-  } else {
-    btDelCol.disabled = true;
-  }
+  btAppendRow.disabled = table?.rows?.length >= 10;
+  btDelRow.disabled = table.rows.length <= 2;
+  btAppendCol.disabled = table?.rows[0]?.cells.length >= 10;
+  btDelCol.disabled = table?.rows[0]?.cells.length <= 2;
 });
