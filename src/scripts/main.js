eec6438fc4b77f0e52147924e658dfb6be8d548d
Roman Perepelkin <perepelkinrs@gmail.com>
2024-07-12 08:55:05 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,58 +1,51 @@
 'use strict';
 
-const buttons = [...document.querySelectorAll('.button')];
-const container = document.querySelector('.container');
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
 const table = document.querySelector('table');
 
 const buttonUpdate = () => {
   const rowsLength = table.rows.length;
   const columnsLength = table.rows[0].cells.length;
 
-  buttons[0].disabled = rowsLength >= 10;
-  buttons[1].disabled = rowsLength <= 2;
-  buttons[2].disabled = columnsLength >= 10;
-  buttons[3].disabled = columnsLength <= 2;
+  appendRowButton.disabled = rowsLength >= 10;
+  removeRowButton.disabled = rowsLength <= 2;
+  appendColumnButton.disabled = columnsLength >= 10;
+  removeColumnButton.disabled = columnsLength <= 2;
 };
 
-container.addEventListener('click', (e) => {
-  const button = buttons.indexOf(e.target);
+appendRowButton.addEventListener('click', (e) => {
+  const tr = document.createElement('tr');
+  const tdLength = table.rows[0].cells.length;
 
-  switch (button) {
-    case 0:
-      const tr = document.createElement('tr');
-      const tdLength = table.rows[0].cells.length;
+  for (let i = 0; i < tdLength; i++) {
+    const td = document.createElement('td');
 
-      for (let i = 0; i < tdLength; i++) {
-        const td = document.createElement('td');
-
-        tr.appendChild(td);
-      }
-      table.appendChild(tr);
-      buttonUpdate();
-      break;
-
-    case 1:
-      table.deleteRow(-1);
-      buttonUpdate();
-      break;
-
-    case 2:
-      for (const row of table.rows) {
-        const td = document.createElement('td');
-
-        row.appendChild(td);
-      }
-      buttonUpdate();
-      break;
-
-    case 3:
-      for (const row of table.rows) {
-        row.deleteCell(-1);
-      }
-      buttonUpdate();
-      break;
-
-    default:
-      break;
+    tr.appendChild(td);
   }
+  table.appendChild(tr);
+  buttonUpdate();
+});
+
+removeRowButton.addEventListener('click', (e) => {
+  table.deleteRow(-1);
+  buttonUpdate();
+});
+
+appendColumnButton.addEventListener('click', (e) => {
+  for (const row of table.rows) {
+    const td = document.createElement('td');
+
+    row.appendChild(td);
+  }
+  buttonUpdate();
+});
+
+removeColumnButton.addEventListener('click', (e) => {
+  for (const row of table.rows) {
+    row.deleteCell(-1);
+  }
+  buttonUpdate();
 });
