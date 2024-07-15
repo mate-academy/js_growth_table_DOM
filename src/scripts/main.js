d68cd6a2af109f9475a6e756dffe082299abb54d
Anastasiia <vasilisandreeva00@gmail.com>
2024-07-15 18:04:16 +0300

    add main button logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,86 +3,78 @@
 const MAX_NUMBER = 10;
 const MIN_NUMBER = 2;
 
-const appendRowButton = document.querySelector('.append-row');
-const removeRowButton = document.querySelector('.remove-row');
+const container = document.querySelector('.container');
+const buttons = container.querySelectorAll('button');
+const table = document.querySelector('table');
 
-appendRowButton.addEventListener('click', addRow);
-removeRowButton.addEventListener('click', removeRow);
+const appendRowButton = container.querySelector('.append-row');
+const removeRowButton = container.querySelector('.remove-row');
+const appendColumnButton = container.querySelector('.append-column');
+const removeColumnButton = container.querySelector('.remove-column');
 
-const appendColumnButton = document.querySelector('.append-column');
-const removeColumnButton = document.querySelector('.remove-column');
+buttons.forEach((button) => {
+  button.addEventListener('click', function (e) {
+    const rows = document.querySelectorAll('tr');
+    const columns = rows.length > 0 ? rows[0].querySelectorAll('td') : [];
 
-appendColumnButton.addEventListener('click', addColumn);
-removeColumnButton.addEventListener('click', removeColumn);
+    switch (true) {
+      case e.target.classList.contains('append-row'):
+        const newRow = document.createElement('tr');
 
-function addColumn() {
-  const rows = document.querySelectorAll('tr');
-  const newColumn = document.createElement('td');
+        for (let i = 0; i < (rows[0].cells.length || 0); i++) {
+          newRow.appendChild(document.createElement('td'));
+        }
 
-  rows.forEach((row) => {
-    row.appendChild(newColumn.cloneNode(true));
-  });
+        table.appendChild(newRow);
 
-  const columns = rows[0].querySelectorAll('td');
+        if (rows.length + 1 >= MAX_NUMBER) {
+          appendRowButton.setAttribute('disabled', '');
+        }
 
-  if (columns.length >= MAX_NUMBER) {
-    appendColumnButton.setAttribute('disabled', '');
-  }
+        removeRowButton.removeAttribute('disabled');
+        break;
 
-  removeColumnButton.removeAttribute('disabled');
-}
+      case e.target.classList.contains('remove-row'):
+        if (rows.length > MIN_NUMBER) {
+          rows[rows.length - 1].remove();
+        }
 
-function removeColumn() {
-  const rows = document.querySelectorAll('tr');
+        const updatedRows = table.querySelectorAll('tr');
 
-  rows.forEach((row) => {
-    if (row.children.length > MIN_NUMBER) {
-      row.removeChild(row.lastChild);
+        if (updatedRows.length <= MIN_NUMBER) {
+          removeRowButton.setAttribute('disabled', '');
+        }
+
+        appendRowButton.removeAttribute('disabled');
+        break;
+
+      case e.target.classList.contains('append-column'):
+        rows.forEach((row) => {
+          row.appendChild(document.createElement('td'));
+        });
+
+        if (columns.length + 1 >= MAX_NUMBER) {
+          appendColumnButton.setAttribute('disabled', '');
+        }
+
+        removeColumnButton.removeAttribute('disabled');
+        break;
+
+      case e.target.classList.contains('remove-column'):
+        rows.forEach((row) => {
+          if (row.cells.length > MIN_NUMBER) {
+            row.removeChild(row.lastChild);
+          }
+        });
+
+        const updatedColumns =
+          rows.length > 0 ? rows[0].querySelectorAll('td') : [];
+
+        if (updatedColumns.length <= MIN_NUMBER) {
+          removeColumnButton.setAttribute('disabled', '');
+        }
+        appendColumnButton.removeAttribute('disabled');
+        break;
     }
   });
-
-  const columns = rows[0].querySelectorAll('td');
-
-  if (columns.length <= MIN_NUMBER) {
-    removeColumnButton.setAttribute('disabled', '');
-  }
-
-  appendColumnButton.removeAttribute('disabled');
-}
-
-function addRow() {
-  const table = document.querySelector('.field');
-  const rows = table.querySelectorAll('tr');
-  const newRow = document.createElement('tr');
-
-  for (let i = 0; i < rows[1].cells.length; i++) {
-    newRow.appendChild(document.createElement('td'));
-  }
-
-  table.appendChild(newRow);
-
-  if (rows.length >= MAX_NUMBER) {
-    appendRowButton.setAttribute('disabled', '');
-  }
-
-  removeRowButton.removeAttribute('disabled');
-}
-
-function removeRow() {
-  const table = document.querySelector('.field');
-  const rows = table.querySelectorAll('tr');
-
-  if (rows.length > MIN_NUMBER) {
-    const lastRow = rows[rows.length - 1];
-
-    lastRow.remove();
-  }
-
-  const updatedRows = table.querySelectorAll('tr');
-
-  if (updatedRows.length <= MIN_NUMBER) {
-    removeRowButton.setAttribute('disabled', '');
-  }
-
-  appendRowButton.removeAttribute('disabled');
-}
+});
