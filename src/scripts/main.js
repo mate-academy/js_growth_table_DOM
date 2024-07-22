16255080d4b26bfacb00ae86a004626446ef4678
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-23 01:20:32 +0300

    add columns behaviour

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@
 const field = document.querySelector('.field');
 const appendRowButton = document.querySelector('.append-row');
 const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
 
 appendRowButton.addEventListener('click', (e) => {
   const row = field.rows[0].cloneNode(true);
@@ -31,5 +33,37 @@ removeRowButton.addEventListener('click', (e) => {
 
   if (field.rows.length === 9) {
     appendRowButton.removeAttribute('disabled');
+  }
+});
+
+appendColumnButton.addEventListener('click', (e) => {
+  for (const row of field.rows) {
+    row.appendChild(document.createElement('td'));
+  }
+
+  if (field.rows[0].cells.length >= 10) {
+    appendColumnButton.setAttribute('disabled', '');
+
+    return;
+  }
+
+  if (field.rows[0].cells.length === 3) {
+    removeColumnButton.removeAttribute('disabled');
+  }
+});
+
+removeColumnButton.addEventListener('click', (e) => {
+  for (const row of field.rows) {
+    row.cells[row.cells.length - 1].outerHTML = '';
+  }
+
+  if (field.rows[0].cells.length <= 2) {
+    removeColumnButton.setAttribute('disabled', '');
+
+    return;
+  }
+
+  if (field.rows[0].cells.length === 9) {
+    appendColumnButton.removeAttribute('disabled');
   }
 });
