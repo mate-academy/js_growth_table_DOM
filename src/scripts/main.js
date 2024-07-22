1c981b4f958c29115d9ec39851bfe68c07ebf573
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-23 01:05:09 +0300

    add behaviour for rows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.field');
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+
+appendRowButton.addEventListener('click', (e) => {
+  const row = field.rows[0].cloneNode(true);
+
+  field.firstElementChild.appendChild(row);
+
+  if (field.rows.length >= 10) {
+    appendRowButton.setAttribute('disabled', true);
+
+    return;
+  }
+
+  if (field.rows.length === 3) {
+    removeRowButton.removeAttribute('disabled', true);
+  }
+});
+
+removeRowButton.addEventListener('click', (e) => {
+  field.rows[0].outerHTML = '';
+
+  if (field.rows.length <= 2) {
+    removeRowButton.setAttribute('disabled', true);
+
+    return;
+  }
+
+  if (field.rows.length === 9) {
+    appendRowButton.removeAttribute('disabled', true);
+  }
+});
