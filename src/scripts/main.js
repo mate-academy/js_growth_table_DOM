a630f224a03d5436fd32969624cc950dfe3a30b6
Valentyn Radobenko <valikradobenko@gmail.com>
2024-08-01 17:15:15 +0300

    create a variables for numbers and use it everywhere

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,16 @@ const appendRowButton = document.querySelector('.append-row');
 const removeRowButton = document.querySelector('.remove-row');
 const appendColumnButton = document.querySelector('.append-column');
 const removeColumnButton = document.querySelector('.remove-column');
+
+const maxRowLength = 10;
+const minRowLength = 2;
+const maxColumnLength = 10;
+const minColumnLength = 2;
+
 const tbody = document.querySelector('tbody');
 
 appendRowButton.addEventListener('click', () => {
-  if (tbody.children.length < 10) {
+  if (tbody.children.length < maxRowLength) {
     const trs = document.querySelectorAll('tr');
     const tr = document.createElement('tr');
 
@@ -20,25 +26,25 @@ appendRowButton.addEventListener('click', () => {
     tbody.appendChild(tr);
   }
 
-  if (tbody.children.length === 10) {
+  if (tbody.children.length === maxRowLength) {
     appendRowButton.disabled = true;
   }
 
-  if (tbody.children.length > 2) {
+  if (tbody.children.length > minRowLength) {
     removeRowButton.removeAttribute('disabled');
   }
 });
 
 removeRowButton.addEventListener('click', () => {
-  if (tbody.children.length > 2) {
+  if (tbody.children.length > minRowLength) {
     tbody.removeChild(tbody.lastElementChild);
   }
 
-  if (tbody.children.length === 2) {
+  if (tbody.children.length === minRowLength) {
     removeRowButton.disabled = true;
   }
 
-  if (tbody.children.length < 10) {
+  if (tbody.children.length < maxRowLength) {
     appendRowButton.removeAttribute('disabled');
   }
 });
@@ -46,7 +52,7 @@ removeRowButton.addEventListener('click', () => {
 appendColumnButton.addEventListener('click', () => {
   const trs = document.querySelectorAll('tr');
 
-  if (trs[0].children.length < 10) {
+  if (trs[0].children.length < maxColumnLength) {
     for (let i = 0; i < trs.length; i++) {
       const td = document.createElement('td');
 
@@ -54,11 +60,11 @@ appendColumnButton.addEventListener('click', () => {
     }
   }
 
-  if (trs[0].children.length === 10) {
+  if (trs[0].children.length === maxColumnLength) {
     appendColumnButton.disabled = true;
   }
 
-  if (trs[0].children.length > 2) {
+  if (trs[0].children.length > minColumnLength) {
     removeColumnButton.removeAttribute('disabled');
   }
 });
@@ -66,17 +72,17 @@ appendColumnButton.addEventListener('click', () => {
 removeColumnButton.addEventListener('click', () => {
   const trs = document.querySelectorAll('tr');
 
-  if (trs[0].children.length > 2) {
+  if (trs[0].children.length > minColumnLength) {
     for (let i = 0; i < trs.length; i++) {
       trs[i].removeChild(trs[i].lastElementChild);
     }
   }
 
-  if (trs[0].children.length === 2) {
+  if (trs[0].children.length === minColumnLength) {
     removeColumnButton.disabled = true;
   }
 
-  if (trs[0].children.length < 10) {
+  if (trs[0].children.length < maxColumnLength) {
     appendColumnButton.removeAttribute('disabled');
   }
 });
