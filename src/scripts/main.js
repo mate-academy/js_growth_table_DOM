9e7f67d6a76f960d5ae122c8f35b5bbe26e2c4fe
Anastasiia <vasilisandreeva00@gmail.com>
2024-08-05 18:10:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,76 +5,83 @@ const MIN_NUMBER = 2;
 
 const container = document.querySelector('.container');
 const buttons = container.querySelectorAll('button');
-const table = document.querySelector('table');
+const table = document.querySelector('.field');
+
+let rows = table.querySelectorAll('tr');
+
+function updateRowsAndColumns() {
+  rows = Array.from(table.querySelectorAll('tr'));
+}
 
 const appendRowButton = container.querySelector('.append-row');
 const removeRowButton = container.querySelector('.remove-row');
 const appendColumnButton = container.querySelector('.append-column');
 const removeColumnButton = container.querySelector('.remove-column');
 
+function appendRow() {
+  const newRow = rows[0].cloneNode(true);
+
+  table.appendChild(newRow);
+  updateRowsAndColumns();
+
+  if (rows.length >= MAX_NUMBER) {
+    appendRowButton.setAttribute('disabled', '');
+  }
+
+  removeRowButton.removeAttribute('disabled');
+}
+
+function removeRow() {
+  if (rows.length > MIN_NUMBER) {
+    rows[rows.length - 1].remove();
+    updateRowsAndColumns();
+
+    if (rows.length <= MIN_NUMBER) {
+      removeRowButton.setAttribute('disabled', '');
+    }
+
+    appendRowButton.removeAttribute('disabled');
+  }
+}
+
+function appendColumn() {
+  rows.forEach((row) => row.insertCell(-1));
+  updateRowsAndColumns();
+
+  if (rows[0].cells.length >= MAX_NUMBER) {
+    appendColumnButton.setAttribute('disabled', '');
+  }
+
+  removeColumnButton.removeAttribute('disabled');
+}
+
+function removeColumn() {
+  rows.forEach((row) => {
+    if (row.cells.length > MIN_NUMBER) {
+      row.deleteCell(-1);
+    }
+  });
+  updateRowsAndColumns();
+
+  if (rows[0].cells.length <= MIN_NUMBER) {
+    removeColumnButton.setAttribute('disabled', '');
+  }
+
+  appendColumnButton.removeAttribute('disabled');
+}
+
 buttons.forEach((button) => {
   button.addEventListener('click', function (e) {
-    const rows = document.querySelectorAll('tr');
-    const columns = rows.length > 0 ? rows[0].querySelectorAll('td') : [];
-
-    switch (true) {
-      case e.target.classList.contains('append-row'):
-        const newRow = document.createElement('tr');
-
-        for (let i = 0; i < (rows[0].cells.length || 0); i++) {
-          newRow.appendChild(document.createElement('td'));
-        }
-
-        table.appendChild(newRow);
-
-        if (rows.length + 1 >= MAX_NUMBER) {
-          appendRowButton.setAttribute('disabled', '');
-        }
-
-        removeRowButton.removeAttribute('disabled');
-        break;
-
-      case e.target.classList.contains('remove-row'):
-        if (rows.length > MIN_NUMBER) {
-          rows[rows.length - 1].remove();
-        }
-
-        const updatedRows = table.querySelectorAll('tr');
-
-        if (updatedRows.length <= MIN_NUMBER) {
-          removeRowButton.setAttribute('disabled', '');
-        }
-
-        appendRowButton.removeAttribute('disabled');
-        break;
-
-      case e.target.classList.contains('append-column'):
-        rows.forEach((row) => {
-          row.appendChild(document.createElement('td'));
-        });
-
-        if (columns.length + 1 >= MAX_NUMBER) {
-          appendColumnButton.setAttribute('disabled', '');
-        }
-
-        removeColumnButton.removeAttribute('disabled');
-        break;
-
-      case e.target.classList.contains('remove-column'):
-        rows.forEach((row) => {
-          if (row.cells.length > MIN_NUMBER) {
-            row.removeChild(row.lastChild);
-          }
-        });
-
-        const updatedColumns =
-          rows.length > 0 ? rows[0].querySelectorAll('td') : [];
-
-        if (updatedColumns.length <= MIN_NUMBER) {
-          removeColumnButton.setAttribute('disabled', '');
-        }
-        appendColumnButton.removeAttribute('disabled');
-        break;
+    if (e.target.classList.contains('append-row')) {
+      appendRow();
+    } else if (e.target.classList.contains('remove-row')) {
+      removeRow();
+    } else if (e.target.classList.contains('append-column')) {
+      appendColumn();
+    } else if (e.target.classList.contains('remove-column')) {
+      removeColumn();
     }
   });
 });
+
+updateRowsAndColumns();
