bdaaf571fab1f762518c82a72d273c30bcae6c53
mrsvolodya <vovamursky@gmail.com>
2024-08-10 16:14:04 +0300

    growth-table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,57 +1,41 @@
 'use strict';
+const maxLen = 10;
+const minLen = 2;
 
-const table = document.querySelector('.field');
+const tr = document.getElementsByTagName('tr');
+const table = document.getElementsByTagName('tbody')[0];
 const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
-const rows = table.lastChild.rows;
+
+function updateButton() {
+  appendRow.disabled = tr.length === maxLen;
+  removeRow.disabled = tr.length === minLen;
+  appendColumn.disabled = tr[0].children.length === maxLen;
+  removeColumn.disabled = tr[0].children.length === minLen;
+}
 
 appendRow.addEventListener('click', () => {
-  const tr = document.querySelectorAll('tr');
-
-  if (tr.length === 9) {
-    appendRow.disabled = true;
-  }
-
-  table.lastChild.append(tr[0].cloneNode(true));
-  removeRow.disabled = false;
+  table.append(tr[0].cloneNode(true));
+  updateButton();
 });
 
 removeRow.addEventListener('click', () => {
-  const tr = document.querySelectorAll('tr');
-
-  if (tr.length === 3) {
-    removeRow.disabled = true;
-  }
-
-  appendRow.disabled = false;
   tr[2].remove();
+  updateButton();
 });
 
 appendColumn.addEventListener('click', () => {
-  const tr = document.querySelectorAll('tr');
+  Array.from(tr).forEach((row) => {
+    row.insertAdjacentHTML('beforeend', '<td></td>');
+  });
 
-  removeColumn.disabled = false;
-
-  if (tr[0].cells.length <= 9) {
-    Array.from(rows).forEach((row) => {
-      row.insertAdjacentHTML('beforeend', '<td></td>');
-    });
-  }
-
-  if (tr[0].cells.length === 10) {
-    appendColumn.disabled = true;
-  }
+  updateButton();
 });
 
 removeColumn.addEventListener('click', () => {
-  appendColumn.disabled = false;
+  Array.from(tr).forEach((row) => row.cells[2].remove());
 
-  Array.from(rows).forEach((row) => {
-    if (row.cells.length === 3) {
-      removeColumn.disabled = true;
-    }
-    row.cells[2].remove();
-  });
+  updateButton();
 });
