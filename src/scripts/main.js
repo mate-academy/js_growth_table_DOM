a54d527d3104a731a298a71dd32378286433c009
Mainaiev <farid.mainaiev@proton.me>
2024-08-13 20:21:39 +0300

    fixed commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,10 +17,10 @@ function toggleButtons() {
   removeColumnButton.disabled = columns <= 2;
   appendColumnButton.disabled = columns >= 10;
 
-  removeRowButton.style.display = rows <= 2 ? 'none' : '';
-  appendRowButton.style.display = rows >= 10 ? 'none' : '';
-  removeColumnButton.style.display = columns <= 2 ? 'none' : '';
-  appendColumnButton.style.display = columns >= 10 ? 'none' : '';
+  removeRowButton.style.display = rows <= 2;
+  appendRowButton.style.display = rows >= 10;
+  removeColumnButton.style.display = columns <= 2;
+  appendColumnButton.style.display = columns >= 10;
 }
 
 container.addEventListener('click', function (e) {
