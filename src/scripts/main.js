7afc8ad9188c844ce12bdcb2653bc6474b3a9d4c
Iryna Zahorodnia <irishas25@ukr.net>
2024-09-18 12:40:13 +0300

    growth table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,59 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const minCount = 2;
+const maxCount = 10;
+
+appendRow.addEventListener('click', () => {
+  const rows = table.rows;
+
+  if (rows.length >= maxCount) {
+    return;
+  }
+
+  const extraRow = rows[0].cloneNode(true);
+
+  table.tBodies[0].append(extraRow);
+  removeRow.removeAttribute('disabled');
+  appendRow.disabled = rows.length === maxCount;
+});
+
+removeRow.addEventListener('click', () => {
+  const rows = table.rows;
+
+  rows[0].remove();
+  appendRow.removeAttribute('disabled');
+  removeRow.disabled = rows.length === minCount;
+});
+
+appendColumn.addEventListener('click', () => {
+  const rows = table.rows;
+  const columns = table.rows[0].children;
+
+  if (columns.length >= maxCount) {
+    return;
+  }
+
+  [...rows].forEach((row) => {
+    const extraColumn = row.firstElementChild.cloneNode();
+
+    row.append(extraColumn);
+  });
+  removeColumn.removeAttribute('disabled');
+  appendColumn.disabled = columns.length === maxCount;
+});
+
+removeColumn.addEventListener('click', () => {
+  const rows = table.rows;
+  const columns = table.rows[0].children;
+
+  [...rows].forEach((row) => {
+    row.firstElementChild.remove();
+  });
+  appendColumn.removeAttribute('disabled');
+  removeColumn.disabled = columns.length === minCount;
+});
