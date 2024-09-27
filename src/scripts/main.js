f6d862600a0846a57e6f905314591ec1220d1a97
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-28 00:08:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,59 @@
 'use strict';
 
-// write code here
+const tbody = document.getElementsByTagName('tbody')[0];
+const apRow = document.getElementsByClassName('append-row')[0];
+const rmRow = document.getElementsByClassName('remove-row')[0];
+const apColumn = document.getElementsByClassName('append-column')[0];
+const rmColumn = document.getElementsByClassName('remove-column')[0];
+
+apRow.addEventListener('click', () => {
+  const oneRow = tbody.lastElementChild;
+
+  tbody.append(oneRow.cloneNode(true));
+
+  if (tbody.children.length >= 10) {
+    apRow.setAttribute('disabled', '');
+  } else {
+    rmRow.removeAttribute('disabled');
+  }
+});
+
+rmRow.addEventListener('click', () => {
+  tbody.lastElementChild.remove();
+
+  if (tbody.children.length <= 2) {
+    rmRow.setAttribute('disabled', '');
+  } else {
+    apRow.removeAttribute('disabled');
+  }
+});
+
+apColumn.addEventListener('click', () => {
+  const rows = [...tbody.children];
+
+  [...tbody.children].forEach((v) => {
+    const cell = v.lastElementChild;
+
+    v.append(cell.cloneNode(true));
+  });
+
+  if (rows[0].children.length >= 10) {
+    apColumn.setAttribute('disabled', '');
+  } else {
+    rmColumn.removeAttribute('disabled');
+  }
+});
+
+rmColumn.addEventListener('click', () => {
+  const rows = [...tbody.children];
+
+  rows.forEach((v) => {
+    v.lastElementChild.remove();
+  });
+
+  if (rows[0].children.length <= 2) {
+    rmColumn.setAttribute('disabled', '');
+  } else {
+    apColumn.removeAttribute('disabled');
+  }
+});
