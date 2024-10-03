e15a9c7be8e52a44d0b5cf878d9ad0078d82a1ee
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-10-03 15:36:09 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write code here
+const buttons = document.querySelectorAll('.button');
+const table = document.querySelector('table');
+const firstRow = table.rows[0];
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (ev) => {
+    const buttonClass = button.classList[0];
+
+    switch (buttonClass) {
+      case 'append-row':
+        if (table.rows.length < 10) {
+          const newRow = table.insertRow();
+
+          for (let cell = 0; cell < firstRow.cells.length; cell++) {
+            const newCell = newRow.insertCell(cell);
+
+            newCell.textContent = '';
+          }
+        }
+
+        break;
+      case 'remove-row':
+        if (table.rows.length > 2) {
+          table.deleteRow(-1);
+        }
+        break;
+      case 'append-column':
+        if (table.rows[0].cells.length < 10) {
+          const rows = document.querySelectorAll('tr');
+
+          rows.forEach((curRow) => {
+            const newCell = curRow.insertCell(-1);
+
+            newCell.textContent = '';
+          });
+        }
+        break;
+      case 'remove-column':
+        if (firstRow.cells.length > 2) {
+          for (const curRow of table.rows) {
+            curRow.deleteCell(-1);
+          }
+        }
+        break;
+    }
+  });
+});
