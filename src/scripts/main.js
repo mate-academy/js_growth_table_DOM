fa6c50e656a915d029a535d4444ecbea95211b9d
Kostya Vasilenko <kostyantin.vasilenko.03@gmail.com>
2024-10-04 20:24:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+
+document.querySelector('.append-row').addEventListener('click', () => {
+  const row = document.createElement('tr');
+  const columnCount = table.rows[0].cells.length;
+
+  for (let i = 0; i < columnCount; i++) {
+    const cell = document.createElement('td');
+
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+});
+
+document.querySelector('.remove-column').addEventListener('click', () => {
+  if (table.rows.length > 2) {
+    table.deleteRow(-1);
+  }
+});
+
+document.querySelector('.append-column').addEventListener('click', () => {
+  const rows = document.querySelectorAll('.field tr');
+
+  rows.forEach((row) => {
+    const cell = document.createElement('td');
+
+    row.appendChild(cell);
+  });
+});
+
+document.querySelector('.remove-row').addEventListener('click', () => {
+  const rows = document.querySelectorAll('.field tr');
+
+  rows.forEach((row) => {
+    if (row.cells.length > 2) {
+      row.deleteCell(-1);
+    }
+  });
+});
