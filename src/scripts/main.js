a7e89cc46ae749fa55100c385262faf4c1a37dc7
tsiupaknazar <sjprime280@gmail.com>
2025-01-28 17:13:56 +0000

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,67 @@
 'use strict';
 
 // write code here
+const MIN_VALUE = 2;
+const MAX_VALUE = 10;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('.field');
+
+  const appendRowBtn = document.querySelector('.append-row');
+  const removeRowBtn = document.querySelector('.remove-row');
+  const appendColBtn = document.querySelector('.append-column');
+  const removeColBtn = document.querySelector('.remove-column');
+
+  function updateButtons() {
+    const rowCount = table.rows.length;
+    const colCount = table.rows[0].cells.length;
+
+    appendRowBtn.disabled = rowCount >= MAX_VALUE;
+    removeRowBtn.disabled = rowCount <= MIN_VALUE;
+    appendColBtn.disabled = colCount >= MAX_VALUE;
+    removeColBtn.disabled = colCount <= MIN_VALUE;
+  }
+
+  appendRowBtn.addEventListener('click', () => {
+    if (table.rows.length < 10) {
+      const newRow = table.insertRow();
+      const colCount = table.rows[0].cells.length;
+
+      for (let i = 0; i < colCount; i++) {
+        newRow.insertCell();
+      }
+    }
+
+    updateButtons();
+  });
+
+  removeRowBtn.addEventListener('click', () => {
+    if (table.rows.length > 2) {
+      table.deleteRow(-1);
+    }
+
+    updateButtons();
+  });
+
+  appendColBtn.addEventListener('click', () => {
+    if (table.rows[0].cells.length < 10) {
+      for (const row of table.rows) {
+        row.insertCell();
+      }
+    }
+
+    updateButtons();
+  });
+
+  removeColBtn.addEventListener('click', () => {
+    if (table.rows[0].cells.length > 2) {
+      for (const row of table.rows) {
+        row.deleteCell(-1);
+      }
+    }
+
+    updateButtons();
+  });
+
+  updateButtons();
+});
