6e2f85581ac8b74a440efed0d1f0f15063b44d6c
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-01-29 21:11:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,63 @@
 'use strict';
 
-// write code here
+const addRow = document.querySelector('.append-row');
+const deleteRow = document.querySelector('.remove-row');
+const addColumn = document.querySelector('.append-column');
+const deleteColumn = document.querySelector('.remove-column');
+
+const table = document.querySelector('.field');
+const maxCount = 10;
+const minCount = 2;
+
+function updateButton() {
+  const rowCount = table.rows.length;
+  const columnCount = table.rows[0]?.cells.length || 0;
+
+  addRow.disabled = rowCount >= maxCount;
+  deleteRow.disabled = rowCount <= minCount;
+  addColumn.disabled = columnCount >= maxCount;
+  deleteColumn.disabled = columnCount <= minCount;
+}
+
+addRow.addEventListener('click', () => {
+  if (table.rows.length < maxCount) {
+    const newRow = table.insertRow();
+    const columnCount = table.rows[0]?.cells.length || minCount;
+
+    for (let i = 0; i < columnCount; i++) {
+      newRow.insertCell();
+    }
+  }
+
+  updateButton();
+});
+
+deleteRow.addEventListener('click', () => {
+  if (table.rows.length > minCount) {
+    table.deleteRow(-1);
+  }
+
+  updateButton();
+});
+
+addColumn.addEventListener('click', () => {
+  if (table.rows[0]?.cells.length < maxCount) {
+    for (const row of table.rows) {
+      row.insertCell();
+    }
+  }
+
+  updateButton();
+});
+
+deleteColumn.addEventListener('click', () => {
+  if (table.rows[0]?.cells.length > minCount) {
+    for (const row of table.rows) {
+      row.deleteCell(-1);
+    }
+  }
+
+  updateButton();
+});
+
+updateButton();
