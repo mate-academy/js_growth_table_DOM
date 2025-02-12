6fa9a58481fde37f25175f4ab04ad1f3efe21f60
Kirill <Face2404@ukr.net>
2025-02-13 01:31:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,70 @@
 'use strict';
 
-// write code here
+const buttons = document.querySelectorAll('button');
+const table = document.querySelector('tbody');
+
+for (const button of buttons) {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('append-row')) {
+      if (table.children.length >= 10) {
+        return;
+      }
+
+      if (table.children.length === 9) {
+        button.setAttribute('disabled', '');
+      } else if (table.children.length === 2) {
+        buttons[1].removeAttribute('disabled');
+      }
+
+      const newRow = document.createElement('tr');
+
+      for (let i = 0; i < table.children[0].children.length; i++) {
+        const newCell = document.createElement('td');
+
+        newRow.append(newCell);
+      }
+
+      table.append(newRow);
+    }
+
+    if (button.classList.contains('remove-row')) {
+      if (table.children.length === 10) {
+        buttons[0].removeAttribute('disabled');
+      } else if (table.children.length === 3) {
+        button.setAttribute('disabled', '');
+      }
+
+      table.lastElementChild.remove();
+    }
+
+    if (button.classList.contains('append-column')) {
+      if (table.children[0].children.length >= 10) {
+        return;
+      }
+
+      if (table.children[0].children.length === 9) {
+        button.setAttribute('disabled', '');
+      } else if (table.children[0].children.length === 2) {
+        buttons[3].removeAttribute('disabled');
+      }
+
+      for (const row of table.children) {
+        const newCell = document.createElement('td');
+
+        row.append(newCell);
+      }
+    }
+
+    if (button.classList.contains('remove-column')) {
+      if (table.children[0].children.length === 3) {
+        button.setAttribute('disabled', '');
+      } else if (table.children[0].children.length === 10) {
+        buttons[2].removeAttribute('disabled');
+      }
+
+      for (const row of table.children) {
+        row.lastElementChild.remove();
+      }
+    }
+  });
+}
