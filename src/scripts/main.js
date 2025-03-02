a997fea1b73af7c0dfaec481a6852bea89d218d6
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-02 16:45:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+appendRow.addEventListener('click', () => {
+  appendRow.disabled = table.rows.length >= 10;
+
+  const newRow = table.insertRow();
+
+  for (let i = 0; i < table.rows[0].cells.length; i++) {
+    newRow.insertCell();
+  }
+  removeRow.disabled = false;
+});
+
+removeRow.addEventListener('click', () => {
+  removeRow.disabled = table.rows.length <= 3;
+  table.deleteRow(1);
+  appendRow.disabled = false;
+});
+
+appendColumn.addEventListener('click', () => {
+  appendColumn.disabled = table.rows[0].children.length >= 9;
+
+  for (const row of table.rows) {
+    row.insertCell();
+  }
+  removeColumn.disabled = false;
+});
+
+removeColumn.addEventListener('click', () => {
+  removeColumn.disabled = table.rows[0].children.length <= 3;
+
+  for (const row of table.rows) {
+    row.deleteCell(1);
+  }
+  appendColumn.disabled = false;
+});
