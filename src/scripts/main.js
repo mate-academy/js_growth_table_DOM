400b9564d306a034c489dfdc2480087fba342ab3
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-02 16:56:25 +0200

    add task solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,42 +1,57 @@
 'use strict';
 
+'use strict';
+
 const table = document.querySelector('.field');
 const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 const appendColumn = document.querySelector('.append-column');
 const removeColumn = document.querySelector('.remove-column');
 
-appendRow.addEventListener('click', () => {
-  appendRow.disabled = table.rows.length >= 10;
+const MAX_ROWS = 10;
+const MIN_ROWS = 2;
+const MAX_COLUMNS = 10;
+const MIN_COLUMNS = 2;
 
+function updateButtons() {
+  appendRow.disabled = table.rows.length >= MAX_ROWS;
+  removeRow.disabled = table.rows.length <= MIN_ROWS;
+
+  const columnsCount = table.rows[0]?.cells.length || 0;
+
+  appendColumn.disabled = columnsCount >= MAX_COLUMNS;
+  removeColumn.disabled = columnsCount <= MIN_COLUMNS;
+}
+
+appendRow.addEventListener('click', () => {
   const newRow = table.insertRow();
 
   for (let i = 0; i < table.rows[0].cells.length; i++) {
     newRow.insertCell();
   }
-  removeRow.disabled = false;
+  updateButtons();
 });
 
 removeRow.addEventListener('click', () => {
-  removeRow.disabled = table.rows.length <= 3;
-  table.deleteRow(1);
-  appendRow.disabled = false;
+  table.deleteRow(-1);
+
+  updateButtons();
 });
 
 appendColumn.addEventListener('click', () => {
-  appendColumn.disabled = table.rows[0].children.length >= 9;
-
   for (const row of table.rows) {
     row.insertCell();
   }
-  removeColumn.disabled = false;
+
+  updateButtons();
 });
 
 removeColumn.addEventListener('click', () => {
-  removeColumn.disabled = table.rows[0].children.length <= 3;
-
   for (const row of table.rows) {
-    row.deleteCell(1);
+    row.deleteCell(-1);
   }
-  appendColumn.disabled = false;
+
+  updateButtons();
 });
+
+updateButtons();
