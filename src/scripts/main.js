086b2920cb18f130580b59cdb6df6d70c2d1b6cd
my-git-username <118355685+my-git-username@users.noreply.github.com>
2025-03-17 11:46:30 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,68 @@
 'use strict';
 
-// write code here
+const minSize = 2;
+const maxSize = 10;
+
+const table = document.querySelector('.field');
+
+const newRow = document.querySelector('.append-row');
+const delRow = document.querySelector('.remove-row');
+
+const newCol = document.querySelector('.append-column');
+const delCol = document.querySelector('.remove-column');
+
+newCol.addEventListener('click', () => {
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].insertCell(-1).innerHTML = '';
+  }
+
+  if (table.rows[0].cells.length === maxSize) {
+    newCol.disabled = true;
+  }
+
+  if (delCol.disabled === true) {
+    delCol.disabled = false;
+  }
+});
+
+delCol.addEventListener('click', () => {
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].deleteCell(-1);
+  }
+
+  if (table.rows[0].cells.length === minSize) {
+    delCol.disabled = true;
+  }
+
+  if (newCol.disabled === true) {
+    newCol.disabled = false;
+  }
+});
+
+newRow.addEventListener('click', () => {
+  const addRow = table.insertRow();
+
+  for (let i = 0; i < table.rows[0].cells.length; i++) {
+    addRow.insertCell(-1).innerHTML = '';
+  }
+
+  if (table.rows.length === maxSize) {
+    newRow.disabled = true;
+  }
+
+  if (delRow.disabled === true) {
+    delRow.disabled = false;
+  }
+});
+
+delRow.addEventListener('click', () => {
+  table.deleteRow(-1);
+
+  if (table.rows.length === minSize) {
+    delRow.disabled = true;
+  }
+
+  if (newRow.disabled === true) {
+    newRow.disabled = false;
+  }
+});
