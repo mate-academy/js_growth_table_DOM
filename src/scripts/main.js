19f6d9b5fe970a56228298cba11018e90bfa4b9c
Olena Tatyuk <tatyukolena@gmail.com>
2025-05-27 11:29:04 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,63 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+function updateRowButtons() {
+  const rowCount = table.rows.length;
+
+  appendRowButton.disabled = rowCount >= 10;
+  removeRowButton.disabled = rowCount <= 2;
+}
+
+appendRowButton.addEventListener('click', () => {
+  const numberOfCells = table.rows[0].cells.length;
+
+  const tableRef = document.querySelector('.field');
+  const newRow = tableRef.insertRow(-1);
+
+  for (let i = 0; i < numberOfCells; i++) {
+    const newCell = newRow.insertCell(0);
+    const newText = document.createTextNode('');
+
+    newCell.appendChild(newText);
+  }
+
+  updateRowButtons();
+});
+
+removeRowButton.addEventListener('click', () => {
+  table.deleteRow(table.rows.length - 1);
+
+  updateRowButtons();
+});
+
+function updateColumnButtons() {
+  const columnCount = table.rows[0].cells.length;
+
+  appendColumnButton.disabled = columnCount >= 10;
+  removeColumnButton.disabled = columnCount <= 2;
+}
+
+appendColumnButton.addEventListener('click', () => {
+  for (const row of table.rows) {
+    const td = document.createElement('td');
+
+    row.appendChild(td);
+  }
+
+  updateColumnButtons();
+});
+
+removeColumnButton.addEventListener('click', () => {
+  for (const row of table.rows) {
+    if (row.cells.length > 0) {
+      row.deleteCell(-1);
+    }
+  }
+
+  updateColumnButtons();
+});
