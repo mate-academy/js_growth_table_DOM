f10b4dd6c61422486e7d7aebab5c27affbb26be3
Yurii <j.zherebilov15@gmail.com>
2025-06-18 14:34:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,101 @@
 'use strict';
 
-// write code here
+// HTML Parts
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+
+// BUTTONS
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+// HELPER Functions
+function getRowsCount() {
+  return table.querySelectorAll('tr').length;
+}
+
+function getColumnsCount() {
+  const firstRow = table.querySelector('tr');
+
+  return firstRow ? firstRow.children.length : 0;
+}
+
+function updateButtonsState() {
+  if (getRowsCount() >= 10) {
+    appendRowButton.disabled = true;
+    removeRowButton.disabled = false;
+  }
+
+  if (getRowsCount() <= 2) {
+    removeRowButton.disabled = true;
+    appendRowButton.disabled = false;
+  }
+
+  if (getColumnsCount() >= 10) {
+    appendColumnButton.disabled = true;
+    removeColumnButton.disabled = false;
+  }
+
+  if (getColumnsCount() <= 2) {
+    removeColumnButton.disabled = true;
+    appendColumnButton.disabled = false;
+  }
+}
+
+// MAIN Logic
+
+appendRowButton.addEventListener('click', () => {
+  if (getRowsCount() < 10) {
+    const tr = document.createElement('tr');
+
+    for (let i = 0; i < getColumnsCount(); i++) {
+      const td = document.createElement('td');
+
+      tr.append(td);
+    }
+    tbody.append(tr);
+  }
+  updateButtonsState();
+});
+
+removeRowButton.addEventListener('click', () => {
+  updateButtonsState();
+
+  const rows = tbody.querySelectorAll('tr');
+
+  if (rows.length > 2) {
+    const lastRow = rows[rows.length - 1];
+
+    tbody.removeChild(lastRow);
+  }
+  updateButtonsState();
+});
+
+appendColumnButton.addEventListener('click', () => {
+  updateButtonsState();
+
+  if (getColumnsCount() < 10) {
+    const rows = tbody.querySelectorAll('tr');
+
+    rows.forEach((row) => {
+      const td = document.createElement('td');
+
+      row.append(td);
+    });
+  }
+  updateButtonsState();
+});
+
+removeColumnButton.addEventListener('click', () => {
+  updateButtonsState();
+
+  if (getColumnsCount() > 2) {
+    const rows = tbody.querySelectorAll('tr');
+
+    rows.forEach((row) => {
+      row.removeChild(row.lastElementChild);
+    });
+  }
+  updateButtonsState();
+});
