93abcb41d1746f242ad6f1ae2f145c35ea00f1e0
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-09-29 15:31:14 +0300

    update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,68 +1,86 @@
 'use strict';
 
-const table = document.querySelector('.field');
-const appendRowBtn = document.querySelector('.append-row');
-const removeRowBtn = document.querySelector('.remove-row');
-const appendColBtn = document.querySelector('.append-column');
-const removeColBtn = document.querySelector('.remove-column');
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('.field');
+  const appendRowBtn = document.querySelector('.append-row');
+  const removeRowBtn = document.querySelector('.remove-row');
+  const appendColBtn = document.querySelector('.append-column');
+  const removeColBtn = document.querySelector('.remove-column');
 
-// кількість рядків
-let rows = table.rows.length;
-// кількість колонок
-let cols = table.rows[0].cells.length;
+  if (!table || table.rows.length === 0) {
+    return;
+  }
 
-function updateButtons() {
-  appendRowBtn.disabled = rows >= 10;
-  removeRowBtn.disabled = rows <= 2;
-  appendColBtn.disabled = cols >= 10;
-  removeColBtn.disabled = cols <= 2;
-}
+  // кількість рядків (колекція всіх <tr>)
+  let rows = table.rows.length;
+  // кількість колонок (колекція всіх клітинки (<td>) лише з першого рядка.)
+  let cols = table.rows[0].cells.length;
 
-// Додати рядок
-appendRowBtn.addEventListener('click', () => {
-  if (rows < 10) {
-    const newRow = table.insertRow();
+  function updateButtons() {
+    appendRowBtn.disabled = rows >= 10;
+    removeRowBtn.disabled = rows <= 2;
+    appendColBtn.disabled = cols >= 10;
+    removeColBtn.disabled = cols <= 2;
+  }
 
-    for (let i = 0; i < cols; i++) {
-      newRow.insertCell();
+  // Додати рядок
+  appendRowBtn.addEventListener('click', () => {
+    if (rows < 10) {
+      // створює новий рядок <tr> в кінці таблиці.
+      const newRow = table.insertRow();
+
+      for (let i = 0; i < cols; i++) {
+        newRow.insertCell();
+      }
+
+      rows++;
+      updateButtons();
     }
-    rows++;
-    updateButtons();
-  }
-});
+  });
 
-// Видалити рядок
-removeRowBtn.addEventListener('click', () => {
-  if (rows > 2) {
-    // видаляє останній рядок
-    table.deleteRow(-1);
-    rows--;
-    updateButtons();
-  }
-});
-
-// Додати колонку
-appendColBtn.addEventListener('click', () => {
-  if (cols < 10) {
-    for (let i = 0; i < rows; i++) {
-      table.rows[i].insertCell();
+  // Видалити рядок
+  removeRowBtn.addEventListener('click', () => {
+    if (rows > 2) {
+      // видаляє останній рядок
+      table.deleteRow(-1);
+      rows--;
+      updateButtons();
     }
-    cols++;
-    updateButtons();
-  }
-});
+  });
 
-// Видалити колонку
-removeColBtn.addEventListener('click', () => {
-  if (cols > 2) {
-    for (let i = 0; i < rows; i++) {
-      // видаляє останню клітинку
-      table.rows[i].deleteCell(-1);
+  // Додати колонку
+  appendColBtn.addEventListener('click', () => {
+    if (cols < 10) {
+      for (let i = 0; i < rows; i++) {
+        // eslint-disable-next-line max-len
+        // У кожному рядку викликаємо insertCell(), щоб додати нову клітинку <td> у кінець цього рядка.
+        table.rows[i].insertCell();
+      }
+
+      cols++;
+      updateButtons();
     }
-    cols--;
-    updateButtons();
-  }
+  });
+
+  // Видалити колонку
+  removeColBtn.addEventListener('click', () => {
+    if (cols > 2) {
+      for (let i = 0; i < rows; i++) {
+        // видаляє останню клітинку
+        table.rows[i].deleteCell(-1);
+      }
+
+      cols--;
+      updateButtons();
+    }
+  });
+
+  // ініціалізація
+  updateButtons();
 });
 
-// ініціалізація
-updateButtons();
+// Логіка
+// В ряд видаляємо один 'tr'
+// В колонку видаляємо в кожному 'tr' останній 'td'
+// <tr1> <td>1</td> <td>2</td> <td>3</td> <td>4</td> </tr1>
+// <tr2> <td>1</td> <td>2</td> <td>3</td> <td>4</td> </tr2>
