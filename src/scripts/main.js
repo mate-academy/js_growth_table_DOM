ca11649189841bb7196c58c3b510c7c0f0848fc7
friranger <65720979+friranger1@users.noreply.github.com>
2025-12-20 14:12:36 +0200

    Fixed preppend to append and removing first index of row and collumn to last index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,27 @@
 'use strict';
+
 const appendRowBtn = document.querySelector('.append-row');
 const removeRowBtn = document.querySelector('.remove-row');
 const appendColumnBtn = document.querySelector('.append-column');
 const removeColumnBtn = document.querySelector('.remove-column');
 const table = document.querySelector('.field');
 
-appendColumnBtn.addEventListener('click', (event) => {
+appendColumnBtn.addEventListener('click', () => {
   appendColumn(table);
 });
 
-removeColumnBtn.addEventListener('click', (event) => {
+removeColumnBtn.addEventListener('click', () => {
   removeColumn(table);
-  updateRemoveColumnBtn(table)
+  updateRemoveColumnBtn(table);
 });
 
-appendRowBtn.addEventListener('click', (event) => {
+appendRowBtn.addEventListener('click', () => {
   appendRow(table);
 });
 
-removeRowBtn.addEventListener('click', (event) => {
+removeRowBtn.addEventListener('click', () => {
   removeRow(table);
 });
-
 
 function appendColumn(elem) {
   const len = elem.rows[0].cells.length;
@@ -29,12 +29,12 @@ function appendColumn(elem) {
   if (len < 10) {
     for (const child of elem.rows) {
       const cell = document.createElement('td');
-      child.prepend(cell);
+
+      child.append(cell);
     }
   }
 
   updateRemoveColumnBtn(elem);
-
 }
 
 function removeColumn(elem) {
@@ -42,9 +42,9 @@ function removeColumn(elem) {
 
   if (len > 2) {
     for (const child of elem.rows) {
-      child.cells[0].remove();
+      child.cells[child.cells.length - 1].remove();
     }
-  };
+  }
 
   len = elem.rows[0].cells.length;
 
@@ -57,11 +57,13 @@ function appendRow(elem) {
 
   if (rowLen < 10) {
     const row = document.createElement('tr');
+
     for (let i = 0; i < columnLen; i++) {
       const cell = document.createElement('td');
+
       row.append(cell);
     }
-    elem.tBodies[0].prepend(row);
+    elem.tBodies[0].append(row);
   }
 
   updateRemoveRowBtn(elem);
@@ -70,9 +72,9 @@ function appendRow(elem) {
 function removeRow(elem) {
   const rowLen = elem.tBodies[0].children.length;
 
-  if (rowLen > 2) {
-    elem.tBodies[0].children[0].remove();
 
+  if (rowLen > 2) {
+    elem.tBodies[0].children[rowLen - 1].remove();
   }
 
   updateRemoveRowBtn(elem);
@@ -91,6 +93,3 @@ function updateRemoveRowBtn(elem) {
   removeRowBtn.disabled = len <= 2;
   appendRowBtn.disabled = len >= 10;
 }
-
-
-
