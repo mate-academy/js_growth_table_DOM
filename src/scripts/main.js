afcfbda286605e1efc8bef96096a0f39de0e77c1
Ivan <opalenyk.i@gmail.com>
2026-04-11 13:11:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,62 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('.field');
+  const appendRowBtn = document.querySelector('.append-row');
+  const removeRowBtn = document.querySelector('.remove-row');
+  const appendColBtn = document.querySelector('.append-column');
+  const removeColBtn = document.querySelector('.remove-column');
+
+  const MIN = 2;
+  const MAX = 10;
+
+  function updateButtons() {
+    const rowsCount = table.rows.length;
+    const colsCount = table.rows[0].cells.length;
+
+    appendRowBtn.disabled = rowsCount >= MAX;
+    removeRowBtn.disabled = rowsCount <= MIN;
+    appendColBtn.disabled = colsCount >= MAX;
+    removeColBtn.disabled = colsCount <= MIN;
+  }
+
+  appendRowBtn.addEventListener('click', () => {
+    const colsCount = table.rows[0].cells.length;
+
+    if (table.rows.length < MAX) {
+      const newRow = table.insertRow();
+
+      for (let i = 0; i < colsCount; i++) {
+        newRow.insertCell().textContent = '';
+      }
+    }
+    updateButtons();
+  });
+
+  removeRowBtn.addEventListener('click', () => {
+    if (table.rows.length > MIN) {
+      table.deleteRow(table.rows.length - 1);
+    }
+    updateButtons();
+  });
+
+  appendColBtn.addEventListener('click', () => {
+    if (table.rows[0].cells.length < MAX) {
+      for (const row of table.rows) {
+        row.insertCell().textContent = '';
+      }
+    }
+    updateButtons();
+  });
+
+  removeColBtn.addEventListener('click', () => {
+    if (table.rows[0].cells.length > MIN) {
+      for (const row of table.rows) {
+        row.deleteCell(row.cells.length - 1);
+      }
+    }
+    updateButtons();
+  });
+
+  updateButtons();
+});
