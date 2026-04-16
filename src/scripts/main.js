7d639fdc0afb7f46b54501797b2a6f998cf5e1d9
OlehYuriev <urievoleg94@gmail.com>
2026-04-16 22:27:48 +0300

    add optional chening

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,8 +8,8 @@ const table = document.querySelector('.field tbody');
 
 const max = 10;
 const min = 2;
-const getRows = () => table.children?.length ?? 0;
-const getColumns = () => table.children[0]?.children.length ?? 0;
+const getRows = () => table?.children?.length ?? 0;
+const getColumns = () => table?.children[0]?.children?.length ?? 0;
 
 function updateState() {
   appendRowButton.disabled = getRows() >= max;
