7bfc67af41afd06d1e066ad64d5d5bc5081329a1
Maksym <zubchenko.2001@gmail.com>
2026-05-01 18:50:26 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,54 @@
 'use strict';
 
-// write code here
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const tbody = document.querySelector('tbody');
+
+function getRowCount() {
+  return tbody.children.length;
+}
+
+function getColumnCount() {
+  return tbody.querySelector('tr').children.length;
+}
+
+function updateButtons() {
+  appendRow.disabled = getRowCount() >= 10;
+  removeRow.disabled = getRowCount() <= 2;
+
+  appendColumn.disabled = getColumnCount() >= 10;
+  removeColumn.disabled = getColumnCount() <= 2;
+}
+
+addEventListener('click', (ev) => {
+  if (ev.target === appendRow && getRowCount() < 10) {
+    const newRow = document.createElement('tr');
+    const cols = getColumnCount();
+
+    for (let i = 0; i < cols; i++) {
+      newRow.append(document.createElement('td'));
+    }
+
+    tbody.append(newRow);
+  }
+
+  if (ev.target === removeRow && getRowCount() > 2) {
+    tbody.lastElementChild.remove();
+  }
+
+  if (ev.target === appendColumn && getColumnCount() < 10) {
+    for (const row of tbody.children) {
+      row.append(document.createElement('td'));
+    }
+  }
+
+  if (ev.target === removeColumn && getColumnCount() > 2) {
+    for (const row of tbody.children) {
+      row.lastElementChild.remove();
+    }
+  }
+
+  updateButtons();
+});
