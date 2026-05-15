6d46ae6bf2a877d817d3ca86ed7103417f8fd441
Rostyslav <rooostik2016@gmail.com>
2026-05-15 19:22:57 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+appendRow.addEventListener('click', () => {
+  if (table.rows.length < 10) {
+    const rows =
+      '<tr>' + '<td></td>'.repeat(table.rows[0].cells.length) + '</tr>';
+
+    table.insertAdjacentHTML('beforeend', rows);
+  }
+
+  updateButtons();
+});
+
+appendColumn.addEventListener('click', () => {
+  if (table.rows[0].cells.length < 10) {
+    table
+      .querySelectorAll('tr')
+      .forEach((e) => e.insertAdjacentHTML('beforeend', '<td></td>'));
+  }
+
+  updateButtons();
+});
+
+removeRow.addEventListener('click', () => {
+  if (table.rows.length > 2) {
+    table.lastElementChild.lastElementChild.remove();
+  }
+
+  updateButtons();
+});
+
+removeColumn.addEventListener('click', () => {
+  if (table.rows[0].cells.length > 2) {
+    table.querySelectorAll('tr').forEach((e) => e.lastElementChild.remove());
+  }
+
+  updateButtons();
+});
+
+function updateButtons() {
+  appendRow.disabled = table.rows.length >= 10;
+  removeRow.disabled = table.rows.length <= 2;
+
+  appendColumn.disabled = table.rows[0].cells.length >= 10;
+  removeColumn.disabled = table.rows[0].cells.length <= 2;
+}
