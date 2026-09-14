375fa8800c0461e54f4c5787fbce45914374fee4
Your Name <you@example.com>
2026-09-15 01:56:27 +0300

    Solution_v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,59 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+
+const addRowBtn = document.querySelector('.append-row');
+const removeRowBtn = document.querySelector('.remove-row');
+const addColumnBtn = document.querySelector('.append-column');
+const removeColumnBtn = document.querySelector('.remove-column');
+
+const max = 10;
+const min = 2;
+
+function updateButtonStates() {
+  const rowCount = table.children.length;
+  const columnCount = table.firstElementChild.children.length;
+
+  addRowBtn.disabled = rowCount >= max;
+  removeRowBtn.disabled = rowCount <= min;
+  addColumnBtn.disabled = columnCount >= max;
+  removeColumnBtn.disabled = columnCount <= min;
+}
+
+addRowBtn.addEventListener('click', () => {
+  const columnCount = table.firstElementChild.children.length;
+
+  const row = document.createElement('tr');
+
+  for (let i = 0; i < columnCount; i++) {
+    const cell = document.createElement('td');
+
+    row.appendChild(cell);
+  }
+
+  table.appendChild(row);
+  updateButtonStates();
+});
+
+removeRowBtn.addEventListener('click', () => {
+  table.removeChild(table.lastChild);
+  updateButtonStates();
+});
+
+addColumnBtn.addEventListener('click', () => {
+  Array.from(table.children).forEach((row) => {
+    const cell = document.createElement('td');
+
+    row.appendChild(cell);
+  });
+  updateButtonStates();
+});
+
+removeColumnBtn.addEventListener('click', () => {
+  Array.from(table.children).forEach((row) => {
+    row.removeChild(row.lastChild);
+  });
+  updateButtonStates();
+});
+
+updateButtonStates();
